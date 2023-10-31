3456411caf3a4b8af585f68d9a4691315a42ab50
Mountain Star <github@mtnstar.net>
2023-10-31 10:44:18 +0100

    Clean up toggle primary group

--- app/decorators/sac_cas/group_decorator.rb
+++ app/decorators/sac_cas/group_decorator.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-
+c
 #  Copyright (c) 2012-2023, Schweizer Alpen-Club. This file is part of
 #  hitobito_sac_cas and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
@@ -7,9 +7,10 @@
 
 module SacCas::GroupDecorator
 
-  def primary_group_toggle_link(person, group)
+  def primary_group_toggle_link(person)
+    group = object
     if model.preferred_primary?
-      super(person, group, title: I18n.t('people.roles.roles_aside.set_main_section'))
+      super(person, group, title: I18n.t('people.roles.roles_aside.set_hauptsektion'))
     elsif !person.primary_group.preferred_primary?
       super(person, group)
     end
