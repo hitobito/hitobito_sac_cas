d6a6874000a955b7bb8bd70de6e133523bd9f9c6
Nils Rauch <rauch@puzzle.ch>
2023-11-14 15:20:31 +0100

    Add mitglied role uniqueness validations

--- app/models/sac_cas/role/mitglied_uniqueness_validation.rb
+++ app/models/sac_cas/role/mitglied_uniqueness_validation.rb
@@ -5,16 +5,10 @@
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_sac_cas.
 
-module SacCas::Role::Mitglied
+module SacCas::Role::MitgliedUniquenessValidation
   extend ActiveSupport::Concern
 
-  include SacCas::Role::MitgliedFamilyValidations
-  include SacCas::Role::MitgliedMinimalAgeValidation
-  include SacCas::Role::MitgliedUniquenessValidation
-  include SacCas::RoleBeitragskategorie
-
   included do
-    self.permissions = []
-    self.basic_permissions_only = true
+    validates :type, uniqueness: { scope: :person_id }
   end
 end
