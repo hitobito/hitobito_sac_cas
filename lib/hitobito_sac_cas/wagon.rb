9408816f25c3bcf62a76683e80014d97b26e90d5
Nils Rauch <rauch@puzzle.ch>
2024-01-15 11:19:40 +0100

    Adjust self registration housemate required attrs and positions

--- lib/hitobito_sac_cas/wagon.rb
+++ lib/hitobito_sac_cas/wagon.rb
@@ -29,6 +29,7 @@ module HitobitoSacCas
       Role.prepend SacCas::Role
       SelfRegistration.prepend SacCas::SelfRegistration
       SelfRegistration::MainPerson.prepend SacCas::SelfRegistration::MainPerson
+      SelfRegistration::Housemate.prepend SacCas::SelfRegistration::Housemate
       Roles::Termination.prepend SacCas::Roles::Termination
 
       StepsComponent.prepend SacCas::StepsComponent
