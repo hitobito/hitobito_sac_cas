c983d72e96db7ea3b541a6d3db89f40fd50fd51f
Nils Rauch <rauch@puzzle.ch>
2024-02-12 13:58:12 +0100

    Fix rubocop

--- app/models/group/sektions_mitglieder.rb
+++ app/models/group/sektions_mitglieder.rb
@@ -39,7 +39,8 @@ class Group::SektionsMitglieder < ::Group
     validate :assert_has_active_membership_role
 
     def assert_has_active_membership_role
-      unless Role.exists?(type: ['Group::SektionsMitglieder::Mitglied', 'Group::SektionsMitglieder::MitgliedZusatzsektion'],
+      unless Role.exists?(type: ['Group::SektionsMitglieder::Mitglied',
+                                 'Group::SektionsMitglieder::MitgliedZusatzsektion'],
           person_id: person_id,
           group_id: group_id)
         errors.add(:person, :must_have_mitglied_role_in_group)
@@ -54,7 +55,8 @@ class Group::SektionsMitglieder < ::Group
     validate :assert_has_active_membership_role
 
     def assert_has_active_membership_role
-      unless Role.exists?(type: ['Group::SektionsMitglieder::Mitglied', 'Group::SektionsMitglieder::MitgliedZusatzsektion'],
+      unless Role.exists?(type: ['Group::SektionsMitglieder::Mitglied',
+                                 'Group::SektionsMitglieder::MitgliedZusatzsektion'],
           person_id: person_id,
           group_id: group_id)
         errors.add(:person, :must_have_mitglied_role_in_group)
