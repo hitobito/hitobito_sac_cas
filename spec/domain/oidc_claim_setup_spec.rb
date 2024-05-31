2d7df8ce5f9e8589e07d28a0915988cb9fc8e689
Andreas Maierhofer <maierhofer@puzzle.ch>
2024-05-31 15:49:21 +0200

    Add additional oidc claims, (#428)

---
+++ spec/domain/oidc_claim_setup_spec.rb
@@ -0,0 +1,170 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2024, Schweizer Alpen-Club. This file is part of
+#  hitobito_sac_cas and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sac_cas
+
+require 'spec_helper'
+
+describe OidcClaimSetup do
+  let(:owner) { people(:admin) }
+  let(:response) { :user_info }
+  let(:token) { Doorkeeper::AccessToken.new(resource_owner_id: owner.id, scopes: scope) }
+  let(:claim_keys) { claims.stringify_keys.keys }
+
+  subject(:claims) { Doorkeeper::OpenidConnect::ClaimsBuilder.generate(token, response) }
+
+  shared_examples 'shared claims' do
+    describe 'phone' do
+      it 'is blank when no matching number exists' do
+        expect(claim_keys).to include('phone')
+      end
+
+      it 'returns first number with matching label' do
+        owner.phone_numbers.create!(label: 'Haupt-Telefon', number: '0791234560')
+        owner.phone_numbers.create!(label: 'Haupt-Telefon', number: '0791234561')
+        expect(claims[:phone]).to eq '+41 79 123 45 60'
+      end
+    end
+  end
+
+  context 'name' do
+    let(:scope) { :name }
+
+    it 'picture_url is present' do
+      expect(claims[:picture_url]).to eq owner.decorate.picture_full_url
+    end
+
+    it_behaves_like 'shared claims'
+  end
+
+  context 'with_roles' do
+    let(:scope) { :with_roles }
+
+    describe 'membership_years' do
+      it 'is blank when no matching number exists' do
+        expect(claim_keys).to include('membership_years')
+      end
+
+      it 'is blank when no matching number exists' do
+        expect(claims[:membership_years]).to eq 0
+      end
+    end
+    it_behaves_like 'shared claims'
+  end
+
+  context 'user_groups' do
+    let(:role) { roles(:admin) }
+    let(:scope) { :user_groups }
+    let(:user_groups) { claims[:user_groups] }
+
+    def create_role(key, role)
+      group = key.is_a?(Group) ? key : groups(key)
+      role_type = group.class.const_get(role)
+      Fabricate(role_type.sti_name, group: group, person: owner)
+    end
+
+    it 'includes SAC_employee key when matching role exists' do
+      expect(user_groups).to include 'SAC_employee'
+      expect(user_groups).to include 'Group::Geschaeftsstelle::Admin#384133472'
+    end
+
+    it 'includes section_functionary key when matching role exists' do
+      role = create_role(:bluemlisalp_funktionaere, 'AdministrationReadOnly')
+      expect(user_groups).to include "section_functionary"
+      expect(user_groups).to include "Group::Geschaeftsstelle::Admin#384133472"
+      expect(user_groups).to include format('%s#%d' % [role.type, role.group_id])
+    end
+
+    it 'includes section_president key when matching role exists' do
+      role = create_role(:bluemlisalp_funktionaere, 'Praesidium')
+      expect(user_groups).to include 'section_president'
+    end
+
+    it 'includes SAC_management key when matching role exists' do
+      group = Fabricate(Group::Geschaeftsleitung.sti_name, parent: groups(:root))
+
+      role = create_role(group, 'Ressortleitung')
+      expect(user_groups).to include 'SAC_management'
+    end
+
+    it 'includes SAC_member key when matching role exists' do
+      role = create_role(:bluemlisalp_mitglieder, 'Mitglied')
+      expect(user_groups).to include 'SAC_member'
+      expect(user_groups).not_to include 'SAC_member_additional'
+    end
+
+    it 'includes SAC_member key when matching role exists' do
+      create_role(:bluemlisalp_mitglieder, 'Mitglied')
+      role = create_role(:matterhorn_mitglieder, 'MitgliedZusatzsektion')
+      expect(user_groups).to include 'SAC_member'
+      expect(user_groups).to include 'SAC_member_additional'
+    end
+
+    it 'includes SAC_central_board_member key when matching role exists' do
+      group = Fabricate(Group::Zentralvorstand.sti_name, parent: groups(:root))
+      create_role(group, 'Praesidium')
+      expect(user_groups).to include 'SAC_central_board_member'
+    end
+
+    it 'includes SAC_central_board_member key when matching role exists' do
+      group = Fabricate(Group::Kommission.sti_name, parent: groups(:root))
+      create_role(group, 'Praesidium')
+      expect(user_groups).to include 'SAC_commission_member'
+    end
+
+    it 'includes SAC_tourenportal_subscriber key when matching role exists' do
+      group = Fabricate(Group::AboTourenPortal.sti_name, parent: groups(:abonnenten))
+      create_role(group, 'Autor')
+      expect(user_groups).to include 'SAC_tourenportal_subscriber'
+    end
+
+    it 'includes section_commission_member key when matching role exists' do
+      group = Fabricate(Group::SektionsKommission.sti_name, parent: groups(:bluemlisalp))
+      create_role(group, 'Mitglied')
+      expect(user_groups).to include 'section_commission_member'
+    end
+
+    it 'includes huts_functionary key when matching role exists' do
+      group = Fabricate(Group::SektionsHuettenkommission.sti_name, parent: groups(:bluemlisalp_funktionaere))
+      create_role(group, 'Huettenobmann')
+      expect(user_groups).to include 'huts_functionary'
+    end
+
+    it 'includes tourenportal_author key when matching role exists' do
+      group = Fabricate(Group::AboTourenPortal.sti_name, parent: groups(:abonnenten))
+      create_role(group, 'Autor')
+      expect(user_groups).to include 'tourenportal_author'
+    end
+
+    it 'includes tourenportal_community key when matching role exists' do
+      group = Fabricate(Group::AboTourenPortal.sti_name, parent: groups(:abonnenten))
+      create_role(group, 'Community')
+      expect(user_groups).to include 'tourenportal_community'
+    end
+
+    it 'includes tourenportal_community key when matching role exists' do
+      group = Fabricate(Group::AboTourenPortal.sti_name, parent: groups(:abonnenten))
+      create_role(group, 'Community')
+      expect(user_groups).to include 'tourenportal_community'
+    end
+
+    it 'includes tourenportal_administrator key when matching role exists' do
+      group = Fabricate(Group::AboTourenPortal.sti_name, parent: groups(:abonnenten))
+      create_role(group, 'Admin')
+      expect(user_groups).to include 'tourenportal_administrator'
+    end
+
+    it 'includes magazin_subscriber key when matching role exists' do
+      group = Fabricate(Group::AboMagazin.sti_name, parent: groups(:abonnenten))
+      create_role(group, 'Andere')
+      expect(user_groups).to include 'magazin_subscriber'
+    end
+
+    it 'includes section_tour_functionary key when matching role exists' do
+      create_role(:bluemlisalp_ortsgruppe_ausserberg_tourenkommission, 'JoChef')
+      expect(user_groups).to include 'section_tour_functionary'
+    end
+  end
+end
--- spec/requests/oauth_profile_spec.rb
+++ spec/requests/oauth_profile_spec.rb
@@ -10,75 +10,100 @@ require "spec_helper"
 RSpec.describe 'GET oauth/profile', type: :request do
   let(:application) { Fabricate(:application) }
   let(:user)        { people(:mitglied) }
+  let(:json) { JSON.parse(response.body) }
+  let(:token)  {
+    Fabricate(:access_token, application: application, scopes: "name #{scope}",
+              resource_owner_id: user.id ) }
 
-  context 'with all scopes in token' do
-    let(:token)  { Fabricate(:access_token, application: application, scopes: 'email name with_roles', resource_owner_id: user.id ) }
 
-    context 'with scope "name" in request' do
-      it 'succeeds' do
-        get '/oauth/profile', headers: { 'Authorization': 'Bearer ' + token.token, 'X-Scope': 'name' }
 
-        expect(response).to have_http_status(:ok)
-        expect(response.content_type).to eq('application/json; charset=utf-8')
-        json = JSON.parse(response.body)
-        expect(json).to match({
-                                id: user.id,
-                                email: user.email,
-                                first_name: user.first_name,
-                                last_name: user.last_name,
-                                nickname: user.nickname,
-                                address: user.address,
-                                zip_code: user.zip_code,
-                                town: user.town,
-                                country: user.country,
-                                picture_url: /\/packs-test\/media\/images\/profile-.*\.svg/,
-                              }.deep_stringify_keys)
-      end
-    end
+  def make_request(skip_checks: true)
+    get '/oauth/profile', headers: { 'Authorization': 'Bearer ' + token.token, 'X-Scope': scope }
+    return if skip_checks
 
-    context 'with scope "with_roles" in request' do
-      it 'succeeds' do
-        get '/oauth/profile', headers: { 'Authorization': 'Bearer ' + token.token, 'X-Scope': 'with_roles' }
+    expect(response).to have_http_status(:ok)
+    expect(response.content_type).to eq('application/json; charset=utf-8')
+  end
 
-        expect(response).to have_http_status(:ok)
-        expect(response.content_type).to eq('application/json; charset=utf-8')
-        json = JSON.parse(response.body)
-        expect(json).to match({
-                             id: user.id,
-                             first_name: user.first_name,
-                             last_name: user.last_name,
-                             nickname: user.nickname,
-                             company_name: user.company_name,
-                             company: user.company,
-                             email: user.email,
-                             address: user.address,
-                             zip_code: user.zip_code,
-                             town: user.town,
-                             country: user.country,
-                             gender: user.gender,
-                             birthday: user.birthday.to_s.presence,
-                             primary_group_id: user.primary_group_id,
-                             language: user.language,
-                             picture_url: /\/packs-test\/media\/images\/profile-.*\.svg/,
-                             roles: [{
-                               group_id: user.roles.first.group_id,
-                               group_name: user.roles.first.group.name,
-                               role: 'Group::SektionsMitglieder::Mitglied',
-                               role_class: 'Group::SektionsMitglieder::Mitglied',
-                               role_name: 'Mitglied (Stammsektion)',
-                               permissions: [],
-                               layer_group_id: user.roles.first.group.layer_group_id
-                             },{
-                               group_id: user.roles.second.group_id,
-                               group_name: user.roles.second.group.name,
-                               role: 'Group::SektionsMitglieder::MitgliedZusatzsektion',
-                               role_class: 'Group::SektionsMitglieder::MitgliedZusatzsektion',
-                               role_name: 'Mitglied (Zusatzsektion)',
-                               permissions: [],
-                               layer_group_id: user.roles.second.group.layer_group_id
-                             }]
-                           }.deep_stringify_keys)
-      end
+  context 'with scope "name" in request' do
+    let(:scope) { :name }
+
+    it 'succeeds' do
+      make_request
+      expect(json).to match({
+        id: user.id,
+        email: user.email,
+        first_name: user.first_name,
+        last_name: user.last_name,
+        nickname: user.nickname,
+        address: user.address,
+        zip_code: user.zip_code,
+        town: user.town,
+        country: user.country,
+        picture_url: /\/packs-test\/media\/images\/profile-.*\.svg/,
+        phone: nil,
+      }.deep_stringify_keys)
     end
   end
+
+  context 'with scope "with_roles" in request' do
+    let(:scope) { 'with_roles' }
+
+    it 'succeeds' do
+      make_request
+      expect(json).to match({
+        id: user.id,
+        first_name: user.first_name,
+        last_name: user.last_name,
+        nickname: user.nickname,
+        company_name: user.company_name,
+        company: user.company,
+        email: user.email,
+        address: user.address,
+        zip_code: user.zip_code,
+        town: user.town,
+        country: user.country,
+        gender: user.gender,
+        birthday: user.birthday.to_s.presence,
+        primary_group_id: user.primary_group_id,
+        language: user.language,
+        picture_url: /\/packs-test\/media\/images\/profile-.*\.svg/,
+        phone: nil,
+        membership_years: 1,
+        roles: [{
+          group_id: user.roles.first.group_id,
+          group_name: user.roles.first.group.name,
+          role: 'Group::SektionsMitglieder::Mitglied',
+          role_class: 'Group::SektionsMitglieder::Mitglied',
+          role_name: 'Mitglied (Stammsektion)',
+          permissions: [],
+          layer_group_id: user.roles.first.group.layer_group_id
+        },{
+          group_id: user.roles.second.group_id,
+          group_name: user.roles.second.group.name,
+          role: 'Group::SektionsMitglieder::MitgliedZusatzsektion',
+          role_class: 'Group::SektionsMitglieder::MitgliedZusatzsektion',
+          role_name: 'Mitglied (Zusatzsektion)',
+          permissions: [],
+          layer_group_id: user.roles.second.group.layer_group_id
+        }]
+      }.deep_stringify_keys)
+    end
+  end
+
+  context 'with scope "user_groups" in request' do
+    let(:scope) { 'user_groups' }
+
+    it 'succeeds' do
+      make_request
+      expect(json['user_groups']).to include 'SAC_member'
+    end
+
+    it 'is forbidden to be used without names scope on token' do
+      token.update!(scopes: 'user_groups')
+      make_request
+      expect(response).to have_http_status(:forbidden)
+    end
+  end
+
 end
