a5a08f1997db63c4470f5327edf43017eaa50973
Chris Hunziker <chris.hunziker@renuo.ch>
2024-06-05 11:58:13 +0000

    Monkeypatch status text

--- app/helpers/sac_cas/event/participation_banner.rb
+++ app/helpers/sac_cas/event/participation_banner.rb
@@ -22,12 +22,28 @@ module SacCas::Event::ParticipationBanner
   end
 
   def can_destroy?
-    can?(:cancel, @user_participation) && @user_participation.participant_cancelable? &&
-      Event::ParticipationButtons.conditions[:cancel].include?(@user_participation.state.to_sym)
+    %w(applied assigned summoned unconfirmed).include?(@user_participation.state)
   end
 
   def render_cancel_popover
     @context.render('event/participations/popover_participant_cancels',
                     entry: @user_participation).to_s
+  end
+
+  def status_text
+    t(@user_participation.state, scope: 'event.participations.states')
+  end
+
+  def status_class    
+    state = @user_participation.state.to_sym
+    alert_class = if %i[assigned attended summoned].include? state
+                    :success
+                  elsif %i[unconfirmed applied absent].include? state
+                    :warning
+                  else
+                    :danger
+                  end
+
+    "alert alert-#{alert_class}"
   end
 end
