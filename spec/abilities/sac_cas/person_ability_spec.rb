e3845c61d883792d72e1b7ccc5fe5dda3ca84650
Nils Rauch <rauch@puzzle.ch>
2024-06-10 13:55:09 +0200

    Refactor person_ability specs

--- spec/abilities/sac_cas/person_ability_spec.rb
+++ spec/abilities/sac_cas/person_ability_spec.rb
@@ -54,38 +54,58 @@ describe PersonAbility do
   end
 
   describe 'household' do
-    let(:admin_ability) { Ability.new(admin) }
-    let(:mitglied_ability) { Ability.new(mitglied) }
-    let(:familienmitglied2) { people(:familienmitglied2) }
-    let(:familienmitglied2_ability) { Ability.new(familienmitglied2) }
+    let(:familienmitglied) { people(:familienmitglied2) }
+
+    let(:child) { people(:familienmitglied_kind) }
 
     let(:mitgliederverwaltung_sektion) do
       Fabricate(Group::SektionsFunktionaere::Mitgliederverwaltung.sti_name.to_sym,
                 group: groups(:bluemlisalp_funktionaere)).person
     end
-    let(:mitgliederverwaltung_sektion_ability) { Ability.new(mitgliederverwaltung_sektion) }
-
-    let(:child) { people(:familienmitglied_kind) }
 
     context 'sac_family_main_person' do
-      it 'can be set as admin' do
-        expect(admin_ability).to be_able_to(:set_sac_family_main_person, familienmitglied2)
+      [Group::Geschaeftsstelle::Mitarbeiter, Group::Geschaeftsstelle::Admin].each do |role_type|
+        context role_type do
+          let(:person) { Fabricate(role_type.sti_name, group: groups(:geschaeftsstelle)).person }
+
+          it 'is permitted' do
+            expect(ability).to be_able_to(:set_sac_family_main_person, familienmitglied)
+          end
+
+          it 'is not permitted when person is not an adult' do
+            expect(ability).not_to be_able_to(:set_sac_family_main_person, child)
+          end
+        end
       end
 
-      it 'cannot be set as mitglied' do
-        expect(mitglied_ability).not_to be_able_to(:set_sac_family_main_person, familienmitglied2)
+      [Group::SektionsFunktionaere::Mitgliederverwaltung, Group::SektionsFunktionaere::Administration].each do |role_type|
+        context role_type do
+          let(:person) { Fabricate(role_type.sti_name, group: groups(:bluemlisalp_funktionaere)).person }
+
+          it 'is permitted' do
+            expect(ability).to be_able_to(:set_sac_family_main_person, familienmitglied)
+          end
+
+          it 'is not permitted when person is not an adult' do
+            expect(ability).not_to be_able_to(:set_sac_family_main_person, child)
+          end
+        end
       end
 
-      it 'cannot be set as yourself' do
-        expect(familienmitglied2_ability).not_to be_able_to(:set_sac_family_main_person, familienmitglied2)
+      context 'as mitglied' do
+        let(:person) { mitglied }
+
+        it 'is not permitted' do
+          expect(ability).not_to be_able_to(:set_sac_family_main_person, familienmitglied)
+        end
       end
 
-      it 'can be set as mitgliederverwaltung_sektion' do
-        expect(mitgliederverwaltung_sektion_ability).to be_able_to(:set_sac_family_main_person, familienmitglied2)
-      end
+      context 'as self' do
+        let(:person) { familienmitglied }
 
-      it 'cannot set when person is not an adult' do
-        expect(mitgliederverwaltung_sektion_ability).not_to be_able_to(:set_sac_family_main_person, child)
+        it 'is not permitted' do
+          expect(ability).not_to be_able_to(:set_sac_family_main_person, familienmitglied)
+        end
       end
     end
   end
