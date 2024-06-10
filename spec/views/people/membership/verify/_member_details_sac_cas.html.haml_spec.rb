00052c9d0d02df6c1161e4ecedfe8263f11b0eed
Chris Hunziker <chris.hunziker@renuo.ch>
2024-06-10 13:10:01 +0000

    Improve spec

--- spec/views/people/membership/verify/_member_details_sac_cas.html.haml_spec.rb
+++ spec/views/people/membership/verify/_member_details_sac_cas.html.haml_spec.rb
@@ -8,24 +8,25 @@ require 'spec_helper'
 describe 'people/membership/verify/_member_details_sac_cas.html.haml' do
   include FormatHelper
 
+  subject(:tour_guide_info) { I18n.t('people.membership.verify.member_details_sac_cas.tour_guide') }
   let(:dom) { render; Capybara::Node::Simple.new(@rendered)  }
-  let(:person) { people(:mitglied) }
+  let(:person) { people(:tourenleiter) }
 
   before { allow(view).to receive_messages(person: person) }
 
-  context 'active tour guide' do
-    it 'renders membership info for active tour guides' do
-      person.roles.destroy_all
-      person.qualifications.create!(
-        qualification_kind: qualification_kinds(:ski_leader),
-        start_at: 1.month.ago
-      )
-      person.roles.create!(
-        type: Group::SektionsTourenkommission::Tourenleiter.sti_name,
-        group: groups(:matterhorn_tourenkommission)
-      )
+  context 'member' do
+    let(:person) { people(:mitglied) }
 
-      expect(dom).to have_text I18n.t('people.membership.verify.member_details_sac_cas.tour_guide')
+    it 'hides tour guide info' do
+      expect(dom).not_to have_text tour_guide_info
+    end
+  end
+
+  context 'tour guide' do
+    let(:person) { people(:tourenleiter) }
+
+    it 'renders tour guide info for active tour guides' do
+      expect(dom).to have_text tour_guide_info
     end
   end
 end
