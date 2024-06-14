3ddad4e20137cd8de895b41eda3f3616b74b964d
Andreas Maierhofer <maierhofer@puzzle.ch>
2024-06-14 13:14:58 +0200

    Implement MemberJoinSektion Base model, (#619)

---
+++ app/models/memberships/member_join_section_base.rb
@@ -0,0 +1,105 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2024, Schweizer Alpen-Club. This file is part of
+#  hitobito_sac_cas and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sac_cas
+
+
+module Memberships
+  class MemberJoinSectionBase
+    include ActiveModel::Model
+    include ActiveModel::Attributes
+    include ActiveModel::Validations
+
+    validate :assert_person_is_sac_member
+    validate :assert_person_is_not_already_member_of_sektion
+    validate :assert_family_main_person, if: :validate_family_main_person?
+
+    def initialize(group, person, join_date, **params)
+      raise 'group is not supported' unless supported?(group)
+
+      @person = person
+      @group = group
+      @join_date = join_date
+
+      super(**params)
+    end
+
+    def valid?
+      super && roles_valid?
+    end
+
+    def save
+      valid? && save_roles.all?
+    end
+
+    def save!
+      raise 'cannot save invalid model' unless valid?
+      save
+    end
+
+    private
+
+    def save_roles
+      Role.transaction do
+        roles.each(&:save!)
+      end
+    end
+
+    def affected_people
+      person.sac_family.member? ? person.sac_family.family_members : [person]
+    end
+
+    def roles
+      @roles ||= affected_people.flat_map { |p| build_roles(p) }
+    end
+
+    def roles_valid?
+      roles.each do |role|
+        role.validate
+        role.errors.full_messages.each do |msg|
+          errors.add(:base, "#{role.person}: #{msg}")
+        end
+      end
+    end
+
+    # build roles of correct type in correct subgroup of sektion
+    # and with correct dates (convert_on/delete_on)
+    def build_roles(_person)
+      []
+    end
+
+    def assert_person_is_sac_member
+      unless person.roles.exists?(type: Group::SektionsMitglieder::Mitglied.sti_name)
+        errors.add(:person, :must_be_sac_member)
+      end
+    end
+
+    def assert_person_is_not_already_member_of_sektion
+      if person.roles.exists?(group_id: group.descendants,
+                              type: SacCas::MITGLIED_HAUPTSEKTION_ROLES)
+        errors.add(:person, :must_not_be_section_member)
+      end
+    end
+
+    def assert_family_main_person
+      unless person.roles.exists?(
+        type: Group::SektionsMitglieder::Mitglied.sti_name,
+        beitragskategorie: SacCas::Beitragskategorie::Calculator::CATEGORY_FAMILY
+      ) && person.sac_family_main_person
+        errors.add(:person, :must_be_family_main_person)
+      end
+    end
+
+    def validate_family_main_person?
+      false
+    end
+
+    def supported?(group)
+      group.is_a?(Group::Sektion) || group.is_a?(Group::Ortsgruppe)
+    end
+
+    attr_reader :person, :group
+  end
+end
