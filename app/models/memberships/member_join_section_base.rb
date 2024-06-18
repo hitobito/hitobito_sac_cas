5616371040c32e11f19c35cb609cba6a76db6cc5
Andreas Maierhofer <maierhofer@puzzle.ch>
2024-06-18 09:44:06 +0200

    Implement MemberJoinSektion Base model, (#619) (#644)

---
+++ app/models/memberships/member_join_section_base.rb
@@ -0,0 +1,110 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2024, Schweizer Alpen-Club. This file is part of
+#  hitobito_sac_cas and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sac_cas
+
+
+module Memberships
+  class MemberJoinSectionBase
+    include ActiveModel::Model
+    include ActiveModel::Attributes
+    include ActiveModel::Validations
+
+    validate :assert_person_is_sac_member
+    validate :assert_person_not_member_of_join_section
+    validate :assert_family_main_person, if: :validate_family_main_person?
+
+    def initialize(join_section, person, join_date, **params)
+      @join_section = join_section
+      assert_sac_section_or_ortsgruppe!
+
+      @person = person
+      @join_date = join_date
+
+      super(**params)
+    end
+
+    def valid?
+      super && roles_valid?
+    end
+
+    def save
+      valid? && save_roles.all?
+    end
+
+    def save!
+      raise 'cannot save invalid model' unless valid?
+      save
+    end
+
+    private
+
+    def save_roles
+      Role.transaction do
+        roles.each(&:save!)
+      end
+    end
+
+    def affected_people
+      person.sac_family.member? ? person.sac_family.family_members : [person]
+    end
+
+    def roles
+      @roles ||= affected_people.flat_map { |p| build_roles(p) }
+    end
+
+    def roles_valid?
+      roles.each do |role|
+        role.validate
+        role.errors.full_messages.each do |msg|
+          errors.add(:base, "#{role.person}: #{msg}")
+        end
+      end
+    end
+
+    # build roles of correct type in correct subgroup of sektion
+    # and with correct dates (convert_on/delete_on)
+    def build_roles(_person)
+      []
+    end
+
+    def assert_person_is_sac_member
+      unless sac_membership.active?
+        errors.add(:person, :must_be_sac_member)
+      end
+    end
+
+    def assert_person_not_member_of_join_section
+      if sac_membership.active_or_pending_in?(join_section)
+        errors.add(:person, :must_not_be_join_section_member)
+      end
+    end
+
+    def assert_family_main_person
+      unless person.roles.exists?(
+        type: Group::SektionsMitglieder::Mitglied.sti_name,
+        beitragskategorie: SacCas::Beitragskategorie::Calculator::CATEGORY_FAMILY
+      ) && person.sac_family_main_person
+        errors.add(:person, :must_be_family_main_person)
+      end
+    end
+
+    def validate_family_main_person?
+      false
+    end
+
+    def assert_sac_section_or_ortsgruppe!
+      unless join_section.is_a?(Group::Sektion) || join_section.is_a?(Group::Ortsgruppe)
+        raise 'must be section/ortsgruppe'
+      end
+    end
+
+    def sac_membership
+      People::SacMembership.new(person)
+    end
+
+    attr_reader :person, :join_section
+  end
+end
---
+++ spec/models/memberships/member_join_section_base_spec.rb
@@ -0,0 +1,204 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2024, Schweizer Alpen-Club. This file is part of
+#  hitobito_sac_cas and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sac_cas
+
+require 'spec_helper'
+
+describe Memberships::MemberJoinSectionBase do
+  def create_role(key, role, owner: person, **attrs)
+    group = key.is_a?(Group) ? key : groups(key)
+    role_type = group.class.const_get(role)
+    Fabricate(role_type.sti_name, group: group, person: owner, **attrs)
+  end
+
+  it 'initialization fails on invalid group' do
+    expect do
+      described_class.new(Group::Sektion.new, :person, :date)
+    end.not_to raise_error
+
+    expect do
+      described_class.new(Group::Ortsgruppe.new, :person, :date)
+    end.not_to raise_error
+
+    expect do
+      described_class.new(Group::SacCas.new, :person, :date)
+    end.to raise_error('must be section/ortsgruppe')
+  end
+
+  describe 'validations' do
+    let(:person) { Fabricate(:person) }
+    let(:join_section) { groups(:bluemlisalp) }
+    let(:errors) { obj.errors.full_messages }
+    let(:date) { Date.new(2024, 6, 14) }
+
+    subject(:obj) { described_class.new(join_section, person, date) }
+
+    it 'is invalid if person is not an sac member' do
+      expect(obj).not_to be_valid
+      expect(errors).to eq ['Person muss Sac Mitglied sein']
+    end
+
+    it 'is valid with membership in different section' do
+      create_role(:matterhorn_mitglieder, 'Mitglied')
+      expect(obj).to be_valid
+    end
+
+    describe 'existing membership in tree' do
+      describe 'join section' do
+        it 'is invalid if person is join section member' do
+          create_role(:bluemlisalp_mitglieder, 'Mitglied')
+          expect(obj).not_to be_valid
+          expect(errors).to eq [
+            'Person ist bereits Mitglied der Sektion oder hat ein offenes Beitrittsgesuch'
+          ]
+        end
+
+        it 'is invalid if person has requested membership via section' do
+          create_role(:bluemlisalp_neuanmeldungen_sektion, 'Neuanmeldung')
+          expect(obj).not_to be_valid
+          expect(errors).to eq [
+            'Person muss Sac Mitglied sein',
+            'Person ist bereits Mitglied der Sektion oder hat ein offenes Beitrittsgesuch'
+          ]
+        end
+
+        it 'is invalid if person has requested membership via nv' do
+          create_role(:bluemlisalp_neuanmeldungen_nv, 'Neuanmeldung')
+          expect(obj).not_to be_valid
+          expect(errors).to eq [
+            'Person muss Sac Mitglied sein',
+            'Person ist bereits Mitglied der Sektion oder hat ein offenes Beitrittsgesuch'
+          ]
+        end
+      end
+
+      describe 'ortsgruppe' do
+        it 'is invalid if person is ortsgruppen member' do
+          create_role(:bluemlisalp_ortsgruppe_ausserberg_mitglieder, 'Mitglied')
+          expect(obj).not_to be_valid
+          expect(errors).to eq [
+            'Person ist bereits Mitglied der Sektion oder hat ein offenes Beitrittsgesuch'
+          ]
+        end
+
+        it 'is invalid if person has requested membership' do
+          create_role(:bluemlisalp_ortsgruppe_ausserberg_neuanmeldungen_nv, 'Neuanmeldung')
+          expect(obj).not_to be_valid
+          expect(errors).to eq [
+            'Person muss Sac Mitglied sein',
+            'Person ist bereits Mitglied der Sektion oder hat ein offenes Beitrittsgesuch'
+          ]
+        end
+      end
+    end
+
+    context 'family main person' do
+      it 'is invalid when obj validates and person is not main family person' do
+        expect(obj).to receive(:validate_family_main_person?).and_return(true)
+        create_role(:bluemlisalp_mitglieder, 'Mitglied')
+        expect(obj).not_to be_valid
+        expect(errors).to eq [
+          'Person ist bereits Mitglied der Sektion oder hat ein offenes Beitrittsgesuch',
+          'Person muss Hauptperson der Familie sein'
+        ]
+      end
+
+      it 'is valid when obj validates and person is not main family person' do
+        expect(obj).to receive(:validate_family_main_person?).and_return(true)
+        person.update!(sac_family_main_person: true)
+        role = create_role(:bluemlisalp_mitglieder, 'Mitglied').tap do |r|
+          Role.where(id: r.id).update_all(beitragskategorie: :family)
+        end
+        expect(obj).not_to be_valid
+        expect(errors).to eq [
+          'Person ist bereits Mitglied der Sektion oder hat ein offenes Beitrittsgesuch'
+        ]
+      end
+    end
+  end
+
+  describe 'saving' do
+    let(:person) { Fabricate(:person) }
+    let(:group) { groups(:matterhorn) }
+    let(:errors) { obj.errors.full_messages }
+    let(:date) { Date.new(2024, 6, 14) }
+
+    subject(:obj) { described_class.new(group, person, date) }
+
+    context 'invalid' do
+      it 'save returns false and populates errors' do
+        expect(obj.save).to eq false
+        expect(obj.errors.full_messages).to eq ['Person muss Sac Mitglied sein']
+      end
+
+      it 'save! raises' do
+        expect { obj.save! }.to raise_error 'cannot save invalid model'
+      end
+    end
+
+    context 'single person' do
+      let(:matterhorn_mitglieder) { groups(:matterhorn_mitglieder) }
+      let(:matterhorn_funktionaere) { groups(:matterhorn_funktionaere) }
+      let!(:bluemlisalp_mitglied) { create_role(:bluemlisalp_mitglieder, 'Mitglied') }
+
+      it 'creates single role for person' do
+        allow(obj).to receive(:build_roles) do |person|
+          Fabricate.build(Group::SektionsMitglieder::MitgliedZusatzsektion.sti_name,
+                          person: person, group: matterhorn_mitglieder)
+        end
+        expect do
+          expect(obj.save).to eq true
+        end.to change { person.reload.roles.count }.by(1)
+      end
+
+      it 'might process multiple roles for single person' do
+        bluemlisalp_mitglied.deleted_at = Time.zone.now
+        allow(obj).to receive(:build_roles) do |person|
+          [bluemlisalp_mitglied,
+           Fabricate.build(Group::SektionsMitglieder::Mitglied.sti_name,
+                           group: matterhorn_mitglieder,
+                           person: person)]
+        end
+        expect(obj.save).to eq true
+        sac_membership = People::SacMembership.new(person)
+        person.reload
+        expect(sac_membership.active_in?(groups(:matterhorn))).to eq(true)
+        expect(sac_membership.active_in?(groups(:bluemlisalp))).to eq(false)
+      end
+    end
+
+    context 'family' do
+      let(:other) { Fabricate(:person) }
+      let(:matterhorn_mitglieder) { groups(:matterhorn_mitglieder) }
+
+      def create_sac_family(person, *others)
+        others.each { |p| person.household.add(p) }
+        person.household.save!
+        person.reload
+      end
+
+      before do
+        create_sac_family(person, other)
+        person.update!(sac_family_main_person: true)
+        create_role(:bluemlisalp_mitglieder, 'Mitglied', beitragskategorie: :family)
+        create_role(:bluemlisalp_mitglieder, 'Mitglied', owner: other.reload,
+                                                         beitragskategorie: :family)
+      end
+
+      it 'creates roles for each member' do
+        allow(obj).to receive(:build_roles) do |person|
+          Fabricate.build(Group::SektionsMitglieder::MitgliedZusatzsektion.sti_name,
+                          person: person, group: matterhorn_mitglieder)
+        end
+
+        expect do
+          expect(obj.save!).to eq true
+        end.to change { Role.count }.by(2)
+      end
+    end
+
+  end
+end
