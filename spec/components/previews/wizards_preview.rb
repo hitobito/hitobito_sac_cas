70509b8d492a911ab3e519a4b90dc86da2ad20c9
Andreas Maierhofer <maierhofer@puzzle.ch>
2024-06-19 13:18:06 +0200

    Feedback from review

--- spec/components/previews/wizards_preview.rb
+++ spec/components/previews/wizards_preview.rb
@@ -7,28 +7,36 @@
 
 class WizardsPreview < ViewComponent::Preview
 
-  def choose_sektion_form_step(wizards_base: {})
-    setup_wrapping_component(Wizards::Steps::ChooseSektionForm, wizards_base) do |view_ctx, step|
+  def choose_sektion_form_step(wizards_preview_wizard: {})
+    wizard = build_wizard(Wizards::Steps::ChooseSektionForm, wizards_preview_wizard)
+    render_wrapped(wizard)
+  end
+
+  def choose_sektion_form_step_with_alert(wizards_preview_wizard: {})
+    wizard = build_wizard(Wizards::Steps::ChooseSektionForm, wizards_preview_wizard)
+    render_wrapped(wizard) do |view_ctx, step|
       view_ctx.content_tag(:p, step.group&.name, class: 'alert alert-info') if step.group
     end
   end
 
   private
 
-  def setup_wrapping_component(step_class, wizards_base)
-    Wizards::Base.steps = [step_class]
-    wizard = Wizards::Base.new(current_step: 0, current_ability: :current_ability, **wizards_base)
-    view_ctx = WizardsPreviewsController.new.view_context
-    wizard.valid? if wizards_base.present?
-    f = StandardFormBuilder.new(:wizard, wizard, view_ctx, {})
+  def render_wrapped(wizard)
     render WrappingComponent.new do
+      view_ctx = WizardsPreviewsController.new.view_context
       view_ctx.standard_form(wizard, url: '', authenticity_token: '', method: :get,
                                      data: { controller: 'autosubmit' }) do |f|
         step_component = StepsComponent.new(partials: wizard.partials, step: 0, form: f)
-        content = yield view_ctx, wizard.step_at(0)
+        content = yield(view_ctx, wizard.step_at(0)) if block_given?
         safe_join([content, view_ctx.render(step_component)].compact)
       end
     end
+  end
+
+  def build_wizard(step_class, params)
+    self.class.const_set('Wizard', Class.new(Wizards::Base) { self.steps = [step_class] })
+    Wizard.new(current_step: 0, current_ability: :current_ability,
+               **params).tap { |w| w.valid? if params.present? }
   end
 
   class WrappingComponent < ViewComponent::Base
