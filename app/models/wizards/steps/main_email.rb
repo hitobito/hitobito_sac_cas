cc8178b806ded6322b4c490795d98fb47605fe81
Raphael Nestler <raphael.nestler@renuo.ch>
2024-06-21 15:09:20 +0200

    Add MainEmail wizard step (#656)
    * Add Wizards::Steps::MainEmail model
    
    * Add main email wizard step view partial
    
    * Add main_email_form_step to wizards preview
    
    * Make email field name translatable
    
    * Addapt to new api
    
    ---------
    
    Co-authored-by: Andreas Maierhofer <maierhofer@puzzle.ch>

---
+++ app/models/wizards/steps/main_email.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2024, Schweizer Alpen-Club. This file is part of
+#  hitobito_sac_cas and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sac_cas.
+
+class Wizards::Steps::MainEmail < Wizards::Step
+  include ValidatedEmail
+
+  attribute :email, :string
+
+  validates :email, presence: true
+  validate :ensure_email_available
+
+  # #email_changed? is used in `ValidatedEmail` to determine if the email
+  # should be validated. Here it should only be validated if the email is
+  # present.
+  def email_changed?
+    email.present?
+  end
+
+  def ensure_email_available
+    return if email.blank?
+
+    if Person.exists?(email: email)
+      errors.add(:email, I18n.t('activerecord.errors.models.person.attributes.email.taken'))
+    end
+  end
+end
