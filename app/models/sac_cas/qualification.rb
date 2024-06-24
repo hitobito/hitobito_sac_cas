037a09a6b939eccab5a17d0593f66230266001d2
Chris Hunziker <chris.hunziker@renuo.ch>
2024-06-24 15:18:29 +0000

    Add validation

--- app/models/sac_cas/qualification.rb
+++ app/models/sac_cas/qualification.rb
@@ -12,6 +12,7 @@ module SacCas::Qualification
     before_validation :set_finish_at, unless: :finish_at
 
     validate :assert_meaningful
+    validate :start_must_be_in_past, on: :create
   end
 
   private
@@ -26,4 +27,7 @@ module SacCas::Qualification
     end
   end
 
+  def start_must_be_in_past
+    errors.add(:start_at, :must_be_in_past) if start_at.present? && start_at > Time.zone.today
+  end
 end
