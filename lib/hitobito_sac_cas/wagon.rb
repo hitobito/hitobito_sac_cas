799debb698a900bed946a0b3a123e42080498f70
Raphael Nestler <raphael.nestler@renuo.ch>
2024-06-28 15:03:52 +0200

    Override TerminateRoleLink helper to link to wizard

--- lib/hitobito_sac_cas/wagon.rb
+++ lib/hitobito_sac_cas/wagon.rb
@@ -58,6 +58,7 @@ module HitobitoSacCas
       SelfRegistration.prepend SacCas::SelfRegistration
       SelfRegistration::MainPerson.prepend SacCas::SelfRegistration::MainPerson
       Roles::Termination.prepend SacCas::Roles::Termination
+      Roles::TerminateRoleLink.prepend SacCas::Roles::TerminateRoleLink
       Qualification.include SacCas::Qualification
       QualificationKind.include SacCas::QualificationKind
       Contactable.prepend SacCas::Contactable
