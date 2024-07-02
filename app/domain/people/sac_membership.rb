hitobito/hitobito_sac_cas

commit 5b1773aef00eb3c6a7666621fbf3239fb4c2ad9f

SacCas Konstanten Namen verbessern (#689)
* Refactor SacCas constants names

* Keep naming consistend: Stammsektion instead of Hauptsektion

app/domain/people/sac_membership.rb
@@ -7,14 +7,8 @@
 
 class People::SacMembership
 
-  MEMBERSHIP_ROLES = [
-    Group::SektionsMitglieder::Mitglied
-  ].freeze
-
   def initialize(person)
     @person = person
-    @roles = MEMBERSHIP_ROLES.map(&:sti_name)
-    @active_or_approvable_roles = SacCas::MITGLIED_HAUPTSEKTION_ROLES.map(&:sti_name)
   end
 
   def active?
@@ -24,13 +18,13 @@ class People::SacMembership
   # checks for any active membership roles
   def active_in?(sac_section)
     @person.roles.exists?(group_id: sac_section.children,
-                          type: @roles)
+                          type: stammsektion_mitglied_sti_names)
   end
 
   # checkes for active and also approvabable (neuanmeldung) roles
   def active_or_approvable_in?(sac_section)
     @person.roles.exists?(group_id: sac_section.children,
-                          type: @active_or_approvable_roles)
+                          type: mitglied_and_neuanmeldung_sti_names)
   end
 
   def anytime?
@@ -38,7 +32,7 @@ class People::SacMembership
   end
 
   def roles
-    @person.roles.select { |r| MEMBERSHIP_ROLES.include?(r.class) }
+    @person.roles.select { |r| SacCas::MITGLIED_STAMMSEKTION_ROLES.include?(r.class) }
   end
 
   # There should be only one active `Mitglied` role at a time anyway
@@ -52,11 +46,14 @@ class People::SacMembership
 
   private
 
+  def stammsektion_mitglied_sti_names = SacCas::MITGLIED_STAMMSEKTION_ROLES.map(&:sti_name)
+  def mitglied_and_neuanmeldung_sti_names = SacCas::MITGLIED_AND_NEUANMELDUNG_ROLES.map(&:sti_name)
+
   def any_future_role?
-    @person.roles.future.where(convert_to: @roles).exists?
+    @person.roles.future.where(convert_to: stammsektion_mitglied_sti_names).exists?
   end
 
   def any_past_role?
-    @person.roles.deleted.where(type: @roles).exists?
+    @person.roles.deleted.where(type: stammsektion_mitglied_sti_names).exists?
   end
 end

app/abilities/sac_cas/role_ability.rb
@@ -35,10 +35,6 @@ module SacCas::RoleAbility
     SacCas::MITGLIED_ROLES.include?(role.class)
   end
 
-  def mitglied_hauptsektion_role?(role = subject)
-    SacCas::MITGLIED_HAUPTSEKTION_ROLES.include?(role.class)
-  end
-
   def mitglied_zusatzsektion_role?(role = subject)
     SacCas::MITGLIED_ZUSATZSEKTION_ROLES.include?(role.class)
   end

app/domain/groups/primary.rb
@@ -7,7 +7,7 @@
 
 module Groups
   class Primary
-    ROLE_TYPES = SacCas::MITGLIED_HAUPTSEKTION_ROLES.map(&:sti_name).freeze
+    ROLE_TYPES = SacCas::STAMMSEKTION_ROLES.map(&:sti_name).freeze
 
     GROUP_TYPES = ROLE_TYPES.collect(&:deconstantize).freeze
 

app/domain/people/sac_family.rb
@@ -167,11 +167,11 @@ class People::SacFamily
   #end
 
   def stammsektion_role_types
-    SacCas::MITGLIED_HAUPTSEKTION_ROLES.map(&:sti_name)
+    SacCas::STAMMSEKTION_ROLES.map(&:sti_name)
   end
 
   def zusatzsektion_role_types
-    SacCas::MITGLIED_ZUSATZSEKTION_ROLES.map(&:sti_name)
+    SacCas::ZUSATZSEKTION_ROLES.map(&:sti_name)
   end
 
   def all_member_and_neuanmeldung_role_types

app/domain/sac_cas.rb
@@ -7,31 +7,50 @@
 
 module SacCas
 
-  SAC_MITARBEITER_ROLES = [
-    ::Group::Geschaeftsstelle::Mitarbeiter,
-    ::Group::Geschaeftsstelle::Admin
-  ].freeze
+  ### Membership roles
 
-  MITGLIED_HAUPTSEKTION_ROLES = [
-    ::Group::SektionsMitglieder::Mitglied,
+  MITGLIED_STAMMSEKTION_ROLES = [::Group::SektionsMitglieder::Mitglied].freeze
+  NEUANMELDUNG_STAMMSEKTION_ROLES = [
     ::Group::SektionsNeuanmeldungenNv::Neuanmeldung,
     ::Group::SektionsNeuanmeldungenSektion::Neuanmeldung
   ].freeze
 
-  MITGLIED_ZUSATZSEKTION_ROLES = [
-    ::Group::SektionsMitglieder::MitgliedZusatzsektion,
-    ::Group::SektionsNeuanmeldungenNv::NeuanmeldungZusatzsektion,
-    ::Group::SektionsNeuanmeldungenSektion::NeuanmeldungZusatzsektion
-  ].freeze
-
-  NEUANMELDUNG_HAUPTSEKTION_ROLES = [
-    ::Group::SektionsNeuanmeldungenNv::Neuanmeldung,
-    ::Group::SektionsNeuanmeldungenSektion::Neuanmeldung
-  ].freeze
-
+  MITGLIED_ZUSATZSEKTION_ROLES = [::Group::SektionsMitglieder::MitgliedZusatzsektion].freeze
   NEUANMELDUNG_ZUSATZSEKTION_ROLES = [
     ::Group::SektionsNeuanmeldungenNv::NeuanmeldungZusatzsektion,
     ::Group::SektionsNeuanmeldungenSektion::NeuanmeldungZusatzsektion
+  ].freeze
+
+  MITGLIED_ROLES = [
+    MITGLIED_STAMMSEKTION_ROLES,
+    MITGLIED_ZUSATZSEKTION_ROLES
+  ].flatten.freeze
+
+  NEUANMELDUNG_ROLES = [
+    NEUANMELDUNG_STAMMSEKTION_ROLES,
+    NEUANMELDUNG_ZUSATZSEKTION_ROLES
+  ].flatten.freeze
+
+  MITGLIED_AND_NEUANMELDUNG_ROLES = [
+    MITGLIED_ROLES,
+    NEUANMELDUNG_ROLES
+  ].flatten.freeze
+
+  STAMMSEKTION_ROLES = [
+    MITGLIED_STAMMSEKTION_ROLES,
+    NEUANMELDUNG_STAMMSEKTION_ROLES
+  ].flatten.freeze
+
+  ZUSATZSEKTION_ROLES = [
+    MITGLIED_ZUSATZSEKTION_ROLES,
+    NEUANMELDUNG_ZUSATZSEKTION_ROLES
+  ].flatten.freeze
+
+  ### Various roles
+
+  SAC_MITARBEITER_ROLES = [
+    ::Group::Geschaeftsstelle::Mitarbeiter,
+    ::Group::Geschaeftsstelle::Admin
   ].freeze
 
   TOUR_GUIDE_ROLES = [
@@ -39,8 +58,7 @@ module SacCas
     ::Group::SektionsTourenkommission::TourenleiterOhneQualifikation
   ].freeze
 
-  MITGLIED_ROLES = (MITGLIED_HAUPTSEKTION_ROLES + MITGLIED_ZUSATZSEKTION_ROLES).freeze
-  NEUANMELDUNG_ROLES = (NEUANMELDUNG_HAUPTSEKTION_ROLES + NEUANMELDUNG_ZUSATZSEKTION_ROLES).freeze
+  ###
 
   NEWSLETTER_MAILING_LIST_INTERNAL_KEY = 'sac_newsletter'
 

app/domain/table_displays/resolver.rb
@@ -67,7 +67,7 @@ module TableDisplays
     def antrag_fuer
       if group_roles.any? { |r| SacCas::NEUANMELDUNG_ZUSATZSEKTION_ROLES.include?(r.class) }
         I18n.t('groups.sektion_secondary')
-      elsif group_roles.any? { |r| SacCas::NEUANMELDUNG_HAUPTSEKTION_ROLES.include?(r.class) }
+      elsif group_roles.any? { |r| SacCas::NEUANMELDUNG_STAMMSEKTION_ROLES.include?(r.class) }
         I18n.t('groups.sektion_primary')
       end
     end
@@ -111,7 +111,7 @@ module TableDisplays
     end
 
     def membership_roles
-      Role.where(type: SacCas::MITGLIED_HAUPTSEKTION_ROLES.collect(&:sti_name),
+      Role.where(type: SacCas::MITGLIED_STAMMSEKTION_ROLES.collect(&:sti_name),
                  person_id: @person.id)
     end
 

app/models/group/sektions_mitglieder.rb
@@ -11,7 +11,7 @@ class Group::SektionsMitglieder < ::Group
 
   ### ROLES
   class Mitglied < ::Role
-    include SacCas::Role::MitgliedHauptsektion
+    include SacCas::Role::MitgliedStammsektion
 
     self.terminatable = true
 

app/models/group/sektions_neuanmeldungen_nv.rb
@@ -11,7 +11,7 @@ class Group::SektionsNeuanmeldungenNv < ::Group
 
   ### ROLES
   class Neuanmeldung < ::Role
-    include SacCas::Role::MitgliedHauptsektion
+    include SacCas::Role::MitgliedStammsektion
     include SacCas::Role::HardDestroy
   end
 

commit 7fa8a49aaf30d4da0c50e77bd29499791bc0eefe

Fix sac_membership regression
Introduced in #689. Instead of only checking for membership in the
Stammsektion it also looked at the Zusatzsektionen.

app/domain/people/sac_membership.rb
@@ -24,7 +24,7 @@ class People::SacMembership
   # checkes for active and also approvabable (neuanmeldung) roles
   def active_or_approvable_in?(sac_section)
     @person.roles.exists?(group_id: sac_section.children,
-                          type: mitglied_and_neuanmeldung_sti_names)
+                          type: stammsektion_sti_names)
   end
 
   def anytime?
@@ -47,7 +47,7 @@ class People::SacMembership
   private
 
   def stammsektion_mitglied_sti_names = SacCas::MITGLIED_STAMMSEKTION_ROLES.map(&:sti_name)
-  def mitglied_and_neuanmeldung_sti_names = SacCas::MITGLIED_AND_NEUANMELDUNG_ROLES.map(&:sti_name)
+  def stammsektion_sti_names = SacCas::STAMMSEKTION_ROLES.map(&:sti_name)
 
   def any_future_role?
     @person.roles.future.where(convert_to: stammsektion_mitglied_sti_names).exists?