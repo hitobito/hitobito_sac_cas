0ce32162180589692f0643cfcd81fcd2a54814c0
Raphael Nestler <raphael.nestler@renuo.ch>
2024-07-02 13:52:03 +0200

    Consequent rename to backoffice

--- app/abilities/sac_cas/person_ability.rb
+++ app/abilities/sac_cas/person_ability.rb
@@ -11,18 +11,18 @@ module SacCas::PersonAbility
 
   prepended do
     on(Person) do
-      class_side(:create_households).if_sac_mitarbeiter
+      class_side(:create_households).backoffice?
       permission(:read_all_people).may(:read_all_people, :show).everybody
       permission(:layer_and_below_full)
         .may(:index_invoices, :create_membership_invoice)
-        .if_sac_mitarbeiter
+        .backoffice?
       permission(:any)
         .may(:set_sac_family_main_person)
         .if_person_is_adult_and_all_household_members_writable
     end
 
-    def if_sac_mitarbeiter
-      SacCas::SAC_MITARBEITER_ROLES.any? { |r| role_type?(r) }
+    def backoffice?
+      SacCas::SAC_BACKOFFICE_ROLES.any? { |r| role_type?(r) }
     end
 
     def if_person_is_adult_and_all_household_members_writable
