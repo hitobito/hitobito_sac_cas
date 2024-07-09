f2d4bb0907e31789f9e724cb072bdb89e3901f71
Daniel Illi <daniel@illi.zone>
2024-07-09 14:21:37 +0200

    Add dev seeds for termination reasons

---
+++ db/seeds/development/5_termination_reasons.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2024, Schweizer Alpen-Club. This file is part of
+#  hitobito_sac_cas and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sac_cas
+
+TerminationReason.seed_once(:id, id: 1, text: 'Kein Interesse mehr')
+TerminationReason.seed_once(:id, id: 2, text: 'Weggezogen')
+TerminationReason.seed_once(:id, id: 3, text: 'Verstorben')
