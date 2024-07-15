6acbd04db7cd739156cfa80d01f95e7ee503487c
Chris Hunziker <chris.hunziker@renuo.ch>
2024-07-15 07:40:46 +0000

    Update person resource

--- app/resources/sac_cas/person_resource.rb
+++ app/resources/sac_cas/person_resource.rb
@@ -9,16 +9,31 @@ module SacCas::PersonResource
   extend ActiveSupport::Concern
 
   included do
-    attribute :family_id, :string, writable: false, sortable: false, filterable: false
-    attribute :membership_number, :integer, writable: false, sortable: false, fiterable: false do
-      @object.membership_number if @object.sac_membership_anytime?
-    end
-
     extra_attribute :membership_years, :integer, sortable: true, filterable: true do
       @object.membership_years if @object.sac_membership_anytime?
     end
     on_extra_attribute :membership_years do |scope|
       scope.with_membership_years
+    end
+
+    with_options writable: false, sortable: false, filterable: false do
+      attribute :family_id, :string
+      attribute :membership_number, :integer do
+        @object.membership_number if @object.sac_membership_anytime?
+      end
+      extra_attribute :sac_remark_national_office, :string, readable: :can_read_national_office_remark?
+  
+      (1..5).each do |num|
+        extra_attribute :"sac_remark_section_#{num}", :string, readable: :can_read_section_remarks?
+      end
+    end
+
+    def can_read_national_office_remark?(person)
+      can?(:manage_national_office_remark, person)
+    end
+    
+    def can_read_section_remarks?(person)
+      can?(:manage_section_remarks, person)
     end
   end
 end
