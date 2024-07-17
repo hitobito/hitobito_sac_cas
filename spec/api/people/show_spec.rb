c5c42e08dc48ac961ec0069279d9f83b83293409
Chris Hunziker <chris.hunziker@renuo.ch>
2024-07-17 14:58:38 +0000

    Update show spec

--- spec/api/people/show_spec.rb
+++ spec/api/people/show_spec.rb
@@ -8,37 +8,36 @@
 require "spec_helper"
 
 RSpec.describe "people#show", type: :request do
-  let(:token) { service_tokens(:permitted_root_layer_token).token }
-  let(:person) { people(:admin) }
+  it_behaves_like "jsonapi authorized requests" do
+    let(:token) { service_tokens(:permitted_root_layer_token).token }
+    let(:person) { people(:admin) }
 
-  subject(:make_request) { jsonapi_get "/api/people/#{person.id}" }
+    subject(:make_request) { jsonapi_get "/api/people/#{person.id}" }
 
-  before do
-    person.update!(sac_remark_national_office: "Remark", sac_remark_section_1: "Remark")
-    sign_in(person)
-  end
+    before { person.update!(sac_remark_national_office: "Remark", sac_remark_section_1: "Remark") }
 
-  context "as employee" do
-    it "can read national office remark but not section remarks" do
-      make_request
-      expect(d.sac_remark_national_office).to eq("Remark")
-      expect(d.sac_remark_section_1).to be_blank
-    end
-  end
-
-  context "as section functionary" do
-    before do
-      person.roles.destroy_all
-      person.roles.create!(
-        group: groups(:matterhorn_funktionaere),
-        type: Group::SektionsFunktionaere::Administration.sti_name
-      )
+    context "as employee" do
+      it "can read national office remark but not section remarks" do
+        sign_in(person)
+        make_request
+        expect(d.sac_remark_national_office).to eq("Remark")
+        expect(d.sac_remark_section_1).to be_blank
+      end
     end
 
-    it "can read section remarks but not national office remark" do
-      make_request
-      expect(d.sac_remark_national_office).to be_blank
-      expect(d.sac_remark_section_1).to eq("Remark")
+    context "as section functionary" do
+      it "can read section remarks but not national office remark" do
+        person.roles.destroy_all
+        person.roles.create!(
+          group: groups(:matterhorn_funktionaere),
+          type: Group::SektionsFunktionaere::Administration.sti_name
+        )
+        sign_in(person)
+
+        make_request
+        expect(d.sac_remark_national_office).to be_blank
+        expect(d.sac_remark_section_1).to eq("Remark")
+      end
     end
   end
 end
