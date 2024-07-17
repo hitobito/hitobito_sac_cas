af1bfe6aff14918deac89bd70fcefc66ae03e476
Robin Bühler <git+r@obin.ch>
2024-07-17 11:14:38 +0200

    add index name

--- db/migrate/20240717085048_create_join_table_course_and_course_compensation_category.rb
+++ db/migrate/20240717085048_create_join_table_course_and_course_compensation_category.rb
@@ -1,7 +1,7 @@
 class CreateJoinTableCourseAndCourseCompensationCategory < ActiveRecord::Migration[6.1]
   def change
     create_join_table :courses, :course_compensation_categories do |t|
-      t.index [:course_id, :course_compensation_category_id], unique: true
+      t.index [:course_id, :course_compensation_category_id], name: "unique_index_course_and_course_compensation_category", unique: true
     end
   end
 end
