2e092f4a62eeac61269d872e22c6a21b1fc1d0a1
carlobeltrame <carlo@beltra.me>
2024-07-22 23:24:55 +0200

    Make data migration work on production data
    Refs #716

--- db/migrate/20240712120000_update_group_structure.rb
+++ db/migrate/20240712120000_update_group_structure.rb
@@ -9,9 +9,20 @@ class UpdateGroupStructure < ActiveRecord::Migration[6.1]
   def up
     return unless connection.adapter_name =~ /mysql/i
 
-    say_with_time("deleting groups of outdated types") do
-      Group.where(type: ["Group::SektionsExterneKontakte", "Group::SektionsHuettenkommission", "Group::SektionsHuette", "Group::SektionsKommission", "Group::SektionsTourenkommission"]).find_each do |group|
-        hard_destroy_group(group)
+    outdated_group_ids = Group.where(type: ["Group::SektionsExterneKontakte", "Group::SektionsHuettenkommission", "Group::SektionsHuette", "Group::SektionsKommission", "Group::SektionsTourenkommission"]).pluck(:id)
+    force_delete_role_types = ["Group::SektionsHuette::Huettenchef"]
+
+    say_with_time("deleting groups and roles of outdated types") do
+      if outdated_group_ids.present?
+        # We need to overwrite the type column, because the outdated value in it will otherwise confuse Rails
+        execute("UPDATE groups SET type='Group', name='temp' WHERE id IN (#{outdated_group_ids.join(', ')})")
+        # Some roles already have to be deleted, otherwise they'll cause problems later
+        execute("DELETE FROM roles WHERE group_id IN (#{outdated_group_ids.join(', ')})")
+      end
+      execute("DELETE FROM roles WHERE type IN ('#{force_delete_role_types.join('\', \'')}')")
+
+      outdated_group_ids.each do |id|
+        hard_destroy_group(id)
       end
     end
 
@@ -28,20 +39,16 @@ class UpdateGroupStructure < ActiveRecord::Migration[6.1]
     say_with_time("creating missing default_children of Sektion and Ortsgruppe groups") do
       Group.where(type: ["Group::Sektion", "Group::Ortsgruppe"]).find_each do |group|
         group.default_children.each do |group_type|
-          next if group.children.where(type: group_type).exists?
-          child = group_type.new(name: group_type.label)
-          child.parent = group
-          child.save!
+          next if group.children.where(type: group_type.sti_name).exists?
+          group_type.create(name: group_type.label, parent: group)
         end
       end
     end
     say_with_time("creating missing default_children of SektionsFunktionaere groups") do
       Group.where(type: "Group::SektionsFunktionaere").find_each do |group|
         group.default_children.each do |group_type|
-          next if group.children.where(type: group_type).exists?
-          child = group_type.new(name: group_type.label)
-          child.parent = group
-          child.save!
+          next if group.children.where(type: group_type.sti_name).exists?
+          group_type.create(name: group_type.label, parent: group)
         end
       end
     end
@@ -49,9 +56,9 @@ class UpdateGroupStructure < ActiveRecord::Migration[6.1]
       Group.where(type: "Group::SektionsKommissionen").find_each do |group|
         ["Group::SektionsKommissionTouren", "Group::SektionsKommissionUmweltUndKultur"].each do |group_type|
           next if group.children.where(type: group_type).exists?
-          child = group_type.new(name: group_type.label)
-          child.parent = group
-          child.save!
+          next unless Object.const_defined? group_type
+          type = Object.const_get(group_type)
+          type.create(name: type.label, parent: group)
         end
       end
     end
@@ -59,24 +66,42 @@ class UpdateGroupStructure < ActiveRecord::Migration[6.1]
       Group.where(type: "Group::SektionsTourenUndKurse").find_each do |group|
         ["Group::SektionsTourenUndKurseSommer", "Group::SektionsTourenUndKurseWinter"].each do |group_type|
           next if group.children.where(type: group_type).exists?
-          child = group_type.new(name: group_type.label)
-          child.parent = group
-          child.save!
+          next unless Object.const_defined? group_type
+          type = Object.const_get(group_type)
+          type.create(name: type.label, parent: group)
         end
+      end
+    end
+
+    say_with_time("apply the effects of some Role#after_destroy callbacks which might have been circumvented (may take up to an hour!)") do
+      Person.find_each do |person|
+        role = person.roles.first
+        next unless role
+
+        # Role#reset_contact_data_visible
+        contact_data = person.roles.collect(&:permissions).flatten.include?(:contact_data)
+        person.update_column :contact_data_visible, contact_data
+
+        # Role#reset_primary_group
+        person.update_column :primary_group_id, person.roles.order(updated_at: :desc).first.try(:group_id)
       end
     end
   end
 
-  def hard_destroy_group(group)
+  def hard_destroy_group(group_id)
     # load relations which are not declared on the group model, to destroy all orphaned later
     relations = [
       Person::AddRequest::IgnoredApprover,
       BackgroundJobLogEntry,
       SacSectionMembershipConfig,
     ]
-    related = relations.flat_map { |clazz| clazz.where(group_id: group.id).to_a }
+    related = relations.flat_map { |clazz| clazz.where(group_id: group_id).to_a }
 
-    group.really_destroy!
+    # We first have to delete all roles in these groups without callbacks, because we will get stuck
+    # trying to load some outdated group or role type in some callback otherwise
+    Role.where(group_id: group_id).delete_all
+
+    Group.find(group_id).really_destroy!
 
     related.each do |related_model|
       related_model.destroy
