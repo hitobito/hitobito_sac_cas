ed6b4e1fb02e069dba8b091ef14ac18399af823b
Mountain Star <github@mtnstar.net>
2024-07-22 15:09:42 +0200

    Implement sac import source file class

--- app/domain/sac_imports/source_file.rb
+++ app/domain/sac_imports/source_file.rb
@@ -3,13 +3,29 @@
 class SacImports::SourceFile
   AVAILABLE_SOURCES = [:NAV1, :NAV2, :NAV3, :WSO21, :WSO22].freeze
 
-  def path(source_name)
-    files = Dir.glob("#{source_name}_*.xlsx")
+  def initialize(source_name)
+    assert_available_source(source_name)
+    @source_name = source_name
+  end
+
+  def path
+    files = Dir.glob("#{source_dir}/#{@source_name}_*.xlsx")
     if files.empty?
-      puts "No files found matching the pattern"
-      exit
+      raise("No source file #{@source_name}_*.xlsx found in RAILS_CORE_ROOT/tmp/xlsx/.")
     end
 
-    Rails.root.join("tmp", "xlsx", "#{files.first}.xlsx")
+    Rails.root.join("tmp", "xlsx", "#{files.first}")
+  end
+
+  private
+
+  def source_dir
+    Rails.root.join("tmp", "xlsx")
+  end
+
+  def assert_available_source(source_name)
+    unless AVAILABLE_SOURCES.include?(source_name)
+      raise "Invalid source name: NAV42\navailable sources: #{AVAILABLE_SOURCES.map(&:to_s).join(', ')}"
+    end
   end
 end
