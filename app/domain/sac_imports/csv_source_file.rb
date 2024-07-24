2aba71739a07978ad3df2281e966028d338e3887
Mountain Star <github@mtnstar.net>
2024-07-24 14:53:42 +0200

    Refactor sac imports source file

---
+++ app/domain/sac_imports/csv_source_file.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+class SacImports::CsvSourceFile
+
+  SOURCE_HEADERS =
+    { NAV1: {},
+      NAV2: {
+        navision_id: "Mitgliedernummer",
+        household_key: "Familien-Nr.",
+        group_navision_id: "Sektion",
+        navision_membership_years: "Vereinsmitgliederjahre" },
+      NAV3: {},
+      WSO21: {},
+      WSO22: {}
+    }.freeze
+
+  AVAILABLE_SOURCES = SOURCE_HEADERS.keys.freeze
+
+  def initialize(source_name)
+    assert_available_source(source_name)
+    @source_name = source_name
+  end
+
+  def rows
+    headers = headers()
+    data = []
+    CSV.foreach(path, headers: true) do |row|
+      row = row.to_h
+      hash = {}
+      headers.keys.each do |header_key|
+        hash[header_key] = row[headers[header_key]]
+      end
+      data << hash
+    end 
+    data
+  end
+
+  private
+
+  def path
+    files = Dir.glob("#{source_dir}/#{@source_name}_*.csv")
+    if files.empty?
+      raise("No source file #{@source_name}_*.csv found in RAILS_CORE_ROOT/tmp/sac_imports_src/.")
+    end
+
+    source_dir.join("#{files.first}")
+  end
+
+  def headers
+    SOURCE_HEADERS[@source_name]
+  end
+
+  def source_dir
+    Rails.root.join("tmp", "sac_imports_src")
+  end
+
+  def assert_available_source(source_name)
+    unless AVAILABLE_SOURCES.include?(source_name)
+      raise "Invalid source name: NAV42\navailable sources: #{AVAILABLE_SOURCES.map(&:to_s).join(', ')}"
+    end
+  end
+end
---
+++ spec/domain/sac_imports/csv_source_file_spec.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2024, Schweizer Alpen-Club. This file is part of
+#  hitobito_sac_cas and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sac_cas.
+
+require "spec_helper"
+
+describe SacImports::CsvSourceFile do
+  let(:source_file) { described_class.new(@source_name) }
+
+  it "throws error if unavailable source file referenced" do
+    @source_name = :NAV42
+    expect do
+      source_file.rows
+    end.to raise_error("Invalid source name: NAV42\navailable sources: #{SacImports::CsvSourceFile::AVAILABLE_SOURCES.map(&:to_s).join(', ')}")
+
+  end
+
+  it "throws error if requested source file does not exist" do
+    @source_name = :NAV2
+    expect(Dir)
+      .to receive(:glob)
+      .with("#{Rails.root.join("tmp", "sac_imports_src")}/NAV2_*.csv")
+      .and_return([])
+
+    expect do
+      source_file.rows
+    end.to raise_error("No source file NAV2_*.csv found in RAILS_CORE_ROOT/tmp/sac_imports_src/.")
+  end
+
+  it "converts csv content to hashes with key value pairs defined by header mapping" do
+    @source_name = :NAV2
+    expect(Dir)
+      .to receive(:glob)
+      .with("#{Rails.root.join("tmp", "sac_imports_src")}/NAV2_*.csv")
+      .and_return([File.expand_path("../../../fixtures/files/sac_imports_src/NAV2_stammmitgliedschaften_2024-01-04.csv", __FILE__)])
+    allow(Dir)
+      .to receive(:glob)
+      .and_call_original
+
+    rows = source_file.rows
+    expect(rows.count).to eq(2)
+    expect(rows.first).to eq({ navision_id: "1000", household_key: nil, group_navision_id: "1500", navision_membership_years: "44" })
+    expect(rows.second).to eq({ navision_id: "600001", household_key: nil, group_navision_id: "1650", navision_membership_years: "9" })
+  end
+end
