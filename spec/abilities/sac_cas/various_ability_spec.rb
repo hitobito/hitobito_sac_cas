a6db814eb50011f3829f2c7e6d38017f17dd7425
Niklas Jäggi <jaeggi@puzzle.ch>
2024-08-05 15:09:44 +0200

    add spec to check new permissions

---
+++ spec/abilities/sac_cas/various_ability_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+# Copyright (c) 2024, Schweizer Alpen-Club. This file is part of
+# hitobito and licensed under the Affero General Public License version 3
+# or later. See the COPYING file at the top-level directory or at
+# https://github.com/hitobito/hitobito.
+
+require "spec_helper"
+
+describe VariousAbility do
+  subject { ability }
+
+  let(:ability) { Ability.new(role.person.reload) }
+
+  context "as mitarbeiter geschäftsstelle" do
+    let(:role) { Fabricate(Group::Geschaeftsstelle::Mitarbeiter.name.to_sym, group: groups(:geschaeftsstelle)) }
+
+    it "may view HitobitoLogEntry records" do
+      is_expected.to be_able_to(:index, HitobitoLogEntry)
+      is_expected.to be_able_to(:show, hitobito_log_entries(:info_mail))
+    end
+  end
+
+  context "as mitarbeiter lesend geschäftsstelle" do
+    let(:role) { Fabricate(Group::Geschaeftsstelle::MitarbeiterLesend.name.to_sym, group: groups(:geschaeftsstelle)) }
+
+    it "may view HitobitoLogEntry records" do
+      is_expected.to be_able_to(:index, HitobitoLogEntry)
+      is_expected.to be_able_to(:show, hitobito_log_entries(:info_mail))
+    end
+  end
+end
