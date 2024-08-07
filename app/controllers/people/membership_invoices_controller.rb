597814f13d725bf16244cbdb27771b7b1dc5e1d2
Niklas Jäggi <jaeggi@puzzle.ch>
2024-08-07 11:44:43 +0200

    create form and adjsut controller to create external invoice with passed params

--- app/controllers/people/membership_invoices_controller.rb
+++ app/controllers/people/membership_invoices_controller.rb
@@ -9,19 +9,43 @@ class People::MembershipInvoicesController < ApplicationController
   def create
     authorize!(:update, person)
 
-    generate_invoice
-    redirect_to group_person_path(params[:group_id], person.id)
+    errors = validate_params(params[:reference_date], params[:invoice_date], params[:send_date])
+
+    if errors.any?
+      set_flash(:alert, message: "#{errors.join(', ')}")
+      redirect_to new_group_person_membership_invoice_path(group, person)
+    else
+      generate_invoice
+      redirect_to external_invoices_group_person_path(group, person)
+    end
+  end
+
+  def new
+    authorize!(:update, member)
+
+    @group = group
   end
 
   private
 
+  def validate_params(reference_date, invoice_date, send_date)
+    [reference_date, invoice_date, send_date]
+      .zip(['Stichtag', 'Rechnungsdatum', 'Versanddatum'])
+      .map { |date, field| "#{field} muss vorhanden sein" if date.blank? }
+      .compact
+  end
+
   def generate_invoice
     handle_exceptions do
-      if invoicer.generate
-        set_flash(:success, abacus_key: invoicer.invoice.abacus_sales_order_key)
-      else
-        set_flash(:alert, message: invoicer.error_messages.join(", "))
-      end
+     ExternalInvoice::SacMembership.create!(
+      state: :draft,
+      year: Date.parse(params[:reference_date]).year,
+      issued_at: params[:invoice_date],
+      sent_at: params[:send_date],
+      person: person,
+      link: Group.find(params[:section_id]),
+     )
+     set_flash(:success)
     end
   end
 
@@ -41,12 +65,20 @@ class People::MembershipInvoicesController < ApplicationController
     Raven.capture_exception(e, options)
   end
 
-  def invoicer
-    @invoicer ||= Invoices::Abacus::MembershipInvoiceGenerator.new(person, date: date)
+  def member
+    @member ||= Invoices::SacMemberships::Member.new(person, context)
   end
 
   def person
     @person ||= context.people_with_membership_years.find(params[:person_id])
+  end
+
+  def context
+    @context ||= Invoices::SacMemberships::Context.new(date)
+  end
+
+  def group
+    @group ||= Group.find(params[:group_id])
   end
 
   def context
