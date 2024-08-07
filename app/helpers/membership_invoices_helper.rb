ccf87e02ff90082a068b8005704760d0b3710483
Niklas Jäggi <jaeggi@puzzle.ch>
2024-08-07 11:44:43 +0200

    create form and adjsut controller to create external invoice with passed params

---
+++ app/helpers/membership_invoices_helper.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2024, Schweizer Alpen-Club. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sac_cas.
+
+module MembershipInvoicesHelper
+  def invoice_possible?
+    context = Invoices::SacMemberships::Context.new(Time.zone.today)
+    member = Invoices::SacMemberships::Member.new(@person, @context)
+    memberships = member.active_memberships
+    memberships.present? && Invoices::Abacus::MembershipInvoice.new(member, memberships).invoice?
+  end
+
+  def already_member_next_year?
+    in_next_year?(@person.sac_membership.stammsektion_role.delete_on)
+  end
+
+  def currently_paying_zusatzsektionen
+    member = Invoices::SacMemberships::Member.new(@person, @context)
+    memberships = member.additional_membership_roles + member.new_additional_section_membership_roles
+      .select { |membership| member
+      .paying_person?(membership.beitragskategorie) }
+      
+    memberships.map(&:layer_group)
+  end
+
+  private
+
+  def in_next_year?(date)
+    date >= Date.new(Date.today.year + 1, 1, 1) && date <= Date.new(Date.today.year + 1, 12, 31)
+  end
+end
