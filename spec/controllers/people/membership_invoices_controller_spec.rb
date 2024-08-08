5003a8727c22c150558481833ee86e25b90f858e
Niklas Jäggi <jaeggi@puzzle.ch>
2024-08-08 08:26:16 +0200

    imrpove validation of passed params

--- spec/controllers/people/membership_invoices_controller_spec.rb
+++ spec/controllers/people/membership_invoices_controller_spec.rb
@@ -14,55 +14,68 @@ describe People::MembershipInvoicesController, type: :controller do
   before { sign_in(people(:admin)) }
 
   before do
+    Role.update_all(delete_on: Date.today.end_of_year)
     SacMembershipConfig.update_all(valid_from: 2015)
     SacSectionMembershipConfig.update_all(valid_from: 2015)
-    expect(Invoices::Abacus::Client).to receive(:new).and_return(client)
   end
 
   describe "POST create" do
-    it "sends invoice to abacus" do
+    it "creates external invoice" do
       person.update!(zip_code: 3600, town: "Thun")
 
-      expect(client).to receive(:create).with(:subject, Hash).and_return({id: 7})
-      expect(client).to receive(:create).with(:address, Hash)
-      expect(client).to receive(:create).with(:communication, Hash)
-      expect(client).to receive(:create).with(:customer, Hash)
-      expect(client).to receive(:create).with(:sales_order, Hash).and_return({sales_order_id: 19})
-      expect(client).to receive(:endpoint).with(:sales_order, Hash)
-      expect(client).to receive(:request).with(:post, String, Hash)
-
       expect do
-        post :create, params: {group_id: groups(:bluemlisalp_mitglieder).id, person_id: person.id, date: "2015-03-01"}
+        post :create, params: {group_id: groups(:bluemlisalp_mitglieder).id, person_id: person.id, reference_date: Date.today, invoice_date: Date.today, send_date: Date.today, section_id: groups(:bluemlisalp_mitglieder).id, discount: "0"}
       end.to change { ExternalInvoice.count }.by(1)
 
-      expect(response).to redirect_to(group_person_path(groups(:bluemlisalp_mitglieder).id, person.id))
+      expect(response).to redirect_to(external_invoices_group_person_path(groups(:bluemlisalp_mitglieder).id, person.id))
       expect(flash[:alert]).to be_nil
-      expect(flash[:notice]).to eq("Die Rechnung wurde erfolgreich an Abacus übermittelt. Auftrag-Nr. 19")
+      expect(flash[:notice]).to eq("Die gewünschte Rechnung wird erzeugt und an Abacus übermittelt.")
     end
 
-    it "handles failure in abacus request" do
+    it "doesnt create external invoice when params invalid" do
       person.update!(zip_code: 3600, town: "Thun")
 
-      expect(client).to receive(:create).with(:subject, Hash).and_return({id: 7})
-      expect(client).to receive(:create).with(:address, Hash)
-      expect(client).to receive(:create).with(:communication, Hash)
-      expect(client).to receive(:create).with(:customer, Hash)
-      expect(client).to receive(:create).with(:sales_order, Hash).and_raise("Something went wrong")
-
+      #no send date
       expect do
-        post :create, params: {group_id: groups(:bluemlisalp_mitglieder).id, person_id: person.id, date: "2015-03-01"}
-      end.to change { ExternalInvoice.count }.by(1)
+        post :create, params: {group_id: groups(:bluemlisalp_mitglieder).id, person_id: person.id, reference_date: Date.today, invoice_date: Date.today, section_id: groups(:bluemlisalp_mitglieder).id, discount: "0"}
+      end.to change { ExternalInvoice.count }.by(0)
 
-      expect(response).to redirect_to(group_person_path(groups(:bluemlisalp_mitglieder).id, person.id))
-      expect(flash[:alert]).to eq("Die Rechnung konnte nicht an Abacus übermittelt werden. Fehlermeldung: Something went wrong")
-    end
+      expect(response).to redirect_to(new_group_person_membership_invoice_path(groups(:bluemlisalp_mitglieder).id, person.id))
+      expect(flash[:alert]).to include("Versanddatum muss vorhanden sein")
 
-    it "cannot send abacus if address is incomplete" do
-      people(:mitglied).update!(zip_code: nil, town: nil)
-      post :create, params: {group_id: groups(:bluemlisalp_mitglieder).id, person_id: person.id, date: "2015-03-01"}
+      #no reference date and no invoice date
+      expect do
+        post :create, params: {group_id: groups(:bluemlisalp_mitglieder).id, person_id: person.id, send_date: Date.today, section_id: groups(:bluemlisalp_mitglieder).id, discount: "0"}
+      end.to change { ExternalInvoice.count }.by(0)
+      expect(response).to redirect_to(new_group_person_membership_invoice_path(groups(:bluemlisalp_mitglieder).id, person.id))
+      expect(flash[:alert]).to include("Stichtag muss vorhanden sein, Rechnungsdatum muss vorhanden sein")
 
-      expect(response).to redirect_to(group_person_path(groups(:bluemlisalp_mitglieder).id, person.id))
-      expect(flash[:alert]).to eq("Die Rechnung konnte nicht an Abacus übermittelt werden. Fehlermeldung: Ort muss ausgefüllt werden, PLZ muss ausgefüllt werden")
+      #reference date in invalid year
+      expect do
+        post :create, params: {group_id: groups(:bluemlisalp_mitglieder).id, person_id: person.id, reference_date: Date.today.next_year(5), invoice_date: Date.today, send_date: Date.today, section_id: groups(:bluemlisalp_mitglieder).id, discount: "0"}
+      end.to change { ExternalInvoice.count }.by(0)
+
+      expect(response).to redirect_to(new_group_person_membership_invoice_path(groups(:bluemlisalp_mitglieder).id, person.id))
+      expect(flash[:alert]).to include("Stichtag kann nicht an diesem datum liegen")
+
+      #set person stammsektion to be continued in next year
+      person.sac_membership.stammsektion_role.update!(delete_on: Date.today.next_year.end_of_year)
+
+      #send date cant be next year
+      expect do
+        post :create, params: {group_id: groups(:bluemlisalp_mitglieder).id, person_id: person.id, reference_date: Date.today, invoice_date: Date.today, send_date: Date.today.next_year, section_id: groups(:bluemlisalp_mitglieder).id, discount: "0"}
+      end.to change { ExternalInvoice.count }.by(0)
+
+      expect(response).to redirect_to(new_group_person_membership_invoice_path(groups(:bluemlisalp_mitglieder).id, person.id))
+      expect(flash[:alert]).to include("Versanddatum kann nicht an diesem datum liegen")
+
+      #invalid discount
+      expect do
+        post :create, params: {group_id: groups(:bluemlisalp_mitglieder).id, person_id: person.id, reference_date: Date.today, invoice_date: Date.today, send_date: Date.today, section_id: groups(:bluemlisalp_mitglieder).id, discount: "16"}
+      end.to change { ExternalInvoice.count }.by(0)
+
+      expect(response).to redirect_to(new_group_person_membership_invoice_path(groups(:bluemlisalp_mitglieder).id, person.id))
+      expect(flash[:alert]).to include("Der Rabatt muss entweder 0, 50 oder 100 sein")
     end
   end
 end
