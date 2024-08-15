833b1466084a605dce8589d65301274480db083b
megli2 <egli@puzzle.ch>
2024-08-15 16:08:56 +0200

    fix model specs again

--- spec/models/household_spec.rb
+++ spec/models/household_spec.rb
@@ -40,13 +40,15 @@ describe Household do
   end
 
   it "uses sequence for household key" do
+    # Begin with counting sequence
+    Sequence.increment!(SacCas::Person::Household::HOUSEHOLD_KEY_SEQUENCE)
+
     expect do
       household = adult.household
       household.add(child)
       household.save!
-    end.to change { sequence.current_value }.by(1)
-
-    expect(adult.reload.household_key).to eq sequence.current_value.to_s
+    end.to change { Sequence.current_value(SacCas::Person::Household::HOUSEHOLD_KEY_SEQUENCE) }.by(1)
+    expect(adult.reload.household_key).to eq Sequence.current_value(SacCas::Person::Household::HOUSEHOLD_KEY_SEQUENCE).to_s
   end
 
   describe "validations" do
