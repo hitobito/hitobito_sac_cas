658792a82d5c8dd96e4a025db012b668774829cc
Chris <76159444+hunchr@users.noreply.github.com>
2024-08-15 10:19:40 +0200

    Update custom contents (#840)
    Some texts like leader_reminder_next_week had missing required placeholders

--- db/seeds/custom_contents.rb
+++ db/seeds/custom_contents.rb
@@ -27,9 +27,12 @@ CustomContent.seed_once(:key,
   {key: Qualifications::ExpirationMailer::REMINDER_TODAY},
   {key: Qualifications::ExpirationMailer::REMINDER_NEXT_YEAR},
   {key: Qualifications::ExpirationMailer::REMINDER_YEAR_AFTER_NEXT_YEAR},
-  {key: Memberships::LeaveZusatzsektionMailer::CONFIRMATION, placeholders_required: "person-name, sektion-name, terminate-on"},
-  {key: Memberships::TerminateSacMembershipMailer::CONFIRMATION, placeholders_required: "person-name, sektion-name, terminate-on"},
-  {key: Memberships::SwitchStammsektionMailer::CONFIRMATION, placeholders_required: "person-name, group-name"})
+  {key: Memberships::LeaveZusatzsektionMailer::CONFIRMATION,
+   placeholders_required: "person-name, sektion-name, terminate-on"},
+  {key: Memberships::TerminateSacMembershipMailer::CONFIRMATION,
+   placeholders_required: "person-name, sektion-name, terminate-on"},
+  {key: Memberships::SwitchStammsektionMailer::CONFIRMATION,
+   placeholders_required: "person-name, group-name"})
 
 participation_rejected_id = CustomContent.get(Event::ParticipationMailer::CONTENT_REJECTED_PARTICIPATION).id
 summon_id = CustomContent.get(Event::ParticipationMailer::CONTENT_SUMMON).id
@@ -41,10 +44,10 @@ CustomContent::Translation.seed_once(:custom_content_id, :locale,
    locale: "de",
    label: "Anlass: E-Mail Ablehnung",
    subject: "Kursablehnung",
-   body: "Hallo {participant-name}<br/><br/>" \
-          "Sie wurden leider für den Kurs {event-name} abgelehnt.<br/><br/>" \
-          "Siehe {application-url}<br/><br/>" \
-          "Kursdetails:<br/>{event-details}<br/>"},
+   body: "Hallo {participant-name}<br><br>" \
+    "Sie wurden leider für den Kurs {event-name} abgelehnt.<br><br>" \
+    "Siehe {application-url}<br><br>" \
+    "Kursdetails:<br>{event-details}<br>"},
   {custom_content_id: participation_rejected_id,
    locale: "fr",
    label: "Événement: E-Mail de refus"},
@@ -83,72 +86,77 @@ CustomContent::Translation.seed_once(:custom_content_id, :locale,
    label: "Kurs: E-Mail Kursvorbereitungen abschliessen",
    subject: "Erinnerung Kursstart",
    body: "Hallo {recipient-name},<br><br>" \
-          "Der Kurs {event-link} findet nächste Woche statt.<br><br>" \
-          "Kursdetails:<br><br>{event-details}"},
+    "Der Kurs {event-name} (Nummer: {event-number}) findet nächste Woche statt.<br><br>" \
+    "Event-Link: {event-link}<br>" \
+    "Kursdetails:<br><br>{event-details}"},
   {custom_content_id: leader_reminder_8_weeks_id,
    locale: "de",
    label: "Kurs: E-Mail Reminder Kursleitung",
    subject: "Erinnerung Kursstart",
    body: "Hallo {recipient-name},<br><br>" \
-          "Der Kurs {event-link} findet 6 Wochen nach dem {six-weeks-before-start} statt.<br><br>" \
-          "Kursdetails:<br><br>{event-details}"},
+    "Der Kurs {event-name} (Nummer: {event-number}) findet 6 Wochen nach dem {six-weeks-before-start} statt.<br><br>" \
+    "Event-Link: {event-link}<br>" \
+    "Kursdetails:<br><br>{event-details}"},
   {custom_content_id: leader_reminder_8_weeks_id,
    locale: "fr",
    label: "Kurs: E-Mail Reminder Kursleitung",
    subject: "Rappel de début de cours",
    body: "Bonjour {recipient-name},<br><br>" \
-        "Le cours {event-link} aura lieu 6 semaines après le {six-weeks-before-start}.<br><br>" \
-        "Détails du cours:<br><br>{event-details}"},
+    "Le cours {event-name} (Nummer: {event-number}) aura lieu 6 semaines après le {six-weeks-before-start}.<br><br>" \
+    "Lien de l'événement: {event-link}<br>" \
+    "Détails du cours:<br><br>{event-details}"},
   {custom_content_id: CustomContent.get(Event::PublishedMailer::NOTICE).id,
    locale: "de",
    label: "Kurs: E-Mail Kursveröffentlichung",
    subject: "Kursveröffentlichung",
    body: "Hallo {recipient-name},<br><br>" \
-          "Der Kurs {event-link}, der 6 Wochen nach dem {six-weeks-before-start} stattfinded, wurde veröffentlicht. " \
-          "Anmeldebeginn ist der {application-opening-at}.<br><br>" \
-          "Kursdetails:<br><br>{event-details}"},
+    "Der Kurs {event-name} (Nummer: {event-number}), der 6 Wochen nach dem {six-weeks-before-start} stattfinded, " \
+    "wurde veröffentlicht. Anmeldebeginn ist der {application-opening-at}.<br><br>" \
+    "Event-Link: {event-link}<br>" \
+    "Kursdetails:<br><br>{event-details}"},
   {custom_content_id: CustomContent.get(Event::ApplicationPausedMailer::NOTICE).id,
    locale: "de",
    label: "Kurs: E-Mail Anmeldung pausiert",
    subject: "Kursanmeldung pausiert",
    body: "Lieber Kursadmin,<br><br>" \
-      "Die Anmeldung für den Kurs {event-link} wurde pausiert.<br><br>" \
-      "Kursdetails:<br><br>{event-details}"},
+    "Die Anmeldung für den Kurs {event-name} (Nummer: {event-number}) wurde pausiert.<br><br>" \
+    "Event-Link: {event-link}<br>" \
+    "Kursdetails:<br><br>{event-details}"},
   {custom_content_id: CustomContent.get(Qualifications::ExpirationMailer::REMINDER_TODAY).id,
    locale: "de",
    label: "Qualifikation: Erinnerungsmail morgen",
    subject: "Erinnerung TL-Anerkennung",
    body: "Liebe(r) Tourenleiter(in), deine TL-Anerkennung ist ab morgen sistiert. " \
-      "Du darfst keine Touren für deine Sektion mehr leiten. " \
-      "Bitte absolviere die nötigen Fortbildungstage, um wieder als aktive(r) Leiter(in) registriert sein zu können."},
+    "Du darfst keine Touren für deine Sektion mehr leiten. " \
+    "Bitte absolviere die nötigen Fortbildungstage, um wieder als aktive(r) Leiter(in) registriert sein zu können."},
   {custom_content_id: CustomContent.get(Qualifications::ExpirationMailer::REMINDER_NEXT_YEAR).id,
    locale: "de",
    label: "Qualifikation: Erinnerungsmail in 1 Jahr",
    subject: "Erinnerung TL-Anerkennung",
    body: "Liebe(r) Tourenleiter(in), per Ende Jahr läuft deine TL-Anerkennung ab. " \
-      "Du darfst keine Touren für deine Sektion mehr leiten. " \
-      "Absolviere bitte die nötigen Fortbildungstage, damit du per Ende Jahr nicht sistiert wirst."},
+    "Du darfst keine Touren für deine Sektion mehr leiten. " \
+    "Absolviere bitte die nötigen Fortbildungstage, damit du per Ende Jahr nicht sistiert wirst."},
   {custom_content_id: CustomContent.get(Qualifications::ExpirationMailer::REMINDER_YEAR_AFTER_NEXT_YEAR).id,
    locale: "de",
    label: "Qualifikation: Erinnerungsmail in 2 Jahren",
    subject: "Erinnerung TL-Anerkennung",
    body: "Liebe(r) Tourenleiter(in), in 2 Jahren läuft deine TL-Anerkennung ab. " \
-      "Plane rechtzeitig deine Fortbildungskurse in Absprache mit deinem(r) Tourenchef(in)."},
+    "Plane rechtzeitig deine Fortbildungskurse in Absprache mit deinem(r) Tourenchef(in)."},
   {custom_content_id: CustomContent.get(Memberships::LeaveZusatzsektionMailer::CONFIRMATION).id,
    locale: "de",
    label: "Mitgliedschaften: Bestätigung Austritt Zusatzsektion",
    subject: "Bestätigung Austritt Zusatzsektion",
-   body: "Hallo {person-name},<br/><br/>" \
+   body: "Hallo {person-name},<br><br>" \
     "Der Austritt aus {sektion-name} wurde per {terminate-on} vorgenommen."},
   {custom_content_id: CustomContent.get(Memberships::TerminateSacMembershipMailer::CONFIRMATION).id,
    locale: "de",
    label: "Bestätigung SAC Austritt",
    subject: "Der SAC Austritt wurde per {terminate-on} vorgenommen",
-   body: "Hallo {person-name},<br/><br/>" \
+   body: "Hallo {person-name},<br><br>" \
     "Der SAC Austritt wurde per {terminate-on} vorgenommen."},
   {custom_content_id: CustomContent.get(Memberships::SwitchStammsektionMailer::CONFIRMATION).id,
    locale: "de",
    label: "Mitgliedschaften: Bestätigung Sektionswechsel",
    subject: "Bestätigung Sektionswechsel",
-   body: "Hallo {person-name},<br/><br/>" \
+   body: "Hallo {person-name},<br><br>" \
      "Der Sektionswechsel zu {group-name} wurde vorgenommen."})
