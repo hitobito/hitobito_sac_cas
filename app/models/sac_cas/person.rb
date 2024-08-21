2583a51ddc4a276e8791a9218ecb241c506f4d8b
megli2 <egli@puzzle.ch>
2024-08-21 14:05:31 +0200

    delete person index

--- app/models/sac_cas/person.rb
+++ app/models/sac_cas/person.rb
@@ -11,6 +11,9 @@ module SacCas::Person
   CORRESPONDENCES = %w[digital print]
   DATA_QUALITIES = %w[ok info warning error]
 
+  Person::SEARCHABLE_ATTRS << :id
+  include PgSearchable
+
   included do
     Person::SAC_REMARK_NATIONAL_OFFICE = "sac_remark_national_office"
     Person::SAC_SECTION_REMARKS = %w[sac_remark_section_1 sac_remark_section_2 sac_remark_section_3
