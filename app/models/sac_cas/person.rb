1d9c5ccda8386caa71115e0b24a12ae7b8d0259a
megli2 <egli@puzzle.ch>
2024-08-21 14:18:15 +0200

    move pg searchable to included to

--- app/models/sac_cas/person.rb
+++ app/models/sac_cas/person.rb
@@ -10,10 +10,10 @@ module SacCas::Person
 
   CORRESPONDENCES = %w[digital print]
 
-  Person::SEARCHABLE_ATTRS << :id
-  include PgSearchable
-
   included do
+    Person::SEARCHABLE_ATTRS << :id
+    include PgSearchabl
+
     Person::SAC_REMARK_NATIONAL_OFFICE = "sac_remark_national_office"
     Person::SAC_SECTION_REMARKS = %w[sac_remark_section_1 sac_remark_section_2 sac_remark_section_3
       sac_remark_section_4 sac_remark_section_5]
