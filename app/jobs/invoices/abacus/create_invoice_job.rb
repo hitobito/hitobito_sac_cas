23a7b7c09766b633d3fc46f2c30fdb2e2dd35e4b
Chris <76159444+hunchr@users.noreply.github.com>
2024-08-27 15:27:27 +0000

    Update invoice job

--- app/jobs/invoices/abacus/create_invoice_job.rb
+++ app/jobs/invoices/abacus/create_invoice_job.rb
@@ -18,17 +18,23 @@ class Invoices::Abacus::CreateInvoiceJob < BaseJob
   end
 
   def perform
-    if membership_invoice.invoice?
+    if membership_invoice.invoice? && person.data_quality != "error"
       transmit_subject
       external_invoice.update!(total: membership_invoice.total)
       transmit_sales_order
-    elsif membership_invoice.memberships.none?
-      external_invoice.update!(state: :error)
-      create_error_log_entry(I18n.t(ExternalInvoice::SacMembership::NO_MEMBERSHIPS_KEY))
-    else
-      external_invoice.update!(state: :error)
-      create_error_log_entry(I18n.t(ExternalInvoice::SacMembership::NOT_POSSIBLE_KEY))
+      return
     end
+
+    external_invoice.update!(state: :error)
+    create_error_log_entry(I18n.t(
+      if person.data_quality == "error"
+        ExternalInvoice::SacMembership::DATA_QUALITY_ERROR_KEY
+      elsif membership_invoice.memberships.none?
+        ExternalInvoice::SacMembership::NO_MEMBERSHIPS_KEY
+      else
+        ExternalInvoice::SacMembership::NOT_POSSIBLE_KEY
+      end
+    ))
   end
 
   def error(job, exception)
