2890ea934fc83c6a0ddde58b642f5feb6c86b549
Raphael Nestler <raphael.nestler@renuo.ch>
2024-08-29 16:28:54 +0200

    Working Spec

--- spec/jobs/invoices/abacus/create_yearly_invoices_job_spec.rb
+++ spec/jobs/invoices/abacus/create_yearly_invoices_job_spec.rb
@@ -13,10 +13,37 @@ end
 describe Invoices::Abacus::CreateYearlyInvoicesJob do
   let(:params) { {invoice_year:, invoice_date:, send_date:, role_finish_date:} }
   let(:invoice_year) { 2024 }
-  let(:invoice_date) { Date.new(2024, 12, 31) }
-  let(:send_date) { nil }
+  let(:invoice_date) { Date.new(2025, 1, 1) }
+  let(:send_date) { Date.new(2025, 1, 2) }
   let(:role_finish_date) { nil }
   let(:subject) { described_class.new(**params) }
+
+  let(:mix_of_people) do
+    # People that shouldn't show up
+    people(:familienmitglied2).update!(abacus_subject_key: "125", data_quality: :error)
+    person_1 = create_person(role_created_at: Date.new(invoice_year, 8, 16), params: {abacus_subject_key: "126"})
+    person_2 = create_person(params: {abacus_subject_key: "127"})
+    person_2.external_invoices.create!(type: ExternalInvoice::SacMembership, year: invoice_year)
+
+    # People that should show up
+    people(:mitglied).update!(abacus_subject_key: "123")
+    people(:familienmitglied).update!(abacus_subject_key: "124")
+    # valid_person = create_person(params: {abacus_subject_key: "128"})
+    # valid_person.external_invoices.create!(type: ExternalInvoice::DummyInvoice, year: invoice_year)
+    [
+      [
+        people(:mitglied),
+        people(:familienmitglied)
+        # valid_person,
+        # create_person(params: {abacus_subject_key: "129"})
+      ],
+      [
+        people(:familienmitglied2),
+        person_1,
+        person_2
+      ]
+    ]
+  end
 
   describe "#enqueue!" do
     it "will create a job and raise if there is already one running" do
@@ -41,23 +68,7 @@ describe Invoices::Abacus::CreateYearlyInvoicesJob do
 
     context "with a wild mix of people" do
       before do
-        # People that shouldn't show up
-        people(:familienmitglied2).update!(abacus_subject_key: "125", data_quality: :error)
-        create_person(role_created_at: Date.new(invoice_year, 8, 16), params: {abacus_subject_key: "126"})
-        person = create_person(params: {abacus_subject_key: "127"})
-        person.external_invoices.create!(type: ExternalInvoice::SacMembership, year: invoice_year)
-
-        # People that should show up
-        people(:mitglied).update!(abacus_subject_key: "123")
-        people(:familienmitglied).update!(abacus_subject_key: "124")
-        valid_person = create_person(params: {abacus_subject_key: "128"})
-        valid_person.external_invoices.create!(type: ExternalInvoice::DummyInvoice, year: invoice_year)
-        @expected_people = [
-          people(:mitglied),
-          people(:familienmitglied),
-          valid_person,
-          create_person(params: {abacus_subject_key: "129"})
-        ]
+        @expected_people = mix_of_people[0]
       end
 
       it "returns the correct people" do
@@ -69,19 +80,93 @@ describe Invoices::Abacus::CreateYearlyInvoicesJob do
   describe "#perform" do
     let(:host) { "https://abacus.example.com" }
     let(:mandant) { 1234 }
+    let(:today) { Time.zone.today.strftime("%Y-%m-%d") }
+    let(:dummy_invoice) do
+      ExternalInvoice::SacMembership.create!(
+        person: people.last,
+        issued_at: today,
+        sent_at: today
+      )
+    end
+    let(:next_invoice_id) { dummy_invoice.id + 1 }
+
+    let(:abacus_client) { Invoices::Abacus::Client.new }
+    let(:sales_order_interface) { Invoices::Abacus::SalesOrderInterface.new(abacus_client) }
+    let(:expected_people) { mix_of_people[0] }
 
     before do
+      expected_people
       Invoices::Abacus::Config.instance_variable_set(:@config, {host: host, mandant: mandant}.stringify_keys)
-    end
-
-    let!(:people) do
-      10.times do |i|
-        create_person(params: {abacus_subject_key: "12#{i}"})
-      end
+      allow(Invoices::Abacus::SalesOrderInterface).to receive(:new).and_return(sales_order_interface)
+      allow(abacus_client).to receive(:token).and_return("42")
+      allow(abacus_client).to receive(:generate_batch_boundary).and_return("batch-boundary-3f8b206b-4aec-4616-bd28-c1ccbe572649")
+      stub_request(:post, "#{host}/api/entity/v1/mandants/1234/$batch")
+        .with(
+          body: batch_body_sales_orders,
+          headers: {
+            "Authorization" => "Bearer 42",
+            "Content-Type" => "multipart/mixed;boundary=batch-boundary-3f8b206b-4aec-4616-bd28-c1ccbe572649"
+          }
+        )
+        .to_return(
+          status: 202,
+          body: batch_response_sales_orders,
+          headers: {"Content-Type" => "multipart/mixed;boundary=batch-boundary-3f8b206b-4aec-4616-bd28-asdasdfasdf"}
+        )
     end
 
     it "works" do
-      expect { subject.perform }.not_to raise_error
+      p next_invoice_id
+      expect { subject.perform }
+        .to change { ExternalInvoice.count }.by(2)
     end
+  end
+
+  def batch_body_sales_orders
+    <<~HTTP
+      --batch-boundary-3f8b206b-4aec-4616-bd28-c1ccbe572649\r
+      Content-Type: application/http\r
+      Content-Transfer-Encoding: binary\r
+      \r
+      POST SalesOrders HTTP/1.1\r
+      Content-Type: application/json\r
+      Accept: application/json\r
+      \r
+      {"CustomerId":123,"OrderDate":"#{invoice_date}","DeliveryDate":"#{send_date}","TotalAmount":183.0,"DocumentCodeInvoice":"R","Language":"de","UserFields":{"UserField1":"#{next_invoice_id}","UserField2":"hitobito","UserField3":true,"UserField4":1.0,"UserField11":"600001;Hillary;Edmund;#{expected_people[0].membership_verify_token}"},"Positions":[{"PositionNumber":1,"Type":"Product","Pricing":{"PriceAfterFinding":40.0},"Quantity":{"Ordered":1,"Charged":1,"Delivered":1},"Product":{"Description":"Beitrag Zentralverband","ProductNumber":"42"},"Accounts":{},"UserFields":{"UserField1":"Beitrag Zentralverband","UserField4":"Einzelmitglied"}},{"PositionNumber":2,"Type":"Product","Pricing":{"PriceAfterFinding":20.0},"Quantity":{"Ordered":1,"Charged":1,"Delivered":1},"Product":{"Description":"Hütten Solidaritätsbeitrag","ProductNumber":"44"},"Accounts":{},"UserFields":{"UserField1":"Beitrag Zentralverband","UserField4":"Einzelmitglied"}},{"PositionNumber":3,"Type":"Product","Pricing":{"PriceAfterFinding":25.0},"Quantity":{"Ordered":1,"Charged":1,"Delivered":1},"Product":{"Description":"Alpengebühren","ProductNumber":"45"},"Accounts":{},"UserFields":{"UserField1":"Beitrag Zentralverband","UserField4":"Einzelmitglied"}},{"PositionNumber":4,"Type":"Product","Pricing":{"PriceAfterFinding":42.0},"Quantity":{"Ordered":1,"Charged":1,"Delivered":1},"Product":{"Description":"Beitrag SAC Blüemlisalp","ProductNumber":"98"},"Accounts":{},"UserFields":{"UserField1":"Beitrag SAC Blüemlisalp","UserField2":578575972,"UserField4":"Einzelmitglied"}},{"PositionNumber":5,"Type":"Product","Pricing":{"PriceAfterFinding":56.0},"Quantity":{"Ordered":1,"Charged":1,"Delivered":1},"Product":{"Description":"Beitrag SAC Matterhorn","ProductNumber":"98"},"Accounts":{},"UserFields":{"UserField1":"Beitrag SAC Matterhorn","UserField2":#{groups(:matterhorn).id},"UserField4":"Einzelmitglied"}}]}\r
+      --batch-boundary-3f8b206b-4aec-4616-bd28-c1ccbe572649\r
+      Content-Type: application/http\r
+      Content-Transfer-Encoding: binary\r
+      \r
+      POST SalesOrders HTTP/1.1\r
+      Content-Type: application/json\r
+      Accept: application/json\r
+      \r
+      {"CustomerId":124,"OrderDate":"#{invoice_date}","DeliveryDate":"2025-01-02","TotalAmount":267.0,"DocumentCodeInvoice":"R","Language":"de","UserFields":{"UserField1":"4","UserField2":"hitobito","UserField3":true,"UserField4":1.0,"UserField11":"600002;Norgay;Tenzing;#{expected_people[1].membership_verify_token}","UserField12":"600003;Norgay;Frieda;#{people(:familienmitglied2).membership_verify_token}","UserField13":"600004;Norgay;Nima;#{people(:familienmitglied_kind).membership_verify_token}"},"Positions":[{"PositionNumber":1,"Type":"Product","Pricing":{"PriceAfterFinding":50.0},"Quantity":{"Ordered":1,"Charged":1,"Delivered":1},"Product":{"Description":"Beitrag Zentralverband","ProductNumber":"42"},"Accounts":{},"UserFields":{"UserField1":"Beitrag Zentralverband","UserField4":"Familienmitglied"}},{"PositionNumber":2,"Type":"Product","Pricing":{"PriceAfterFinding":20.0},"Quantity":{"Ordered":1,"Charged":1,"Delivered":1},"Product":{"Description":"Hütten Solidaritätsbeitrag","ProductNumber":"44"},"Accounts":{},"UserFields":{"UserField1":"Beitrag Zentralverband","UserField4":"Familienmitglied"}},{"PositionNumber":3,"Type":"Product","Pricing":{"PriceAfterFinding":25.0},"Quantity":{"Ordered":1,"Charged":1,"Delivered":1},"Product":{"Description":"Alpengebühren","ProductNumber":"45"},"Accounts":{},"UserFields":{"UserField1":"Beitrag Zentralverband","UserField4":"Familienmitglied"}},{"PositionNumber":4,"Type":"Product","Pricing":{"PriceAfterFinding":84.0},"Quantity":{"Ordered":1,"Charged":1,"Delivered":1},"Product":{"Description":"Beitrag SAC Blüemlisalp","ProductNumber":"98"},"Accounts":{},"UserFields":{"UserField1":"Beitrag SAC Blüemlisalp","UserField2":#{groups(:bluemlisalp).id},"UserField4":"Familienmitglied"}},{"PositionNumber":5,"Type":"Product","Pricing":{"PriceAfterFinding":88.0},"Quantity":{"Ordered":1,"Charged":1,"Delivered":1},"Product":{"Description":"Beitrag SAC Matterhorn","ProductNumber":"98"},"Accounts":{},"UserFields":{"UserField1":"Beitrag SAC Matterhorn","UserField2":#{groups(:matterhorn).id},"UserField4":"Familienmitglied"}}]}\r
+      --batch-boundary-3f8b206b-4aec-4616-bd28-c1ccbe572649--\r
+    HTTP
+  end
+
+  def batch_response_sales_orders
+    <<~HTTP
+      --batch-boundary-3f8b206b-4aec-4616-bd28-asdasdfasdf\r
+      Content-Type: application/http\r
+      Content-Transfer-Encoding: binary\r
+      \r
+      HTTP/1.1 201 Created\r
+      Content-Type: application/json\r
+      Accept: application/json\r
+      \r
+      {"SalesOrderId":42,"SalesOrderBacklogId":0}\r
+      --batch-boundary-3f8b206b-4aec-4616-bd28-asdasdfasdf\r
+      Content-Type: application/http\r
+      Content-Transfer-Encoding: binary\r
+      \r
+      HTTP/1.1 201 Created\r
+      Content-Type: application/json\r
+      Accept: application/json\r
+      \r
+      {"SalesOrderId":45,"SalesOrderBacklogId":0}\r
+      --batch-boundary-3f8b206b-4aec-4616-bd28-asdasdfasdf--\r
+    HTTP
   end
 end
