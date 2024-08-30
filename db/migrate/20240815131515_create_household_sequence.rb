9ec4af742b2b323127840be879e2b997af693301
megli2 <egli@puzzle.ch>
2024-08-30 17:12:38 +0200

    Use getvalue method to retrieve value from query

--- db/migrate/20240815131515_create_household_sequence.rb
+++ db/migrate/20240815131515_create_household_sequence.rb
@@ -2,8 +2,8 @@
 class CreateHouseholdSequence < ActiveRecord::Migration[6.1]
   def up
     result = execute("SELECT MAX(sub.household_key::integer) FROM (SELECT household_key from people
-where (household_key IS NOT NULL OR household_key != '') AND household_key NOT LIKE 'F%') as sub") + 1
-    current_household_key = result.values[0][0].to_i
+where (household_key IS NOT NULL OR household_key != '') AND household_key NOT LIKE 'F%') as sub")
+    current_household_key = (result.getvalue(0, 0) || 0) + 1
 
     execute <<-SQL
       CREATE SEQUENCE household_sequence
