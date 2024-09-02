60cd9a235d85a93657171a9cedb6c9118fc53809
megli2 <egli@puzzle.ch>
2024-09-02 13:49:36 +0200

    adjust migration to analyse numbers with F at start as well to not miss values for highest_number in sequence start

--- db/migrate/20240815131515_create_household_sequence.rb
+++ db/migrate/20240815131515_create_household_sequence.rb
@@ -1,9 +1,13 @@
 
 class CreateHouseholdSequence < ActiveRecord::Migration[6.1]
   def up
-    result = execute("SELECT MAX(sub.household_key::integer) FROM (SELECT household_key from people
-where (household_key IS NOT NULL OR household_key != '') AND household_key NOT LIKE 'F%') as sub")
-    current_household_key = (result.getvalue(0, 0) || 0) + 1
+    highest_number_without_prefix = execute("SELECT MAX(sub.household_key::integer) FROM (SELECT household_key from people
+where (household_key IS NOT NULL OR household_key != '') AND household_key NOT LIKE 'F%') as sub").getValue(0, 0)
+
+    highest_number_with_prefix = execute("SELECT MAX(REPLACE(household_key, 'F', '')::integer) FROM people
+where household_key LIKE 'F%';").getValue(0, 0)
+
+    current_household_key = ([highest_number_without_prefix, highest_number_with_prefix].max || 0) + 1
 
     execute <<-SQL
       CREATE SEQUENCE household_sequence
