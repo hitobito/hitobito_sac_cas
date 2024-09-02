6d8739cce899adec18e1263359b7f1c5c6a486af
megli2 <egli@puzzle.ch>
2024-09-02 15:09:34 +0200

    Simplify queries in one sql statement

--- db/migrate/20240815131515_create_household_sequence.rb
+++ db/migrate/20240815131515_create_household_sequence.rb
@@ -2,7 +2,7 @@
 class CreateHouseholdSequence < ActiveRecord::Migration[6.1]
   def up
     highest_number = execute("SELECT MAX(REPLACE(household_key, 'F', '')::integer) FROM people
-where household_key LIKE 'F%';").getValue(0, 0)
+where household_key LIKE 'F%';").getvalue(0, 0)
 
     current_household_key = (highest_number || 0) + 1
 
