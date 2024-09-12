8147345e1bd18bace0f9da244aa91b39a2a7d1cf
Raphael Nestler <raphael.nestler@renuo.ch>
2024-09-12 13:52:57 +0200

    Only mock Rails.root

--- spec/domain/sac_imports/csv_source_spec.rb
+++ spec/domain/sac_imports/csv_source_spec.rb
@@ -8,7 +8,15 @@
 require "spec_helper"
 
 describe SacImports::CsvSource do
-  let(:nav2_csv_fixture) { File.expand_path(file_fixture("sac_imports_src/NAV2_stammmitgliedschaften_2024-01-04.csv")) }
+  let(:sac_imports_src) { file_fixture("sac_imports_src").expand_path }
+
+  before do
+    allow(Rails.root)
+      .to receive(:join)
+      .with("tmp", "sac_imports_src")
+      .and_return(sac_imports_src)
+  end
+
   let(:source_file) { described_class.new(@source_name) }
 
   it "throws error if unavailable source file referenced" do
@@ -22,7 +30,7 @@ describe SacImports::CsvSource do
     @source_name = :NAV2
     expect(Dir)
       .to receive(:glob)
-      .with(Rails.root.join("tmp", "sac_imports_src", "NAV2_*.csv").to_s)
+      .with(sac_imports_src.join("NAV2_*.csv").to_s)
       .and_return([])
 
     expect do
@@ -32,14 +40,6 @@ describe SacImports::CsvSource do
 
   it "converts csv content to hashes with key value pairs defined by header mapping" do
     @source_name = :NAV2
-    expect(Dir)
-      .to receive(:glob)
-      .with(Rails.root.join("tmp", "sac_imports_src", "NAV2_*.csv").to_s)
-      .and_return([nav2_csv_fixture])
-    allow(Dir)
-      .to receive(:glob)
-      .and_call_original
-
     rows = source_file.rows
     expect(rows.count).to eq(2)
     expect(rows.first).to eq({navision_name: "Montana Andreas", navision_id: "1000", household_key: nil, group_navision_id: "1500", navision_membership_years: "44"})
