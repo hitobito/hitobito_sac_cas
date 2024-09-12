96790fec9c4a2ea732822012abf60030b820c042
Chris <76159444+hunchr@users.noreply.github.com>
2024-09-12 12:20:52 +0000

    Add custom contents

---
+++ app/mailers/signups/sektion_mailer.rb
@@ -0,0 +1,85 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2024, Schweizer Alpen-Club. This file is part of
+#  hitobito_sac_cas and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sac_cas.
+
+class Signups::SektionMailer < ApplicationMailer
+  CONFIRMATION = "sektion_signup_confirmation"
+  APPROVAL_PENDING_CONFIRMATION = "sektion_signup_approval_pending_confirmation"
+
+  def confirmation(person, section)
+    send_mail(person, section, CONFIRMATION)
+  end
+
+  def approval_pending_confirmation(person, section)
+    send_mail(person, section, APPROVAL_PENDING_CONFIRMATION)
+  end
+
+  private
+
+  def send_mail(person, section, content_key)
+    @person = person
+    headers = {bcc: [SacCas::MV_EMAIL, section.email].compact}
+    locales = [person.language]
+
+    compose(person, content_key, headers, locales)
+  end
+
+  def placeholder_first_name
+    @person.first_name
+  end
+
+  def placeholder_last_name
+    @person.last_name
+  end
+
+  def placeholder_birthday
+    @person.birthday
+  end
+
+  def placeholder_email
+    @person.email
+  end
+
+  def placeholder_phone_number
+    "TODO"
+  end
+
+  def placeholder_address_care_of
+    @person.address_care_of
+  end
+
+  def placeholder_street_with_number
+    @person.address
+  end
+
+  def placeholder_postbox
+    @person.postbox
+  end
+
+  def placeholder_zip_code
+    @person.zip_code
+  end
+
+  def placeholder_section_name
+    "TODO"
+  end
+
+  def placeholder_membership_category
+    "TODO"
+  end
+
+  def placeholder_invoice_details
+    "TODO"
+  end
+
+  def placeholder_profile_url
+    group_person_url(@person.default_group_id, @person.id)
+  end
+
+  def placeholder_faq_url
+    t("global.links.faq")
+  end
+end
