22f18d3271cf5c2594d500bdf028260130b00daa
Raphael Nestler <raphael.nestler@renuo.ch>
2024-09-12 13:57:00 +0200

    Add WSO21 CSV import source

--- spec/domain/sac_imports/csv_source_spec.rb
+++ spec/domain/sac_imports/csv_source_spec.rb
@@ -45,4 +45,34 @@ describe SacImports::CsvSource do
     expect(rows.first).to eq({navision_name: "Montana Andreas", navision_id: "1000", household_key: nil, group_navision_id: "1500", navision_membership_years: "44"})
     expect(rows.second).to eq({navision_name: "Hillary Edmund", navision_id: "600001", household_key: nil, group_navision_id: "1650", navision_membership_years: "9"})
   end
+
+  it "converts WSO21 content to hashes with key value pairs defined by header mapping" do
+    @source_name = :WSO21
+    rows = source_file.rows
+    expect(rows.count).to eq(9)
+    expect(rows.first).to eq({
+      address: "Drosselweg 99b",
+      address_care_of: nil,
+      birthday: "16.11.1993",
+      country: "CH",
+      email: "example@example.com",
+      email_verified: "1",
+      first_name: "Assunta",
+      gender: "HERR",
+      language: "F",
+      last_name: "Elliot",
+      navision_id: "4200000",
+      phone: "'79 123 45 67",
+      phone_business: nil,
+      postbox: nil,
+      role_abonnent: "0",
+      role_basiskonto: "0",
+      role_gratisabonnent: "0",
+      town: "Miège",
+      wso2_legacy_password_hash: "YfVCNCik1IQnGrRNcxlsNpJl6rCKmOFb3wGISEB81l0=",
+      # ggignore: secret
+      wso2_legacy_password_salt: "xwQcHLXumdiO3O22lSX6Jw==",
+      zip_code: "3972"
+    })
+  end
 end
