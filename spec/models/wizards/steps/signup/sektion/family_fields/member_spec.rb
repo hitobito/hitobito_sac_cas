420636f65333f352fd64e7f823ef6a68dfd586b5
Andreas Maierhofer <maierhofer@puzzle.ch>
2024-09-15 16:26:09 +0200

    Mandatory email and phone for adults and email validation during section signup (#971)

--- spec/models/wizards/steps/signup/sektion/family_fields/member_spec.rb
+++ spec/models/wizards/steps/signup/sektion/family_fields/member_spec.rb
@@ -10,13 +10,15 @@ require "spec_helper"
 describe Wizards::Steps::Signup::Sektion::FamilyFields::Member do
   let(:wizard) { instance_double(Wizards::Signup::SektionWizard, requires_adult_consent?: false, requires_policy_acceptance?: false) }
   let(:family) { instance_double(Wizards::Steps::Signup::Sektion::FamilyFields, emails: %w[test@example.com]) }
-  subject(:form) { described_class.new(family, {}) }
+  subject(:member) { described_class.new }
 
   let(:required_attrs) {
     {
       first_name: "Max",
       last_name: "Muster",
-      birthday: "01.01.2000"
+      birthday: "01.01.2000",
+      email: "test2@example.com",
+      phone_number: "0791234567"
     }
   }
 
@@ -28,52 +30,68 @@ describe Wizards::Steps::Signup::Sektion::FamilyFields::Member do
 
   describe "validations" do
     it "is valid if required attrs are set" do
-      form.attributes = required_attrs
-      expect(form).to be_valid
+      member.attributes = required_attrs
+      expect(member).to be_valid
+    end
+
+    it "is valid without email or phone_number if person is not an adult" do
+      member.attributes = required_attrs.except(:email, :phone_number)
+      member.birthday = 10.years.ago.to_date
+      expect(member).to be_valid
     end
 
     it "validates presence of each required attr" do
-      expect(form).not_to be_valid
-      required_attrs.keys.each do |attr|
-        expect(form.errors.attribute_names).to include(attr)
+      expect(member).not_to be_valid
+      (required_attrs.keys - [:email, :phone_number]).each do |attr|
+        expect(member.errors.attribute_names).to include(attr)
       end
     end
 
     describe "phone_number" do
       it "must be have a valid format" do
-        form.phone_number = "test"
-        expect(form).not_to be_valid
-        expect(form.errors[:phone_number]).to eq ["ist nicht gültig"]
+        member.phone_number = "test"
+        expect(member).not_to be_valid
+        expect(member.errors[:phone_number]).to eq ["ist nicht gültig"]
       end
     end
 
     describe "email" do
-      it "must not be contained in family emails" do
-        form.email = "test@example.com"
-        expect(form).not_to be_valid
-        expect(form.errors[:email]).to eq ["ist bereits vergeben. Die E-Mail muss eindeutig sein pro Person."]
+      it "must not be taken by others" do
+        member.email = "e.hillary@hitobito.example.com"
+        expect(member).not_to be_valid
+        expect(member.errors.full_messages).to include "E-Mail ist bereits vergeben. Die E-Mail muss eindeutig sein pro Person."
       end
     end
 
     describe "birthday" do
-      it "validates user is old enough" do
-        form.attributes = required_attrs.merge(birthday: 1.year.ago.to_date)
-        expect(form).not_to be_valid
-        expect(form.errors.full_messages).to eq ["Person muss 6 Jahre oder älter sein"]
+      it "validates user is not below 6 years" do
+        member.attributes = required_attrs.merge(birthday: 1.year.ago.to_date)
+        expect(member).not_to be_valid
+        expect(member.errors.full_messages).to eq ["Person muss 6 Jahre oder älter sein"]
       end
 
-      it "validates user is old enough" do
-        form.attributes = required_attrs.merge(birthday: 20.years.ago.to_date)
-        expect(form).not_to be_valid
-        expect(form.errors.full_messages).to eq ["Geburtstag Jugendliche im Alter von 18 bis 21 Jahre können nicht\nin einer Familienmitgliedschaft aufgenommen werden.\n"]
+      it "accepts birthday on last day 17 years ago but rejects on first day 18 years ago" do
+        member.attributes = required_attrs.merge(birthday: 17.years.ago.end_of_year.to_date)
+        expect(member).to be_valid
+
+        member.attributes = required_attrs.merge(birthday: 18.years.ago.beginning_of_year.to_date)
+      end
+
+      it "rejects birthday on last day 21 years ago but accepts on first day 22 years ago" do
+        member.attributes = required_attrs.merge(birthday: 21.years.ago.end_of_year.to_date)
+        expect(member).not_to be_valid
+        expect(member.errors.full_messages).to eq ["Geburtstag Jugendliche im Alter von 18 bis 21 Jahre können nicht\nin einer Familienmitgliedschaft aufgenommen werden.\n"]
+
+        member.attributes = required_attrs.merge(birthday: 22.years.ago.beginning_of_year.to_date)
+        expect(member).to be_valid
       end
     end
   end
 
   it "attributes builds with nested phone_number attributes" do
-    form.attributes = required_attrs.merge(phone_number: "0791234567")
+    member.attributes = required_attrs.merge(phone_number: "0791234567")
 
-    expect(form.person_attributes).to eq required_attrs
+    expect(member.person_attributes).to eq required_attrs
       .except(:phone_number)
       .merge(
         birthday: Date.new(2000, 1, 1),
