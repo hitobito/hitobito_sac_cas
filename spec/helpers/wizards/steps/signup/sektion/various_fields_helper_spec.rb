287101851c45a5cd25c58a380695057490b0d2a4
Niklas Jäggi <jaeggi@puzzle.ch>
2024-09-16 10:13:29 +0200

    Fix rubocop

--- spec/helpers/wizards/steps/signup/sektion/various_fields_helper_spec.rb
+++ spec/helpers/wizards/steps/signup/sektion/various_fields_helper_spec.rb
@@ -9,19 +9,19 @@ require "spec_helper"
 
 describe Wizards::Steps::Signup::Sektion::VariousFieldsHelper do
   it "return first period info text" do
-    travel_to(Time.new(2000, 2, 1)) do
+    travel_to(Time.zone.local(2000, 2, 1)) do
       expect(entry_date_text).to eq("Bis 30. Juni ist der volle Beitrag des laufenden Jahres geschuldet. Es können noch weitere Gebühren anfallen, falls die Korrespondenzadresse im Ausland registriert ist.")
     end
   end
 
   it "should display second period info text" do
-    travel_to(Time.new(2000, 8, 1)) do
+    travel_to(Time.zone.local(2000, 8, 1)) do
       expect(entry_date_text).to eq("Bei Eintritt zwischen dem 01. Juli und dem 30. September erhältst du 50% Rabatt auf den jährlichen Beitrag des laufenden Jahres. Es können noch weitere Gebühren anfallen, falls die Korrespondenzadresse im Ausland registriert ist.")
     end
   end
 
   it "should display third period info text" do
-    travel_to(Time.new(2000, 11, 1)) do
+    travel_to(Time.zone.local(2000, 11, 1)) do
       expect(entry_date_text).to eq("Bei Eintritt ab dem 01. Oktober entfällt der jährliche Beitrag des laufenden Jahres. Es können noch weitere Gebühren anfallen, falls die Korrespondenzadresse im Ausland registriert ist.")
     end
   end
