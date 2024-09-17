a42ec3504669506ab931756951f551813d5384fe
Andreas Maierhofer <maierhofer@puzzle.ch>
2024-09-17 16:26:17 +0200

    slight refactorings, more specs

--- spec/models/wizards/steps/signup/sektion/various_fields_spec.rb
+++ spec/models/wizards/steps/signup/sektion/various_fields_spec.rb
@@ -85,20 +85,38 @@ describe Wizards::Steps::Signup::Sektion::VariousFields do
     end
 
     context "current_date_entry_reductions text" do
-      it "return first period info text" do
-        travel_to(Time.zone.local(2000, 2, 1)) do
+      it "renders first period info text on first day of period" do
+        travel_to(Time.zone.local(2000, 1, 1)) do
           expect(fields.current_date_entry_reductions).to eq("Bis 30.Juni ist der volle Beitrag des laufenden Jahres geschuldet. Es können noch weitere Gebühren anfallen, falls die Korrespondenzadresse im Ausland registriert ist.")
         end
       end
 
-      it "should display second period info text" do
-        travel_to(Time.zone.local(2000, 8, 1)) do
+      it "renders first period info text on last day of first period" do
+        travel_to(Time.zone.local(2000, 6, 30)) do
+          expect(fields.current_date_entry_reductions).to eq("Bis 30.Juni ist der volle Beitrag des laufenden Jahres geschuldet. Es können noch weitere Gebühren anfallen, falls die Korrespondenzadresse im Ausland registriert ist.")
+        end
+      end
+
+      it "renders third period info text on first day third of period" do
+        travel_to(Time.zone.local(2000, 7, 1)) do
           expect(fields.current_date_entry_reductions).to eq("Bei Eintritt zwischen dem 01.Juli und dem 30.September erhältst du 50% Rabatt auf den jährlichen Beitrag des laufenden Jahres. Es können noch weitere Gebühren anfallen, falls die Korrespondenzadresse im Ausland registriert ist.")
         end
       end
 
-      it "should display third period info text" do
-        travel_to(Time.zone.local(2000, 11, 1)) do
+      it "renders third period info text on last day of third period" do
+        travel_to(Time.zone.local(2000, 9, 30)) do
+          expect(fields.current_date_entry_reductions).to eq("Bei Eintritt zwischen dem 01.Juli und dem 30.September erhältst du 50% Rabatt auf den jährlichen Beitrag des laufenden Jahres. Es können noch weitere Gebühren anfallen, falls die Korrespondenzadresse im Ausland registriert ist.")
+        end
+      end
+
+      it "renders third period info text on first day third of period" do
+        travel_to(Time.zone.local(2000, 10, 1)) do
+          expect(fields.current_date_entry_reductions).to eq("Bei Eintritt ab dem 01.Oktober entfällt der jährliche Beitrag des laufenden Jahres. Es können noch weitere Gebühren anfallen, falls die Korrespondenzadresse im Ausland registriert ist.")
+        end
+      end
+
+      it "renders third period info text on last day of third period" do
+        travel_to(Time.zone.local(2000, 12, 31)) do
           expect(fields.current_date_entry_reductions).to eq("Bei Eintritt ab dem 01.Oktober entfällt der jährliche Beitrag des laufenden Jahres. Es können noch weitere Gebühren anfallen, falls die Korrespondenzadresse im Ausland registriert ist.")
         end
       end
