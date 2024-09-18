db050a63fc28a28011eda87717f8c519d2739c80
Andreas Maierhofer <maierhofer@puzzle.ch>
2024-09-18 11:57:19 +0200

    Spec out mailers a bit more

--- spec/mailers/signup/sektion_mailer_spec.rb
+++ spec/mailers/signup/sektion_mailer_spec.rb
@@ -9,6 +9,7 @@ require "spec_helper"
 
 describe Signup::SektionMailer do
   let(:person) { people(:mitglied) }
+  let(:body) { mail.body.to_s }
 
   context "sektion requiring approval" do
     let(:group) { groups(:bluemlisalp_neuanmeldungen_sektion) }
@@ -18,7 +19,15 @@ describe Signup::SektionMailer do
       expect(mail.to).to eq(["e.hillary@hitobito.example.com"])
       expect(mail.bcc).to include(SacCas::MV_EMAIL)
       expect(mail.subject).to eq("SAC Eintritt Bestellbestätigung")
-      expect(mail.body.to_s).to include("Sektion: SAC Blüemlisalp", "Vielen Dank")
+      expect(body).to include("Sektion: SAC Blüemlisalp", "Hallo Edmund")
+      expect(body).to include("Über die Aufnahme neuer Mitglieder entscheidet die Sektion")
+      expect(body).to include(group_person_path(person.primary_group, person))
+    end
+
+    it "uses person language to localize message" do
+      CustomContent.get(Signup::SektionMailer::APPROVAL_PENDING_CONFIRMATION).update!(locale: :fr, label: "fr", subject: "Acceptee", body: "Bonjour")
+      person.update!(language: :fr)
+      expect(mail.subject).to eq("Acceptee")
     end
   end
 
@@ -30,13 +39,22 @@ describe Signup::SektionMailer do
       expect(mail.to).to eq(["e.hillary@hitobito.example.com"])
       expect(mail.bcc).to include(SacCas::MV_EMAIL)
       expect(mail.subject).to eq("SAC Eintritt Bestellbestätigung")
-      expect(mail.body.to_s).to include(
+      expect(body).to include "Teil des grössten Bergsportverbands der Schweiz bist"
+      expect(body).to include(group_person_path(person.primary_group, person))
+      expect(body).to include(
         "Sektion: SAC Blüemlisalp",
         "Mitgliedschaftskategorie: Einzel",
         "Geburtsdatum: 01.01.2000",
         "Strasse und Nr: Ophovenerstrasse 79a",
         "Viel Spass beim SAC!"
       )
+    end
+
+    it "uses person language to localize message" do
+      CustomContent.get(Signup::SektionMailer::CONFIRMATION).update!(locale: :fr, label: "fr", subject: "Acceptee", body: "Bonjour")
+      person.update!(language: :fr)
+      expect(mail.subject).to eq("Acceptee")
+      expect(mail.body).to include("Bonjour")
     end
   end
 end
