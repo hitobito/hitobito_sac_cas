a7ca4fa1eb3030d974bafcf4ce5110c92791549b
Andreas Maierhofer <maierhofer@puzzle.ch>
2024-09-18 13:25:29 +0200

    Update spec

--- spec/mailers/invoices/sac_memberships_mailer.rb
+++ spec/mailers/invoices/sac_memberships_mailer.rb
@@ -13,5 +13,13 @@ describe Invoices::SacMembershipsMailer do
 
   it "sends confirmation email to person" do
     expect(mail.body.to_s).to include("Hallo Edmund,", "Vielen Dank für deine Zahlung, welche bei uns eingegangen ist.")
+    expect(mail.body.to_s).to include(group_person_path(person.primary_group, person))
+  end
+
+  it "considers person's language when sending" do
+    CustomContent.get(Invoices::SacMembershipsMailer::MEMBERSHIP_ACTIVATED).update(locale: :fr, label: "label", subject: "Acceptee", body: "Bonjour")
+    person.update!(language: :fr)
+    expect(mail.subject).to eq("Acceptee")
+    expect(mail.body.to_s).to include("Bonjour")
   end
 end
