e3f9395cf6d383b8e3a29e20510e66ac9564a648
Nils Rauch <rauch@puzzle.ch>
2024-10-01 14:40:44 +0200

    Generate key data sheet for course leaders (#993) Refs: hitobito/hitobito_sac_cas#993

---
+++ app/domain/export/pdf/participations/key_data_sheet/sections/logo.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2024, Schweizer Alpen-Club. This file is part of
+#  hitobito_sac_cas and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sac_cas.
+
+class Export::Pdf::Participations::KeyDataSheet::Sections::Logo < Export::Pdf::Section
+  LOGO_WIDTH = 180
+  LOGO_HEIGHT = 70
+
+  def render
+    render_logo
+  end
+
+  def render_logo
+    float do
+      image(logo_path, at: [pdf.bounds.right - LOGO_WIDTH, pdf.bounds.top], width: LOGO_WIDTH, height: LOGO_HEIGHT)
+    end
+  end
+
+  def logo_path
+    logo = "logo.png"
+
+    image_path(logo)
+  end
+
+  def image_path(name)
+    Wagons.find_wagon(__FILE__).root.join("app", "assets", "images", name)
+  end
+end
