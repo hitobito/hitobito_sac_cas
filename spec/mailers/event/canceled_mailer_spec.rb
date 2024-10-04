c86146db339242ecc52aca93da7d351892703f2d
Chris <76159444+hunchr@users.noreply.github.com>
2024-10-04 15:20:57 +0000

    Send emails when course is canceled with reason (#636)
    PR  (#1033)
    
    ---------
    
    Co-authored-by: Andi Idogawa <largo@users.noreply.github.com>

---
+++ spec/mailers/event/canceled_mailer_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2024, Schweizer Alpen-Club. This file is part of
+#  hitobito_sac_cas and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sac_cas.
+
+require "spec_helper"
+
+describe Event::CanceledMailer do
+  let(:event) { Fabricate(:sac_open_course, number: 123).tap { |c| c.update_attribute(:state, :canceled) } }
+  let(:participation) { event.participations.create!(person: people(:mitglied)) }
+
+  before { event.groups.first.update!(course_admin_email: "admin@example.com") }
+
+  [["minimum_participants", "Grund dafür ist eine zu geringe Teilnehmerzahl."],
+    ["no_leader", "Grund dafür ist der Ausfall der Kursleitung."],
+    ["weather", "Grund dafür ist das Wetterrisiko."]].each do |canceled_reason|
+    reason, reason_text = canceled_reason
+
+    context reason do
+      let(:mail) { described_class.public_send(reason, participation) }
+
+      it "sends email to course leader" do
+        expect(mail.to).to match_array(["e.hillary@hitobito.example.com"])
+        expect(mail.bcc).to match_array(["admin@example.com"])
+        expect(mail.body.to_s).to include(
+          "Hallo Edmund,",
+          "Der Kurs Eventus (Nummer: 123) wurde leider abgesagt.",
+          reason_text
+        )
+      end
+    end
+  end
+end
