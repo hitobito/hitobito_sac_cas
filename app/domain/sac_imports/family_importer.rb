0f40429c4aa6316f9b17027947845888a29b0d96
Raphael Nestler <raphael.nestler@renuo.ch>
2024-10-08 14:51:56 +0200

    Implement SacImports::FamilyImporter

---
+++ app/domain/sac_imports/family_importer.rb
@@ -0,0 +1,70 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2024, Schweizer Alpen-Club. This file is part of
+#  hitobito_sac_cas and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sac_cas.
+
+module SacImports
+  class FamilyImporter
+    REPORT_HEADERS = [
+      :navision_id,
+      :hitobito_person,
+      :household_key,
+      :errors
+    ]
+
+    attr_reader :output, :source_file, :csv_report
+
+    def initialize(output: $stdout)
+      @output = output
+      @source_file = CsvSource.new(:NAV1)
+      @csv_report = CsvReport.new(:"nav1-2_sac_families", REPORT_HEADERS)
+    end
+
+    def create
+      Role.where(type: "Group::SektionsMitglieder::Mitglied", beitragskategorie: :family).includes(:person).find_each do |role|
+        process_person(role.person)
+      end
+      @csv_report.finalize(output: @output)
+    end
+
+    private
+
+    def person_id_to_household_key(person_id)
+      @person_id_to_household_key_map ||= @source_file.rows.filter { |p| !p[:family].nil? }.to_h { |p| [p[:navision_id], p[:family]] }
+      @person_id_to_household_key_map[person_id.to_s]
+    end
+
+    def process_person(person)
+      household_key = person_id_to_household_key(person.id)
+      @output.print("#{person.id} (#{person}):")
+      assign_household(person, household_key)
+      @output.print(" ✅\n")
+    rescue StandardError => e
+      @output.print(" ❌ #{e.message}\n")
+      @csv_report.add_row({
+        navision_id: person.id,
+        hitobito_person: person.to_s,
+        household_key: household_key,
+        errors: e.message
+      })
+    end
+
+    def assign_household(person, household_key)
+      raise StandardError, "No household_key found in NAV1 data" if household_key.blank?
+      return if household_key == person.household_key # already assigned
+      raise StandardError, "Person has different household_key than the one in NAV1 data" if person.household_key && person.household_key != household_key
+
+      if (other_person = ::Person.find_by(household_key: household_key))
+        # Household key exists already, assign person to existing household
+        household = Household.new(other_person, maintain_sac_family: false)
+        household.add(person)
+        household.save!(context: :import)
+      else
+        # Household key does not exist yet, save it on the person
+        person.update!(household_key: household_key)
+      end
+    end
+  end
+end
---
+++ spec/domain/sac_imports/family_importer_spec.rb
@@ -0,0 +1,86 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2024, Schweizer Alpen-Club. This file is part of
+#  hitobito_sac_cas and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sac_cas.
+
+require "spec_helper"
+
+describe SacImports::FamilyImporter do
+  let(:output) { double(puts: nil, print: nil) }
+  let(:sac_imports_src) { file_fixture("sac_imports_src").expand_path }
+  let(:importer) { described_class.new(output: output) }
+
+  let(:report_file) { Rails.root.join("log", "sac_imports", "nav1-2_sac_families_2024-01-23-11:42.csv") }
+  let(:report_headers) { %w[navision_id hitobito_person household_key errors] }
+  let(:csv_report) { CSV.read(report_file, col_sep: ";") }
+
+  before do
+    travel_to DateTime.new(2024, 1, 23, 10, 42)
+    stub_const("SacImports::CsvSource::SOURCE_DIR", sac_imports_src)
+  end
+
+  context "with fixture data" do
+    let(:household_key_in_fixture) { "F50235" }
+
+    context "when person exists" do
+      let(:person) { Fabricate(:person, id: 4200004, sac_family_main_person: true) }
+
+      before do
+        Fabricate(Group::SektionsMitglieder::Mitglied.name,
+          group: groups(:bluemlisalp_mitglieder),
+          person: person,
+          beitragskategorie: "family")
+      end
+
+      it "changes the household key" do
+        expect { importer.create }
+          .to change { Person.find(4200004).household_key }.from(nil).to(household_key_in_fixture)
+
+        expect(File.exist?(report_file)).to be_truthy
+        expect(csv_report.size).to eq(4)
+        expect(csv_report.first).to eq(report_headers)
+        expect(csv_report.pluck(3).compact).to eq(["errors"] + ["No household_key found in NAV1 data"] * 3)
+      end
+    end
+  end
+
+  context "with mocked person_id_to_household_key" do
+    let(:main_person) { Fabricate(:person, sac_family_main_person: true) }
+    let(:second_person) { Fabricate(:person) }
+
+    let(:person_household_mapping) do
+      {
+        main_person.id => "F50235",
+        second_person.id => "F50235"
+      }
+    end
+
+    before do
+      person_household_mapping.each do |person_id, household_key|
+        expect(importer).to receive(:person_id_to_household_key).with(person_id).and_return(household_key)
+        role = Group::SektionsMitglieder::Mitglied.new(
+          group: groups(:bluemlisalp_mitglieder),
+          person_id: person_id,
+          beitragskategorie: "family",
+          created_at: Time.current,
+          delete_on: Time.current.end_of_year
+        )
+        role.save!(context: :import)
+      end
+      allow(importer).to receive(:person_id_to_household_key).and_return(nil)
+    end
+
+    it "creates the households accordingly" do
+      expect { importer.create }
+        .to change { main_person.reload.household_key }.from(nil).to("F50235")
+        .and change { second_person.reload.household_key }.from(nil).to("F50235")
+
+      expect(File.exist?(report_file)).to be_truthy
+      expect(csv_report.size).to eq(4)
+      expect(csv_report.first).to eq(report_headers)
+      expect(csv_report.pluck(3).compact).to eq(["errors"] + ["No household_key found in NAV1 data"] * 3)
+    end
+  end
+end
