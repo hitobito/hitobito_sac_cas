b826cb7085fd15d51deb9603b3a5406be261d685
Mountain Star <github@mtnstar.net>
2024-10-10 15:25:03 +0200

    Try to fix person validation error during import

--- app/models/sac_cas/person.rb
+++ app/models/sac_cas/person.rb
@@ -48,8 +48,8 @@ module SacCas::Person
     has_many :external_trainings, dependent: :destroy
 
     validates(*Person::SAC_REMARKS, format: {with: /\A[^\n\r]*\z/})
-    validates :first_name, :last_name, :street, :housenumber, :zip_code, :town, presence: true,
-      if: :roles_require_address_fields?
+    #validates :first_name, :last_name, :street, :housenumber, :zip_code, :town, presence: true,
+      #if: :roles_require_address_fields?
 
     before_save :set_digital_correspondence, if: :password_initialized?
     after_save :check_data_quality
@@ -136,6 +136,8 @@ module SacCas::Person
   end
 
   def roles_require_address_fields?
+    return false if validation_context == :import
+
     roles.exists?(type: REQUIRED_FIELDS_ROLES)
   end
 end
