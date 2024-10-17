026e5855c203acfa9f7e17d513c44709aba00c9d
Chris <76159444+hunchr@users.noreply.github.com>
2024-10-17 09:33:59 +0200

    Add Event::Level API endpoint

--- app/resources/event/level_resource.rb
+++ app/resources/event/level_resource.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
-#
 # Copyright (c) 2024, Schweizer Alpen-Club. This file is part of
 # hitobito_sac_cas and licensed under the Affero General Public License version 3
 # or later. See the COPYING file at the top-level directory or at
-# https://github.com/hitobito/hitobito
+# https://github.com/hitobito/hitobito_sac_cas.
 
 class Event::LevelResource < ApplicationResource
+  primary_endpoint "event_levels", %i[index show]
+
   self.type = "event_levels"
 
   with_options writable: false do
