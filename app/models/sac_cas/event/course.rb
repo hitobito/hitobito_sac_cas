999854035a44fa8b01869ce74f1edc8ccc3afe06
Niklas Jäggi <jaeggi@puzzle.ch>
2024-10-21 10:46:01 +0200

    Add new roles

--- app/models/sac_cas/event/course.rb
+++ app/models/sac_cas/event/course.rb
@@ -161,6 +161,20 @@ module SacCas::Event::Course
 
     self.countable_participation_states = %w[unconfirmed applied assigned summoned attended absent]
 
+    [
+      ::Event::Role::Leader,
+      ::Event::Role::AssistantLeader
+    ].each do |type|
+      disable_role_type(type)
+    end
+
+    [
+      ::Event::Course::Role::Leader,
+      ::Event::Course::Role::AssistantLeader
+    ].each do |type|
+      register_role_type(type)
+    end
+
     belongs_to :cost_center, optional: true
     belongs_to :cost_unit, optional: true
     validates :number, presence: true, uniqueness: {if: :number}
