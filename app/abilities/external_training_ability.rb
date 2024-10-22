abd0fdba973863bb5abefc201c800fd0f63746d0
Niklas Jäggi <jaeggi@puzzle.ch>
2024-10-22 14:38:55 +0200

    Adjust ExternalTraining ability

--- app/abilities/external_training_ability.rb
+++ app/abilities/external_training_ability.rb
@@ -10,13 +10,41 @@ class ExternalTrainingAbility < AbilityDsl::Base
 
   on(ExternalTraining) do
     permission(:layer_full).may(:create, :destroy).in_same_layer
-    permission(:layer_and_below_full).may(:create, :destroy).in_same_layer_or_below
+    permission(:layer_and_below_full).may(:create, :destroy).in_same_layer_or_below_if_section_may_create
 
-    permission(:group_full).may(:create, :destroy).in_same_group
-    permission(:group_and_below_full).may(:create, :destroy).in_same_group_or_below
+    permission(:group_full).may(:create, :destroy).in_same_group_if_section_may_create
+    permission(:group_and_below_full).may(:create, :destroy).in_same_group_or_below_if_section_may_create
   end
 
   def person
     subject.person
+  end
+
+  def in_same_layer_or_below_unless_sektions_mitgliederverwaltung
+    without_role(Group::SektionsFunktionaere::Mitgliederverwaltung) do
+      in_same_layer_or_below
+    end
+  end
+
+  def in_same_layer_if_section_may_create
+    in_same_layer && permisson_on_root_or_section_may_create(:layer_full)
+  end
+
+  def in_same_layer_or_below_unless_sektions_mitgliederverwaltung_if_section_may_create
+    in_same_layer_or_below_unless_sektions_mitgliederverwaltung && permisson_on_root_or_section_may_create(:layer_and_below_full)
+  end
+
+  def in_same_group_if_section_may_create
+    in_same_group && permisson_on_root_or_section_may_create(:group_full)
+  end
+
+  def in_same_group_or_below_if_section_may_create
+    in_same_group_or_below && permisson_on_root_or_section_may_create(:group_and_below_full)
+  end
+
+  def permisson_on_root_or_section_may_create(permission)
+    permitted_groups = user.groups_with_permission(permission)
+    user_context.layer_ids(permitted_groups).include?(Group.root.id) ||
+      subject.event_kind.section_may_create?
   end
 end
