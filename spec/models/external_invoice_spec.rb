8b5e103dbc22e55c1ab8e3c65e38c62e4a15de04
Andi Idogawa <Largo@users.noreply.github.com>
2024-10-30 17:42:15 +0100

    Disallow deleting participation if external_invoice exists (#1165) (#1184)
    * Disallow deleting participation if external_invoice exists (#1165)
    
    * Only create external_invoice when needed in spec
    
    ---------
    
    Co-authored-by: Raphael Nestler <raphael.nestler@renuo.ch>

---
+++ spec/models/external_invoice_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2024, Schweizer Alpen-Club. This file is part of
+#  hitobito_sac_cas and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sac_cas.
+
+require "spec_helper"
+
+describe ExternalInvoice do
+  describe "#destroy" do
+    let!(:participation) { Fabricate(:event_participation) }
+
+    context "with associated external_invoice" do
+      before { Fabricate(:external_invoice, link: participation) }
+
+      it "is prevented" do
+        expect { participation.destroy }.not_to change { Event::Participation.count }
+        expect(participation.errors.full_messages[0]).to eq "Datensatz kann nicht gelöscht werden, " \
+          "da abhängige Rechnungen existieren."
+      end
+    end
+
+    context "without associated external_invoice" do
+      it "succeeds" do
+        expect { participation.destroy }.to change { Event::Participation.count }.by(-1)
+      end
+    end
+  end
+end
