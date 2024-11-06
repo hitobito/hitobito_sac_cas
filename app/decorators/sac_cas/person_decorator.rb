23b044917dcf4346bbba10f04627fc22c33b8b4a
Pascal Zumkehr <zumkehr@puzzle.ch>
2024-11-06 16:56:16 +0100

    Overwrite person decorator full_label core method directly

--- app/decorators/sac_cas/person_decorator.rb
+++ app/decorators/sac_cas/person_decorator.rb
@@ -12,17 +12,17 @@ module SacCas::PersonDecorator
     {wso2_legacy_password: "user-check"}.merge(super)
   end
 
-  def as_typeahead
-    {id: id, label: h.h(full_label_with_changed_suffix)}
-  end
-
   def roles_for_oauth
     object.roles.includes(group: :layer_group).collect(&:decorate).collect(&:for_oauth)
   end
 
-  def as_quicksearch
-    super.tap do |data|
-      data[:label] = h.h(full_label_with_changed_suffix)
+  def full_label
+    suffix = [model.birthday&.year, model.id].compact.join("; ")
+
+    to_s.tap do |label|
+      label << ", #{town}" if town?
+      label << " (#{full_name})" if company? && full_name.present?
+      label << " (#{suffix})"
     end
   end
 
@@ -33,16 +33,6 @@ module SacCas::PersonDecorator
       super.merge(class: "text-warning")
     else
       super
-    end
-  end
-
-  def full_label_with_changed_suffix
-    suffix = [model.birthday&.year, model.id].compact.join("; ")
-
-    to_s.tap do |label|
-      label << ", #{town}" if town?
-      label << " (#{full_name})" if company? && full_name.present?
-      label << " (#{suffix})"
     end
   end
 end
