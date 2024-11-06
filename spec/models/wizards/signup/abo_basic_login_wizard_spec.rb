a9d8e334440486a9d9b7e93566a09316838d815a
Niklas Jäggi <jaeggi@puzzle.ch>
2024-11-06 14:26:28 +0100

    Fix abo wizard specs

--- spec/models/wizards/signup/abo_basic_login_wizard_spec.rb
+++ spec/models/wizards/signup/abo_basic_login_wizard_spec.rb
@@ -26,6 +26,7 @@ describe Wizards::Signup::AboBasicLoginWizard do
         email: "max.muster@example.com"
       },
       person_fields: {
+        gender: "m",
         first_name: "Max",
         last_name: "Muster",
         address_care_of: "c/o Musterleute",
