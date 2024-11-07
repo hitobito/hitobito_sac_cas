50059d95f2fc2f70fd944b76abf4a588584de27c
Niklas Jäggi <jaeggi@puzzle.ch>
2024-11-07 09:14:14 +0100

    Check validation in controller and replace turboframe form to display error messages

--- app/controllers/event/courses/leader_settlement_pdfs_controller.rb
+++ app/controllers/event/courses/leader_settlement_pdfs_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 #  Copyright (c) 2024, Schweizer Alpen-Club. This file is part of
 #  hitobito_sac_cas and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
@@ -7,7 +5,37 @@
 
 class Event::Courses::LeaderSettlementPdfsController < ApplicationController
   def create
+    authorize!(:create, Event::Courses::LeaderSettlementInvoice.new) # TODO
+    @group = group
+    @event = event
+    assign_attributes
 
-    
+    if invoice_form.valid?
+      # TODO
+    else
+      render turbo_stream: turbo_stream.replace(
+        "leader_settlement_invoice_form",
+        partial: "event/participations/popover_create_course_leader_invoice",
+        locals: { invoice_form: invoice_form }
+      ), status: :unprocessable_entity
+    end
   end
+
+  private
+
+  def assign_attributes
+    invoice_form.attributes = invoice_form_params
+  end
+
+  def invoice_form_params
+    params
+      .require(:event_courses_leader_settlement_invoice)
+      .permit(:actual_days, :iban).merge(course: event)
+  end
+
+  def invoice_form = @invoice_form ||= Event::Courses::LeaderSettlementInvoice.new
+
+  def group = @group ||= Group.find(params[:group_id])
+
+  def event = @event ||= Event::Course.find(params[:event_id])
 end
