eaa6b0825576b7f934a805fc74fa4ef9b3f29c60
Niklas Jäggi <jaeggi@puzzle.ch>
2024-11-27 15:36:55 +0100

    Adjust dropdown labels and check for course state to display mail dispatch dropdown

--- app/helpers/dropdown/events/courses/mail_dispatch.rb
+++ app/helpers/dropdown/events/courses/mail_dispatch.rb
@@ -22,8 +22,17 @@ module Dropdown::Events::Courses
     private
 
     def init_items
-      add_item(translate(".finish_preparation"), template.group_event_mail_dispatch_path(group, course), method: :post, "data-confirm": translate(".confirmation"))
-      add_item(translate(".survey"), template.group_event_mail_dispatch_path(group, course), method: :post, "data-confirm": translate(".confirmation"))
+      add_mail_item(:leader_reminder) if course.state == "ready"
+      add_mail_item(:survey)
+    end
+
+    def add_mail_item(mail_type)
+      add_item(
+        translate(".#{mail_type}"),
+        template.group_event_mail_dispatch_path(group, course, mail_type: mail_type),
+        method: :post,
+        "data-confirm": translate(".confirmation")
+      )
     end
   end
 end
