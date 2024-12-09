56104b0b72a377be47ccdef9c8b230b825b8f001
Niklas Jäggi <jaeggi@puzzle.ch>
2024-12-09 17:48:24 +0100

    Add section offerings to api

--- app/resources/sac_cas/group_resource.rb
+++ app/resources/sac_cas/group_resource.rb
@@ -10,6 +10,10 @@ module SacCas::GroupResource
 
   included do
     with_options writable: false do
+      attribute :offerings, :array_of_strings do
+        @object.section_offerings.map(&:title)
+      end
+      
       attribute :navision_id, :integer
 
       Group.subclasses.flat_map(&:mounted_attr_names).uniq.each do |attr|
