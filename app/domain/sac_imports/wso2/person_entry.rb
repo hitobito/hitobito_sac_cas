hitobito/hitobito_sac_cas

commit 99054be0afe349b1a986d3a0207077925af65412

Use new um_id column on person instead of tag

app/domain/sac_imports/wso2/person_entry.rb
@@ -95,7 +95,7 @@ module SacImports::Wso2
       else
         person.correspondence = "print" unless person.confirmed_at?
       end
-      person.tag_list << um_id_tag
+      person.um_id = um_id_tag
     end
 
     def assign_existing_person_attributes
@@ -218,7 +218,7 @@ module SacImports::Wso2
     def find_person
       person ||= Person.find_by(id: navision_id) if navision_id.present?
       person ||= Person.find_by(email: email) if email.present?
-      person || Person.tagged_with(um_id_tag).first
+      person || Person.where(um_id: um_id).first
     end
 
     def warn(message)

spec/domain/sac_imports/wso2/person_entry_spec.rb
@@ -173,7 +173,7 @@ describe SacImports::Wso2::PersonEntry do
       end
 
       it "finds the person by tag" do
-        people(:mitglied).update!(tag_list: "UM-ID-42")
+        people(:mitglied).update!(um_id: "UM-ID-42")
         expect(entry.person).to eq(people(:mitglied))
       end
 
@@ -208,8 +208,8 @@ describe SacImports::Wso2::PersonEntry do
         end
 
         it "assigns tag" do
-          expect(people(:mitglied).tag_list).not_to include("UM-ID-42")
-          expect(entry.person.tag_list).to include("UM-ID-42")
+          expect(people(:mitglied).um_id).not_to eq("UM-ID-42")
+          expect(entry.person.um_id).to eq "UM-ID-42"
         end
 
         describe "email" do
@@ -265,7 +265,7 @@ describe SacImports::Wso2::PersonEntry do
         end
 
         it "assigns tag" do
-          expect(entry.person.tag_list).to include("UM-ID-42")
+          expect(entry.person.um_id).to eq "UM-ID-42"
         end
 
         describe "email" do

db/migrate/20241217123924_add_um_id_to_people.rb
@@ -1,0 +1,13 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2024, Schweizer Alpen-Club. This file is part of
+#  hitobito_sac_cas and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
+class AddUmIdToPeople < ActiveRecord::Migration[7.0]
+  def change
+    add_column(:people, :um_id, :string)
+    add_index(:people, :um_id, unique: true)
+  end
+end

commit aafb698043c2edf108cd1ae977dbf14f9a655921

Fix SacImports::Wso2::PersonEntry
Fixes regression introduced by 99054be0afe349b1a986d3a0207077925af65412

app/domain/sac_imports/wso2/person_entry.rb
@@ -71,7 +71,7 @@ module SacImports::Wso2
       (errors.full_messages + [warning]).join(", ")
     end
 
-    def um_id_tag ="UM-ID-#{row.um_id}"
+    def um_id ="UM-ID-#{row.um_id}"
 
     def gender
       GENDERS[row.gender&.to_sym]
@@ -95,7 +95,7 @@ module SacImports::Wso2
       else
         person.correspondence = "print" unless person.confirmed_at?
       end
-      person.um_id = um_id_tag
+      person.um_id = um_id
     end
 
     def assign_existing_person_attributes

spec/domain/sac_imports/wso2/person_entry_spec.rb
@@ -44,10 +44,10 @@ describe SacImports::Wso2::PersonEntry do
 
   subject(:entry) { described_class.new(data, basic_login_group, abo_group, existing_emails) }
 
-  describe "#um_id_tag" do
+  describe "#um_id" do
     it "includes the um_id" do
       expect(row[:um_id]).to be_present
-      expect(entry.um_id_tag).to eq("UM-ID-#{row[:um_id]}")
+      expect(entry.um_id).to eq("UM-ID-#{row[:um_id]}")
     end
   end
 