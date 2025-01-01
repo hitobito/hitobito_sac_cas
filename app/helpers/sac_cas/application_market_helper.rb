e6a7072ddbeb01c8d0101c688ea01e6acfc26e83
Andreas Maierhofer <maierhofer@puzzle.ch>
2025-01-01 15:30:08 +0100

    Adjust call to helper

--- app/helpers/sac_cas/application_market_helper.rb
+++ app/helpers/sac_cas/application_market_helper.rb
@@ -6,17 +6,17 @@
 #  https://github.com/hitobito/hitobito_sac_cas
 
 module SacCas::ApplicationMarketHelper
-  def application_market_application_link(p)
+  def application_market_application_link(group, event, participation)
     link_to(icon("arrow-left"),
-      participant_group_event_application_market_path(@group, @event, p), # rubocop:disable Rails/HelperInstanceVariable
+      participant_group_event_application_market_path(group, event, participation),
       remote: true,
       method: :put,
       data: {confirm: t(".add_participant_confirm")})
   end
 
-  def application_market_participant_link(p)
+  def application_market_participant_link(group, event, participation)
     link_to(icon("arrow-right"),
-      participant_group_event_application_market_path(@group, @event, p), # rubocop:disable Rails/HelperInstanceVariable
+      participant_group_event_application_market_path(group, event, participation),
       remote: true,
       method: :delete,
       data: {confirm: t(".remove_participant_confirm")})
