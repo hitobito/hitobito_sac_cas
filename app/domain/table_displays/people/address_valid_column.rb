c1c38abaf0e45ba42bebc3af9fff03cf8ad1c822
Niklas Jäggi <jaeggi@puzzle.ch>
2025-01-16 08:58:14 +0100

    Add table display class for address valid column

---
+++ app/domain/table_displays/people/address_valid_column.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2025, Schweizer Alpen-Club. This file is part of
+#  hitobito_sac_cas and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sac_cas
+
+module TableDisplays::People
+  class AddressValidColumn < TableDisplays::Column
+    def required_model_attrs(attr)
+      []
+    end
+
+    def render(attr)
+      super do |person|
+        template.f(ActsAsTaggableOn::Tagging.joins(:tag)
+                                            .where(taggable: person, tags: {name: PersonTags::Validation::ADDRESS_INVALID})
+                                            .none?)
+      end
+    end
+
+    def required_permission(attr)
+      :show
+    end
+  end
+end
