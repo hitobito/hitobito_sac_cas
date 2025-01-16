a9ef2684c922583ab558cb2b8dbce066691a96a2
Daniel Illi <daniel@illi.zone>
2025-01-17 00:22:51 +0100

    Extend feature spec for people list remark columns

--- spec/features/people/list_spec.rb
+++ spec/features/people/list_spec.rb
@@ -74,6 +74,10 @@ describe "people list page", :js do
       group: groups(:matterhorn_funktionaere),
       type: Group::SektionsFunktionaere::Administration.sti_name
     )
+    person.update(
+      sac_remark_national_office: "Bemerkung für Geschäftsstelle",
+      sac_remark_section_1: "Bemerkung 1 für Sektion"
+    )
 
     visit group_people_path(group_id: person.groups.first.id)
     click_link("Spalten")
@@ -82,6 +86,14 @@ describe "people list page", :js do
       expect(page).to have_text("Bemerkungen Geschäftsstelle")
       expect(page).to have_text("Bemerkungen Sektion 1")
     end
+
+    check("Bemerkungen Sektion 1")
+    expect(page).to have_css("td[data-attribute-name=sac_remark_section_1]", text: "Bemerkung 1 für Sektion")
+
+    check("Bemerkungen Geschäftsstelle")
+    expect(page).to have_css("td[data-attribute-name=sac_remark_national_office]", text: "fehlende Berechtigung")
+
+    expect(page).to have_no_text("Bemerkung für Geschäftsstelle")
   end
 
   it "allows showing the data_quality column" do
