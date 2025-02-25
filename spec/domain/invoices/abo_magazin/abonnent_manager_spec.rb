10a2b52a5098ebd24d978257d6675e9f6034ef95
Niklas Jäggi <jaeggi@puzzle.ch>
2025-02-25 10:08:52 +0100

    Create abo roles on invoice payment (#1562)

--- spec/domain/invoices/abo_magazin/abonnent_manager_spec.rb
+++ spec/domain/invoices/abo_magazin/abonnent_manager_spec.rb
@@ -12,9 +12,9 @@ describe Invoices::AboMagazin::AbonnentManager do
   let(:abonnent_person) { people(:abonnent) }
   let(:die_alpen) { groups(:abo_die_alpen) }
 
-  context "person is abonnent already" do
-    subject { described_class.new(abonnent_person, die_alpen) }
+  subject { described_class.new(abonnent_person, die_alpen) }
 
+  context "person is abonnent already" do
     it "adds one year to current end_on" do
       end_on_before = abonnent.end_on
 
@@ -22,11 +22,26 @@ describe Invoices::AboMagazin::AbonnentManager do
 
       expect(abonnent.reload.end_on).to eq(end_on_before + 1.year)
     end
+
+    it "only extends role from linked grouped of invoice" do
+      die_alpen_fr = die_alpen.dup
+      die_alpen_fr.save!
+      Fabricate(:role, type: Group::AboMagazin::Abonnent.sti_name,
+        group: die_alpen_fr,
+        person: abonnent_person,
+        start_on: 2.days.ago,
+        end_on: 30.days.from_now)
+
+      end_on_before = abonnent.end_on
+      subject.update_abonnent_status
+      expect(abonnent.reload.end_on).to eq(end_on_before + 1.year)
+
+      # expect role in different group not to have updated
+      expect(abonnent_person.roles.second.end_on).to eq(30.days.from_now.to_date)
+    end
   end
 
   context "person has expired abonnent role" do
-    subject { described_class.new(abonnent_person, die_alpen) }
-
     it "does nothing if previous role expired over a year ago" do
       abonnent.update_column(:end_on, 2.years.ago)
       end_on_before = abonnent.end_on
@@ -44,6 +59,81 @@ describe Invoices::AboMagazin::AbonnentManager do
       expect(new_role.start_on).to eq(Time.zone.today)
       expect(new_role.person).to eq(abonnent.person)
       expect(new_role.group).to eq(abonnent.group)
+    end
+  end
+
+  context "person has neuanmeldungs role" do
+    before do
+      abonnent.destroy!
+      Fabricate(:role, type: Group::AboMagazin::Neuanmeldung.sti_name,
+        group: die_alpen,
+        person: abonnent_person,
+        start_on: 2.days.ago,
+        end_on: 30.days.from_now)
+    end
+
+    it "creates new role starting today ending one year after previous role" do
+      old_role = abonnent_person.roles.first
+      subject.update_abonnent_status
+      new_role = abonnent_person.roles.first
+
+      expect(old_role.reload.end_on).to eq(Time.zone.yesterday)
+
+      expect(new_role.end_on).to eq(old_role.start_on + 1.year)
+      expect(new_role.start_on).to eq(old_role.start_on)
+      expect(new_role.person).to eq(old_role.person)
+      expect(new_role.group).to eq(old_role.group)
+    end
+
+    it "creates no new role if new roles start_on would be after end_on" do
+      abonnent_person.roles.first.update_columns(start_on: 5.years.ago, end_on: 4.years.ago)
+      subject.update_abonnent_status
+      expect(abonnent_person.roles).to be_empty
+    end
+  end
+
+  context "person has expired neuanmeldungs role" do
+    before do
+      abonnent.destroy!
+      Fabricate(:role, type: Group::AboMagazin::Neuanmeldung.sti_name,
+        group: die_alpen,
+        person: abonnent_person,
+        start_on: 60.days.ago,
+        end_on: 30.days.ago)
+    end
+
+    it "creates new role starting today ending one year after previous role" do
+      old_role = abonnent_person.roles.with_inactive.first
+      subject.update_abonnent_status
+      new_role = abonnent_person.roles.first
+
+      expect(new_role.end_on).to eq(old_role.end_on + 1.year)
+      expect(new_role.start_on).to eq(Time.zone.today)
+      expect(new_role.person).to eq(old_role.person)
+      expect(new_role.group).to eq(old_role.group)
+    end
+  end
+
+  context "is abonnent already and requests neuanmeldung in different magazin" do
+    let(:die_alpen_fr) { die_alpen.dup }
+
+    subject { described_class.new(abonnent_person, die_alpen_fr) }
+
+    before do
+      die_alpen_fr.save!
+      Fabricate(:role, type: Group::AboMagazin::Neuanmeldung.sti_name,
+        group: die_alpen_fr,
+        person: abonnent_person,
+        start_on: 2.days.ago,
+        end_on: 30.days.from_now)
+    end
+
+    it "still creates new abo role when abo role in different group already exists" do
+      subject.update_abonnent_status
+      expect(abonnent_person.roles.count).to eq(2)
+      expect(abonnent_person.roles.first.group).to eq die_alpen
+      expect(abonnent_person.roles.second.group).to eq die_alpen_fr
+      expect(abonnent_person.roles.map(&:type).all?(Group::AboMagazin::Abonnent.sti_name)).to be_truthy
     end
   end
 end
