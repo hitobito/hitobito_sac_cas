6b19f7145373d410d86f2894ef55561f754e7f01
Niklas Jäggi <jaeggi@puzzle.ch>
2025-02-27 16:32:01 +0100

    Add setting to disable abo magazin invocing (#1694)

--- spec/jobs/invoices/abacus/create_yearly_abo_alpen_invoices_job_spec.rb
+++ spec/jobs/invoices/abacus/create_yearly_abo_alpen_invoices_job_spec.rb
@@ -61,6 +61,15 @@ describe Invoices::Abacus::CreateYearlyAboAlpenInvoicesJob do
     end
   end
 
+  describe "automatic_invoice_disabled true" do
+    it "does nothing" do
+      allow(Settings.invoicing.abo_magazin).to receive(:automatic_invoice_disabled).and_return(true)
+
+      expect(subject).not_to receive(:process_invoices)
+      subject.perform
+    end
+  end
+
   describe "#active_abonnenten" do
     it "includes every abonnent role where role end in next 62 days" do
       expect(subject.send(:active_abonnenten)).to match_array [abonnent_role, abonnent_role_2, abonnent_role_3, abonnent_role_4, abonnent_role_5, abonnent_role_6]
--- spec/models/wizards/signup/abo_magazin_wizard_spec.rb
+++ spec/models/wizards/signup/abo_magazin_wizard_spec.rb
@@ -145,6 +145,15 @@ describe Wizards::Signup::AboMagazinWizard do
       expect { wizard.save! }.not_to change { Person.count }
       expect(people(:admin).roles.last.type).to eq Group::AboMagazin::Neuanmeldung.sti_name
     end
+
+    context "automatic_invoice_disabled is true" do
+      it "does nothing" do
+        allow(Settings.invoicing.abo_magazin).to receive(:automatic_invoice_disabled).and_return(true)
+        
+        expect(wizard).not_to receive(:generate_invoice)
+        wizard.save!
+      end
+    end
   end
 
   describe "#calculate_costs" do
