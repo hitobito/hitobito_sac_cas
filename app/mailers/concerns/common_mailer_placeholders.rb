2e43e53f4a15259e89440b4367e5caf82f5b604a
Andreas Maierhofer <a.maierhofer@gmail.com>
2025-03-12 16:49:30 +0100

    Add additional placeholders to membership actived mailer (#1706)

--- app/mailers/concerns/common_mailer_placeholders.rb
+++ app/mailers/concerns/common_mailer_placeholders.rb
@@ -57,4 +57,31 @@ module CommonMailerPlaceholders
   def placeholder_faq_url
     t("global.links.faq")
   end
+
+  def placeholder_person_ids
+    if @person.sac_family_main_person && @beitragskategorie.to_s == SacCas::Beitragskategorie::Calculator::CATEGORY_FAMILY.to_s
+      family_ids = @person.household_people.distinct.order(:id).pluck(:id)
+      family_ids.present? ? "#{@person.id} (#{family_ids.join(", ")})" : @person.id
+    else
+      @person.id
+    end
+  end
+
+  def placeholder_section_name
+    @section.to_s
+  end
+
+  def placeholder_membership_category
+    t(@beitragskategorie, scope: "roles.beitragskategorie")
+  end
+
+  def placeholder_invoice_details
+    presenter = Invoices::SacMemberships::SectionSignupFeePresenter.new(
+      @section,
+      @beitragskategorie,
+      @person,
+      main: false
+    )
+    ApplicationController.render("wizards/signup/_section_fee_positions_table", layout: false, locals: {active: true, group: @section, presenter:}).html_safe
+  end
 end
