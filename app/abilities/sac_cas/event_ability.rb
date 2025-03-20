9c16f81548627244a570ae40c76530878e0d611a
Niklas Jäggi <jaeggi@puzzle.ch>
2025-03-20 11:55:06 +0100

    Add manage_attachments for course leader roles (#1695)

---
+++ app/abilities/sac_cas/event_ability.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2024, Schweizer Alpen-Club. This file is part of
+#  hitobito_sac_cas and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sac_cas
+
+module SacCas::EventAbility
+  extend ActiveSupport::Concern
+
+  prepended do
+    on(Event) do
+      permission(:any).may(:manage_attachments).for_participations_full_events
+    end
+  end
+end
