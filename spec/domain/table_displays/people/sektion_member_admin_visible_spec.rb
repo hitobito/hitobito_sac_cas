f5c5326e4cd2d6fb3d2089a27f4a7b856341f1dc
Niklas Jäggi <jaeggi@puzzle.ch>
2025-03-25 13:03:04 +0100

    Include person association for participation table displays (#1753)

--- spec/domain/table_displays/people/sektion_member_admin_visible_spec.rb
+++ spec/domain/table_displays/people/sektion_member_admin_visible_spec.rb
@@ -17,104 +17,112 @@ describe TableDisplays::People::SektionMemberAdminVisible, type: :helper do
   let(:table) { StandardTableBuilder.new([mitglied.decorate], self) }
   let(:sektion_mitglieder) { groups(:bluemlisalp_mitglieder) }
 
-  subject(:column) { TableDisplays::People::TerminateOnColumn.new(ability, model_class: Person) }
-
   before do
     allow_any_instance_of(ActionView::Base).to receive(:parent).and_return(sektion_mitglieder)
     roles(:mitglied).update!(end_on: Date.new(2025, 3, 10))
   end
 
-  context "as admin" do
-    it "shows value" do
-      expect(column.value_for(mitglied, :terminated_on)).to eq "10.03.2025"
-    end
-  end
+  [[Person, [:roles_with_ended_readable, {roles_unscoped: :group}]], [Event::Participation, [:roles_with_ended_readable, {person: {roles_unscoped: :group}}]]].each do |model_class, expected_includes|
+    context "table display on #{model_class}" do
+      subject(:column) { TableDisplays::People::TerminateOnColumn.new(ability, model_class: model_class) }
 
-  context "in section" do
-    context "with schreibrecht" do
-      let(:current_user) { Fabricate(Group::SektionsMitglieder::Schreibrecht.sti_name, group: sektion_mitglieder).person }
-
-      it "shows value" do
-        expect(column.value_for(mitglied, :terminated_on)).to eq "10.03.2025"
+      context "as admin" do
+        it "shows value" do
+          expect(column.value_for(mitglied, :terminated_on)).to eq "10.03.2025"
+        end
       end
-    end
 
-    context "with mitgliederverwaltung" do
-      let(:current_user) { Fabricate(Group::SektionsFunktionaere::Mitgliederverwaltung.sti_name, group: groups(:bluemlisalp_funktionaere)).person }
+      context "in section" do
+        context "with schreibrecht" do
+          let(:current_user) { Fabricate(Group::SektionsMitglieder::Schreibrecht.sti_name, group: sektion_mitglieder).person }
 
-      it "shows value" do
-        expect(column.value_for(mitglied, :terminated_on)).to eq "10.03.2025"
+          it "shows value" do
+            expect(column.value_for(mitglied, :terminated_on)).to eq "10.03.2025"
+          end
+        end
+
+        context "with mitgliederverwaltung" do
+          let(:current_user) { Fabricate(Group::SektionsFunktionaere::Mitgliederverwaltung.sti_name, group: groups(:bluemlisalp_funktionaere)).person }
+
+          it "shows value" do
+            expect(column.value_for(mitglied, :terminated_on)).to eq "10.03.2025"
+          end
+        end
       end
-    end
-  end
 
-  context "from outside of sektion" do
-    let(:current_user) { Fabricate(Group::SektionsMitglieder::Schreibrecht.sti_name, group: groups(:matterhorn_mitglieder)).person }
+      context "from outside of sektion" do
+        let(:current_user) { Fabricate(Group::SektionsMitglieder::Schreibrecht.sti_name, group: groups(:matterhorn_mitglieder)).person }
 
-    it "shows value because we have show_full via zusatzsektion " do
-      expect(column.value_for(mitglied, :terminated_on)).to eq "10.03.2025"
-    end
+        it "shows value because we have show_full via zusatzsektion " do
+          expect(column.value_for(mitglied, :terminated_on)).to eq "10.03.2025"
+        end
 
-    it "hides value when no zusatzsektion exists for that sektion" do
-      roles(:mitglied_zweitsektion).destroy
-      expect(column.value_for(mitglied, :terminated_on)).to eq "fehlende Berechtigung"
-    end
-  end
-
-  context "without ability" do
-    let(:ability) { Ability.new(people(:abonnent)) }
-
-    it "hides value" do
-      expect(column.value_for(mitglied, :terminated_on)).to eq "fehlende Berechtigung"
-    end
-  end
-
-  describe "columns including or excluding" do
-    [
-      "beitrittsdatum",
-      "birthday",
-      "company",
-      "company_name",
-      "country",
-      "data_quality",
-      "email",
-      "gender",
-      "id",
-      "j_s_number",
-      "language",
-      "membership_years",
-      "nationality_j_s",
-      "sac_remark_national_office",
-      "sac_remark_section_1",
-      "sac_remark_section_2",
-      "sac_remark_section_3",
-      "sac_remark_section_4",
-      "sac_remark_section_5",
-      "self_registration_reason",
-      "terminate_on",
-      "termination_reason",
-      "wiedereintritt"
-    ].each do |column|
-      it "column #{column} includes #{described_class}" do
-        column = TableDisplay.table_display_columns["Person"][column]
-        expect(column.ancestors).to include(described_class)
+        it "hides value when no zusatzsektion exists for that sektion" do
+          roles(:mitglied_zweitsektion).destroy
+          expect(column.value_for(mitglied, :terminated_on)).to eq "fehlende Berechtigung"
+        end
       end
-    end
 
-    [
-      "address_valid",
-      "antrag_fuer",
-      "antragsdatum",
-      "beitragskategorie",
-      "confirmed_at",
-      "duplicate_exists",
-      "layer_group_label",
-      "login_status",
-      "primary_group_id"
-    ].each do |column|
-      it "column #{column} excludes #{described_class}" do
-        column = TableDisplay.table_display_columns["Person"][column]
-        expect(column.ancestors).not_to include(described_class)
+      context "without ability" do
+        let(:ability) { Ability.new(people(:abonnent)) }
+
+        it "hides value" do
+          expect(column.value_for(mitglied, :terminated_on)).to eq "fehlende Berechtigung"
+        end
+      end
+
+      it "only includes needed associations" do
+        expect(column.required_model_includes(:terminated_on)).to eq expected_includes
+      end
+
+      describe "columns including or excluding" do
+        [
+          "beitrittsdatum",
+          "birthday",
+          "company",
+          "company_name",
+          "country",
+          "data_quality",
+          "email",
+          "gender",
+          "id",
+          "j_s_number",
+          "language",
+          "membership_years",
+          "nationality_j_s",
+          "sac_remark_national_office",
+          "sac_remark_section_1",
+          "sac_remark_section_2",
+          "sac_remark_section_3",
+          "sac_remark_section_4",
+          "sac_remark_section_5",
+          "self_registration_reason",
+          "terminate_on",
+          "termination_reason",
+          "wiedereintritt"
+        ].each do |column|
+          it "column #{column} includes #{described_class}" do
+            column = TableDisplay.table_display_columns["Person"][column]
+            expect(column.ancestors).to include(described_class)
+          end
+        end
+
+        [
+          "address_valid",
+          "antrag_fuer",
+          "antragsdatum",
+          "beitragskategorie",
+          "confirmed_at",
+          "duplicate_exists",
+          "layer_group_label",
+          "login_status",
+          "primary_group_id"
+        ].each do |column|
+          it "column #{column} excludes #{described_class}" do
+            column = TableDisplay.table_display_columns["Person"][column]
+            expect(column.ancestors).not_to include(described_class)
+          end
+        end
       end
     end
   end
