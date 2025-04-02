f4456deee02a88642d69fdc26efe3c0e6feaa3ed
Niklas Jäggi <jaeggi@puzzle.ch>
2025-04-02 13:22:03 +0200

    Improve query readability

--- app/jobs/people/sac_memberships/destroy_households_for_inactive_memberships_job.rb
+++ app/jobs/people/sac_memberships/destroy_households_for_inactive_memberships_job.rb
@@ -19,14 +19,12 @@ module People
       def affected_family_people
         family_role_types = (SacCas::MITGLIED_STAMMSEKTION_ROLES + SacCas::NEUANMELDUNG_STAMMSEKTION_ROLES).map(&:sti_name)
 
-        people_with_active_family_roles_ids = Role.select(:person_id).where(
-          type: family_role_types,
-          beitragskategorie: SacCas::Beitragskategorie::Calculator::CATEGORY_FAMILY
-        )
+        people_with_active_family_roles_ids = Role.where(type: family_role_types,
+          beitragskategorie: SacCas::Beitragskategorie::Calculator::CATEGORY_FAMILY)
+          .pluck(:person_id)
 
         Person.where.not(household_key: "")
-          .where("id NOT IN (:people_ids)", # rubocop:disable Rails/WhereNot
-            people_ids: people_with_active_family_roles_ids)
+          .where.not(id: people_with_active_family_roles_ids)
           .distinct_on(:household_key)
       end
     end
