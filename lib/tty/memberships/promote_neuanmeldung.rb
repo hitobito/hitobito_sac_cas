4e8525b048e5cc14f5da1b9157f72aa5903750f9
Daniel Illi <daniel@illi.zone>
2025-04-03 16:56:48 +0200

    Improve promote_neuanmeldung script

--- lib/tty/memberships/promote_neuanmeldung.rb
+++ lib/tty/memberships/promote_neuanmeldung.rb
@@ -14,12 +14,12 @@ module TTY
     class PromoteNeuanmeldung
       include TTY::Helpers::Format
 
-      attr_reader :neuanmeldung, :end_date
+      attr_reader :neuanmeldung, :end_on
 
       def initialize
         puts light_yellow "Promote Neuanmeldung to Membership"
         @neuanmeldung = ask_for_neuanmeldung_role
-        @end_date = ask_for_end_date
+        @end_on = ask_for_end_on
       end
 
       def run
@@ -29,47 +29,52 @@ module TTY
       private
 
       def ask_for_neuanmeldung_role
-        print "Please enter the ID of the Neuanmeldung role you want to promote: "
-        neuanmeldung_role_id = gets.chomp
+        loop do
+          print "Please enter the ID of the Neuanmeldung role you want to promote: "
+          neuanmeldung_role_id = gets.chomp
 
-        unless /\A\d+\z/.match?(neuanmeldung_role_id)
-          puts error "Invalid ID. Please enter a number."
-          return ask_for_neuanmeldung_role
+          unless /\A\d+\z/.match?(neuanmeldung_role_id)
+            puts error "Invalid ID. Please enter a number."
+            next # ask again
+          end
+
+          neuanmeldung = SacCas::NEUANMELDUNG_ROLES.lazy.map do |role_type|
+            role_type.find_by(id: neuanmeldung_role_id)
+          end.find(&:itself)
+
+          unless neuanmeldung
+            puts error "Neuanmeldung #{neuanmeldung_role_id} not found. Please give me a valid ID."
+            next # ask again
+          end
+
+          puts "Found #{neuanmeldung.type} in #{neuanmeldung.group.layer_group} for person #{neuanmeldung.person_id} #{neuanmeldung.person}"
+          return neuanmeldung
         end
-
-        neuanmeldung = SacCas::NEUANMELDUNG_ROLES.lazy.map do |role_type|
-          role_type.find_by(id: neuanmeldung_role_id)
-        end.find(&:itself)
-
-        unless neuanmeldung
-          puts error "Neuanmeldung #{neuanmeldung_role_id} not found. Please give me a valid ID."
-          return ask_for_neuanmeldung_role
-        end
-
-        puts "Found #{neuanmeldung.type} in #{neuanmeldung.group.layer_group} for person #{neuanmeldung.person_id} #{neuanmeldung.person}"
-        neuanmeldung
       end
 
       # ask for the date formatted as DD.MM.YYYY
       # use default date Date.current.end_of_year if no date is given
       # parse the date and return it, ask again if the date is invalid
-      def ask_for_end_date
-        print "Please enter the end date for the membership (DD.MM.YYYY) or leave empty for default (#{Date.current.end_of_year.strftime("%d.%m.%Y")}): "
-        end_date = gets.chomp
+      def ask_for_end_on
+        default_date = Date.current.end_of_year
+        default_date_formatted = default_date.strftime("%d.%m.%Y")
+        prompt = "Please enter the end date for the membership (DD.MM.YYYY) or leave empty for default (#{default_date_formatted}): "
 
-        if end_date.empty?
-          puts "Using default end date: #{Date.current.end_of_year}"
-          return Date.current.end_of_year
+        loop do
+          print prompt
+          end_on_input = gets.chomp
+
+          if end_on_input.empty?
+            puts "Using default end date: #{default_date}"
+            return default_date
+          end
+
+          begin
+            return Date.strptime(end_on, "%d.%m.%Y")
+          rescue ArgumentError
+            puts error "Invalid date format. Please enter a valid date (DD.MM.YYYY)."
+          end
         end
-
-        parsed_date = Date.strptime(end_date, "%d.%m.%Y") rescue nil
-
-        unless parsed_date
-          puts error "Invalid date format. Please enter a valid date (DD.MM.YYYY)."
-          return ask_for_end_date
-        end
-
-        parsed_date
       end
 
       def mitglied_role_type
@@ -82,8 +87,8 @@ module TTY
         @mitglied_group ||= neuanmeldung.group.layer_group.children.find_by(type: Group::SektionsMitglieder.sti_name)
       end
 
-      def people
-        @people ||= if neuanmeldung.family?
+      def people_to_promote
+        @people_to_promote ||= if neuanmeldung.family?
           neuanmeldung.person.household.people
         else
           [neuanmeldung.person]
@@ -92,7 +97,7 @@ module TTY
 
       def promote
         Person.transaction do
-          people.each do |person|
+          people_to_promote.each do |person|
             promote_person(person)
           end
         end
@@ -100,36 +105,43 @@ module TTY
       end
 
       def promote_person(person)
-        puts "Promoting #{person.id} #{person} to Mitglied"
+        puts " -> Promoting #{person.id} #{person} to #{mitglied_role_type.sti_name}"
+        start_on = [Date.current, end_on].min
+
+        # Clear the Neuanmeldung role first
         person.roles.where(type: neuanmeldung.class.sti_name, group_id: neuanmeldung.group_id).destroy_all
+
+        # Create the new Mitglied role
         mitglied_role_type.create!(
           person: person,
           group: mitglied_group,
-          start_on: [Date.current, end_date].min,
-          end_on: end_date
+          start_on:,
+          end_on:
         )
       end
 
       def confirm?
         if neuanmeldung.family?
           puts "This is a family Neuanmeldung with the following members:"
-          people.each { |person| puts "  * #{person.id} #{person}" }
+          people_to_promote.each { |person| puts "  * #{person.id} #{person}" }
           puts
         end
 
         print "Do you want to execute the promotion? (y/n): "
-        choice = STDIN.getch
 
-        case choice
-        when "y"
-          puts "Starting promotion..."
-          true
-        when "n"
-          puts light_red "Promotion canceled."
-          false
-        else
-          puts error "Invalid choice. Please enter 'y' or 'n'."
-          confirm?
+        loop do
+          choice = $stdin.getch.tap { puts } # Read a single character and print a newline
+
+          case choice
+          when "y"
+            puts "Starting promotion..."
+            return true
+          when "n"
+            puts light_red "Promotion canceled."
+            return false
+          else
+            puts error "Invalid choice. Please enter 'y' or 'n'."
+          end
         end
       end
     end
