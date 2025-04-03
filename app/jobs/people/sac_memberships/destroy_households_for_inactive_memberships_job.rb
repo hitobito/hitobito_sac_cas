e57f4f5b9b9b8d33bb5c3569ce2f73b378bdc023
Daniel Illi-Zuberbühler <daniel-illi@users.noreply.github.com>
2025-04-03 09:37:01 +0200

    Add family cleanup job for ended stammsektion roles (#1641) (#1773)
    ---------
    
    Co-authored-by: Niklas Jäggi <jaeggi@puzzle.ch>
    Co-authored-by: Nils Rauch <nils.rauch@gmail.com>

---
+++ app/jobs/people/sac_memberships/destroy_households_for_inactive_memberships_job.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2025, Schweizer Alpen-Club. This file is part of
+#  hitobito_sac_cas and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sac_cas
+
+module People
+  module SacMemberships
+    class DestroyHouseholdsForInactiveMembershipsJob < RecurringJob
+      def next_run
+        Time.current.tomorrow.change(hour: 0, min: 8)
+      end
+
+      def perform_internal
+        affected_family_people.each { _1.household.destroy }
+      end
+
+      def affected_family_people
+        family_role_types = (SacCas::MITGLIED_STAMMSEKTION_ROLES + SacCas::NEUANMELDUNG_STAMMSEKTION_ROLES).map(&:sti_name)
+
+        people_with_active_family_roles_ids = Role.select(:person_id).where(
+          type: family_role_types,
+          beitragskategorie: SacCas::Beitragskategorie::Calculator::CATEGORY_FAMILY
+        )
+
+        Person.where.not(household_key: "")
+          .where("id NOT IN (:people_ids)", # rubocop:disable Rails/WhereNot
+            people_ids: people_with_active_family_roles_ids)
+          .distinct_on(:household_key)
+      end
+    end
+  end
+end
