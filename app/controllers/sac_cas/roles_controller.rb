82a4ea23bcb88bf8fef6ba44582fed18e2c418c3
Nils Rauch <rauch@puzzle.ch>
2025-05-08 10:55:02 +0200

    Move family neuanmeldungs roles destruction to controller Since the model callback messes with sac membership logic

--- app/controllers/sac_cas/roles_controller.rb
+++ app/controllers/sac_cas/roles_controller.rb
@@ -2,16 +2,34 @@ module SacCas::RolesController
   extend ActiveSupport::Concern
 
   prepended do
-    after_destroy :destroy_household, if: :neuanmeldungs_role?
+    after_destroy :resolve_household_for_neuanmeldung
   end
 
   private
 
-  def neuanmeldungs_role?
+  def resolve_household_for_neuanmeldung
+    destroy_family_neuanmeldungen if family_neuanmeldungs_role?
+    destroy_household if neuanmeldungs_stammsektion_role?
+  end
+
+  def neuanmeldungs_stammsektion_role?
     ::SacCas::NEUANMELDUNG_STAMMSEKTION_ROLES.map(&:sti_name).include?(entry.type)
+  end
+
+  def family_neuanmeldungs_role?
+    ::SacCas::NEUANMELDUNG_ROLES.map(&:sti_name).include?(entry.type) && entry.family?
   end
 
   def destroy_household
     Household.new(entry.person).destroy
+  end
+
+  def destroy_family_neuanmeldungen
+    Role.where(type: SacCas::NEUANMELDUNG_ROLES.map(&:sti_name),
+      person_id: family_mitglieder.pluck(:id)).destroy_all
+  end
+
+  def family_mitglieder
+    Household.new(person).people
   end
 end
