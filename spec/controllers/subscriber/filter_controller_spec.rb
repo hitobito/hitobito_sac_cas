7d49a19aa2a0ce07d2600ef164d87faf9e687502
megli2 <egli@puzzle.ch>
2025-05-08 15:02:33 +0200

    Rework people filters on sac environment

--- spec/controllers/subscriber/filter_controller_spec.rb
+++ spec/controllers/subscriber/filter_controller_spec.rb
@@ -15,8 +15,8 @@ describe Subscriber::FilterController do
   context "PATCH update" do
     let(:filters) do
       {
-        language: {"allowed_values" => %w[de fr]},
-        attributes: {"1699698452786" => {"constraint" => "greater", "key" => "years", "value" => "16"}}
+        attributes: {"1699698452786" => {"constraint" => "greater", "key" => "years", "value" => "16"},
+                     "1699698452567" => {"constraint" => "equal", "key" => "language", "value" => %w[de fr]}}
       }
     end
 
@@ -59,9 +59,6 @@ describe Subscriber::FilterController do
         id: mailing_list.id))
 
       filter_chain = mailing_list.reload.filter_chain
-      expect(filter_chain[:language]).to be_a(Person::Filter::Language)
-      expect(filter_chain[:language].args).to eq(filters[:language].deep_stringify_keys)
-      expect(filter_chain[:language].allowed_values).to contain_exactly("de", "fr")
       expect(filter_chain[:attributes]).to be_a(Person::Filter::Attributes)
       expect(filter_chain[:attributes].args).to eq(filters[:attributes].deep_stringify_keys)
     end
