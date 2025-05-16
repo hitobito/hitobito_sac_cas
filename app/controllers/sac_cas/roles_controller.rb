5d9160a8bb7552c4962bca248b071cc8f2933b91
Nils Rauch <rauch@puzzle.ch>
2025-05-16 09:50:10 +0200

    Allow destroying neuanmeldung roles for backoffice #1759
    * Allow destroying neuanmeldung roles for backoffice
    * Remove only family neuanmeldungs roles in the same layer

---
+++ app/controllers/sac_cas/roles_controller.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2025, Schweizer Alpen-Club. This file is part of
+#  hitobito_sac_cas and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sac_cas.
+
+module SacCas::RolesController
+  extend ActiveSupport::Concern
+
+  prepended do
+    after_destroy :resolve_household_for_neuanmeldung
+  end
+
+  private
+
+  def resolve_household_for_neuanmeldung
+    destroy_family_neuanmeldungen if family_neuanmeldungs_role?
+    destroy_household if neuanmeldungs_stammsektion_role?
+  end
+
+  def neuanmeldungs_stammsektion_role?
+    ::SacCas::NEUANMELDUNG_STAMMSEKTION_ROLES.map(&:sti_name).include?(entry.type)
+  end
+
+  def family_neuanmeldungs_role?
+    ::SacCas::NEUANMELDUNG_ROLES.map(&:sti_name).include?(entry.type) && entry.family?
+  end
+
+  def destroy_household
+    Household.new(entry.person).destroy
+  end
+
+  def destroy_family_neuanmeldungen
+    Role.where(type: SacCas::NEUANMELDUNG_ROLES.map(&:sti_name),
+      person_id: family_mitglieder.pluck(:id),
+      group_id: group.groups_in_same_layer.pluck(:id)).destroy_all
+  end
+
+  def family_mitglieder
+    Household.new(person).people
+  end
+end
---
+++ spec/controllers/roles_controller_spec.rb
@@ -0,0 +1,141 @@
+#  Copyright (c) 2025, Schweizer Alpen-Club. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
+require "spec_helper"
+
+describe RolesController do
+  before { sign_in(people(:admin)) }
+
+  let(:person) { Fabricate(:person) }
+
+  describe "DELETE destroy" do
+    let!(:household) do
+      household = Household.new(person, maintain_sac_family: false, validate_members: false)
+      household.set_family_main_person!
+      household
+    end
+
+    def build_depending_roles_and_family(beitragskategorie: :family)
+      [
+        [Group::SektionsNeuanmeldungenNv::Neuanmeldung, :bluemlisalp_neuanmeldungen_nv, false],
+        [Group::SektionsNeuanmeldungenSektion::Neuanmeldung, :bluemlisalp_neuanmeldungen_sektion, false],
+        [Group::SektionsNeuanmeldungenNv::NeuanmeldungZusatzsektion, :bluemlisalp_neuanmeldungen_nv, true],
+        [Group::SektionsNeuanmeldungenSektion::NeuanmeldungZusatzsektion, :bluemlisalp_neuanmeldungen_sektion, true]
+      ].map do |role_class, group, add_membership_role|
+        build_depending_role_and_add_to_family(role_class, group, add_membership_role:, beitragskategorie:)
+      end
+    end
+
+    def build_depending_role_and_add_to_family(role_class, group, add_membership_role: false, beitragskategorie: :family)
+      p = Fabricate(:person, birthday: 12.years.ago)
+      household.add(p)
+      household.save!
+      Fabricate(Group::SektionsMitglieder::Mitglied.sti_name.to_sym, group: groups(:matterhorn_mitglieder), person: p) if add_membership_role
+      Fabricate(role_class.sti_name.to_sym, group: groups(group), beitragskategorie:, person: p)
+    end
+
+    context "for family neuanmeldungs role" do
+      let(:group) { groups(:bluemlisalp_neuanmeldungen_nv) }
+
+      it "destroys household" do
+        role = Fabricate(Group::SektionsNeuanmeldungenNv::Neuanmeldung.sti_name.to_sym, group: group, beitragskategorie: :family, person: person)
+
+        other = Fabricate(Group::SektionsMitglieder::Mitglied.sti_name.to_sym, group: groups(:matterhorn_mitglieder)).person
+        other2 = Fabricate(:person, birthday: 12.years.ago)
+
+        household.add(other)
+        household.add(other2)
+        household.save!
+
+        expect(household.people).to match_array([person, other, other2])
+
+        delete :destroy, params: {group_id: group.id, id: role.id}
+
+        household = person.reload.household
+
+        expect(household.people).to match_array([person])
+      end
+
+      it "destroys family neuanmeldung roles in same layer" do
+        household.add(people(:mitglied))
+        role = Fabricate(Group::SektionsNeuanmeldungenNv::Neuanmeldung.sti_name.to_sym, group: group, beitragskategorie: :family, person: person)
+
+        depending_roles = build_depending_roles_and_family
+
+        delete :destroy, params: {group_id: group.id, id: role.id}
+
+        depending_roles.each do |depending_role|
+          expect(Role.where(id: depending_role.id)).to_not be_present
+        end
+      end
+
+      it "does not destroy family neuanmeldung roles in other layer" do
+        household.add(people(:mitglied))
+        role = Fabricate(Group::SektionsNeuanmeldungenNv::Neuanmeldung.sti_name.to_sym, group: group, beitragskategorie: :family, person: person)
+
+        depending_role = build_depending_role_and_add_to_family(Group::SektionsNeuanmeldungenNv::Neuanmeldung, :matterhorn_neuanmeldungen_nv)
+
+        delete :destroy, params: {group_id: group.id, id: role.id}
+
+        expect(Role.where(id: depending_role.id)).to be_present
+      end
+    end
+
+    context "for adult neuanmeldungs role" do
+      let(:group) { groups(:bluemlisalp_neuanmeldungen_nv) }
+
+      it "does not destroy family neuanmeldung roles" do
+        household.add(people(:mitglied))
+        role = Fabricate(Group::SektionsNeuanmeldungenNv::Neuanmeldung.sti_name.to_sym, group: group, beitragskategorie: :adult, person: person)
+
+        depending_roles = build_depending_roles_and_family(beitragskategorie: :adult)
+
+        delete :destroy, params: {group_id: group.id, id: role.id}
+
+        depending_roles.each do |depending_role|
+          expect(Role.where(id: depending_role.id)).to be_present
+        end
+      end
+    end
+
+    context "for abo role" do
+      let(:group) { groups(:abo_die_alpen) }
+
+      it "does not destroy household" do
+        Fabricate(Group::SektionsNeuanmeldungenNv::Neuanmeldung.sti_name.to_sym, group: groups(:bluemlisalp_neuanmeldungen_nv), beitragskategorie: :family, person: person)
+        role = Fabricate(Group::AboMagazin::Andere.sti_name.to_sym, group: group)
+
+        other = Fabricate(Group::SektionsMitglieder::Mitglied.sti_name.to_sym, group: groups(:matterhorn_mitglieder)).person
+        other2 = Fabricate(:person, birthday: 12.years.ago)
+
+        household.add(other)
+        household.add(other2)
+        household.save!
+
+        expect(household.people).to match_array([person, other, other2])
+
+        delete :destroy, params: {group_id: group.id, id: role.id}
+
+        household = person.reload.household
+
+        expect(household.people).to match_array([person, other, other2])
+      end
+
+      it "does not destroy family neuanmeldung roles in same layer" do
+        household.add(people(:mitglied))
+        Fabricate(Group::SektionsNeuanmeldungenNv::Neuanmeldung.sti_name.to_sym, group: groups(:bluemlisalp_neuanmeldungen_nv), beitragskategorie: :adult, person: person)
+        role = Fabricate(Group::AboMagazin::Andere.sti_name.to_sym, group: group)
+
+        depending_roles = build_depending_roles_and_family
+
+        delete :destroy, params: {group_id: group.id, id: role.id}
+
+        depending_roles.each do |depending_role|
+          expect(Role.where(id: depending_role.id)).to be_present
+        end
+      end
+    end
+  end
+end
