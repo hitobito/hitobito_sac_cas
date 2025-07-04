bb8eed1a6a71ff94c46a4b7742be2bfd3287d654
Pascal Zumkehr <zumkehr@puzzle.ch>
2025-07-04 15:23:00 +0200

    Refactor multilingual mailer for new core custom content locale fallbacks

--- app/mailers/concerns/multilingual_mailer.rb
+++ app/mailers/concerns/multilingual_mailer.rb
@@ -18,17 +18,20 @@ module MultilingualMailer
   def compose_multilingual(recipients, content_key, locales = [])
     return if recipients.blank?
 
+    locales = [I18n.locale] if locales.empty?
+    if locales.size == 1
+      I18n.with_locale(locales.first) { compose(recipients, content_key) }
+    else
+      do_compose_multilingual(recipients, content_key, locales)
+    end
+  end
+
+  def do_compose_multilingual(recipients, content_key, locales)
     view_context  # must be set explicitly for draper see (hitobito/hitobito#6f310725c2)
 
     content = CustomContent.get(content_key)
-    locales = [I18n.locale] if locales.empty?
     subject, body = localized_subject_and_body(content, locales)
     headers[:to] = use_mailing_emails(recipients)
-
-    # use default locale if no translation is available to avoid sending empty mail
-    if body.blank?
-      subject, body = localized_subject_and_body(content, [I18n.default_locale])
-    end
 
     I18n.with_locale(locales.first) do
       mail(subject:) do |f|
