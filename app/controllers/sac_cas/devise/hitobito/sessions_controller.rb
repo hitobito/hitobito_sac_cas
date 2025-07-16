ffdc6ad61592221870c58eb2160f425dc0d0628b
Daniel Illi <daniel@illi.zone>
2025-07-16 10:02:51 +0200

    Fix scope

--- app/controllers/sac_cas/devise/hitobito/sessions_controller.rb
+++ app/controllers/sac_cas/devise/hitobito/sessions_controller.rb
@@ -15,16 +15,18 @@ module SacCas::Devise::Hitobito::SessionsController
   private
 
   def redirect_if_unconfirmed
-    unconfirmed_people = Person.where(confirmed_at: nil)
-    login_identity = params.dig(:person, :login_identity)
-
-    person = Person.devise_login_id_attrs.reduce(unconfirmed_people) do |memo, attr|
-      memo.or(Person.where(attr => login_identity))
-    end.first
+    person = find_person_by_login_identity
 
     if person&.valid_password?(params.dig(:person, :password))
       person.send_confirmation_instructions
       redirect_to new_session_path(:person), alert: t("devise.sessions.create.confirm_email_before_logging_in")
     end
+  end
+
+  def find_person_by_login_identity
+    login_identity = params.dig(:person, :login_identity)
+    Person.devise_login_id_attrs.reduce(Person.none) do |scope, attr|
+      scope.or(Person.where(attr => login_identity))
+    end.first
   end
 end
