2bf157bc36dcd32cbdeba78e29b7db90cd3d0c00
Andreas Maierhofer <a.maierhofer@gmail.com>
2025-11-03 11:28:47 +0100

    Add people export for eintritte (#1665)

---
+++ app/models/people/export/eintritte_form.rb
@@ -0,0 +1,17 @@
+#  Copyright (c) 2025, Schweizer Alpen-Club. This file is part of
+#  hitobito_sac_cas and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito
+
+class People::Export::EintritteForm
+  include ActiveModel::Model
+  include ActiveModel::Attributes
+
+  attr_accessor :group
+
+  attribute :from, :date, default: -> { Date.current.beginning_of_year }
+  attribute :to, :date, default: -> { Date.current.end_of_year }
+
+  validates_date :to, after: :from
+  validates_date :to, on_or_before: ->(m) { m.from + 1.year }
+end
