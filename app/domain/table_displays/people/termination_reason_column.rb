6b727ad005e5a052449e79c000147ac1551d4772
Andreas Maierhofer <a.maierhofer@gmail.com>
2025-11-26 08:43:49 +0100

    Remove obsolete 2nd null check

--- app/domain/table_displays/people/termination_reason_column.rb
+++ app/domain/table_displays/people/termination_reason_column.rb
@@ -1,8 +1,13 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2025, Schweizer Alpen-Club. This file is part of
+#  hitobito_sac_cas and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sac_cas
+
 module TableDisplays::People
   class TerminationReasonColumn < TerminationColumn
     def value(terminated_role)
-      return if terminated_role.nil?
-
       terminated_role.termination_reason_text
     end
   end
