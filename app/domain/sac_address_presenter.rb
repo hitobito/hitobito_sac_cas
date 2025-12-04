32fc94a56f1a15010e32326472c750b51a2d92f0
carlobeltrame <carlo@beltra.me>
2025-12-04 15:23:18 +0100

    Adapt to core changes (hitobito/hitobito#3775)

--- app/domain/sac_address_presenter.rb
+++ app/domain/sac_address_presenter.rb
@@ -11,8 +11,8 @@ class SacAddressPresenter
       key_data_sheet_format
     when :mail
       mail_format
-    when :leader_settlement
-      leader_settlement_format
+    when :leader_settlement_invoice_attributes
+      leader_settlement_invoice_attributes
     end
   end
 
@@ -22,29 +22,35 @@ class SacAddressPresenter
     <<~HTML.strip
       #{@data[:name]}
       #{@data[:address]}
-      #{@data[:town]}
+      #{@data[:zip_code_and_town]}
       Tel: <a href="tel:#{telephone_link(@data[:phone])}">#{@data[:phone]}</a>
       <a href="mailto:#{@data[:email_mv]}">#{@data[:email_mv]}</a>
     HTML
   end
 
   def key_data_sheet_format
-    # rubocop:todo Layout/LineLength
-    "#{@data[:name]}, #{@data[:address]}, #{@data[:postbox]}, CH-#{@data[:town]}, #{@data[:phone_self_registration]}, #{@data[:email]}"
-    # rubocop:enable Layout/LineLength
+    "#{@data[:name]}, #{@data[:address]}, #{@data[:postbox]}, " \
+      "CH-#{@data[:zip_code_and_town]}, #{@data[:phone_self_registration]}, #{@data[:email]}"
   end
 
   def mail_format
     <<~HTML.strip
       #{@data[:name]}
       #{@data[:address]}
-      #{@data[:town]}
+      #{@data[:zip_code_and_town]}
       <a href="#{@data[:website]}">#{@data[:website]}</a>
     HTML
   end
 
-  def leader_settlement_format
-    "#{@data[:name]}\n#{@data[:zentralverband]}, #{@data[:address]}\n#{@data[:town]}\n\n\n"
+  def leader_settlement_invoice_attributes
+    {
+      recipient_name: @data[:name],
+      recipient_street: "#{@data[:zentralverband]}, #{@data[:street]}",
+      recipient_housenumber: @data[:housenumber],
+      recipient_zip_code: @data[:zip_code],
+      recipient_town: @data[:town],
+      recipient_country: @data[:country]
+    }
   end
 
   def telephone_link(telephone)
