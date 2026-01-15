4d3528f6bfe2f886c30862aadefbee954a1c080a
Pascal Zumkehr <zumkehr@puzzle.ch>
2026-01-15 15:59:15 +0100

    Add event fitness requirements controller (#2191)

--- lib/hitobito_sac_cas/wagon.rb
+++ lib/hitobito_sac_cas/wagon.rb
@@ -131,6 +131,7 @@ module HitobitoSacCas
       Ability.store.register CourseCompensationCategoryAbility
       Ability.store.register CourseCompensationRateAbility
       Ability.store.register Event::DisciplineAbility
+      Ability.store.register Event::FitnessRequirementAbility
       Ability.store.register Event::LevelAbility
       Ability.store.register Event::TargetGroupAbility
       Ability.store.register Event::TechnicalRequirementAbility
@@ -230,6 +231,7 @@ module HitobitoSacCas
         event_levels
         event_target_groups
         event_technical_requirements
+        event_fitness_requirements
         termination_reasons
         section_offerings
         course_compensation_categories
