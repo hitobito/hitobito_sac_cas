5a87a6119a2b64372191772a258f979e9edf5805
Andreas Maierhofer <maierhofer@puzzle.ch>
2026-01-28 08:54:29 +0100

    Fix null check in abo_magazin_company_toggling

--- app/assets/javascripts/abo_magazin_company_toggling.js
+++ app/assets/javascripts/abo_magazin_company_toggling.js
@@ -1,32 +1,36 @@
 (function () {
-  const firstName = "label[for=wizards_signup_abo_magazin_wizard_person_fields_first_name]";
-  const lastName = "label[for=wizards_signup_abo_magazin_wizard_person_fields_last_name]";
-  const birthday = "label[for=wizards_signup_abo_magazin_wizard_person_fields_birthday]";
-  const companyName = "label[for=wizards_signup_abo_magazin_wizard_person_fields_company_name]";
+  const firstName =
+    "label[for=wizards_signup_abo_magazin_wizard_person_fields_first_name]";
+  const lastName =
+    "label[for=wizards_signup_abo_magazin_wizard_person_fields_last_name]";
+  const birthday =
+    "label[for=wizards_signup_abo_magazin_wizard_person_fields_birthday]";
+  const companyName =
+    "label[for=wizards_signup_abo_magazin_wizard_person_fields_company_name]";
 
-  const toggleRequired = function(checked) {
+  const toggleRequired = function (checked) {
     if (checked) {
-      $(firstName).removeClass("required")
-      $(lastName).removeClass("required")
-      $(birthday).removeClass("required")
-      $(companyName).addClass("required")
+      $(firstName).removeClass("required");
+      $(lastName).removeClass("required");
+      $(birthday).removeClass("required");
+      $(companyName).addClass("required");
     } else {
-      $(firstName).addClass("required")
-      $(lastName).addClass("required")
-      $(birthday).addClass("required")
-      $(companyName).removeClass("required")
+      $(firstName).addClass("required");
+      $(lastName).addClass("required");
+      $(birthday).addClass("required");
+      $(companyName).removeClass("required");
     }
-  }
+  };
 
   // as forms are submitted as streams we listen on turbo:render instead of turbo:load
   $(document).on("turbo:render", function () {
     const elem = $("#wizards_signup_abo_magazin_wizard_person_fields_company");
-    if(elem) {
+    if (elem[0]) {
       toggleRequired(elem[0].checked);
 
-      elem.on("change", function() {
+      elem.on("change", function () {
         toggleRequired(elem[0].checked);
       });
     }
-  })
+  });
 }).call(this);
