f4750e406d0368801d333c38a8a25b377cf810dd
Nils Rauch <rauch@puzzle.ch>
2026-02-03 14:25:44 +0100

    Add Event::ApprovalKind data structure (#2251)
    * Add Event::ApprovalKind data structure (#2251)
    
    * Remove uniqueness index on translations

---
+++ spec/models/event/approval_kind_spec.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2026, Schweizer Alpen-Club. This file is part of
+#  hitobito_sac_cas and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sac_cas
+
+require "spec_helper"
+
+describe Event::ApprovalKind do
+  let(:approval_kind) { event_approval_kinds(:professional) }
+
+  it ".list orders entries" do
+    expect(Event::ApprovalKind.list)
+      .to eq(event_approval_kinds(:professional, :security, :editorial))
+  end
+
+  context "validations" do
+    it "require presence of name" do
+      entry = described_class.new
+      expect(entry).not_to be_valid
+      expect(entry.errors[:name]).to eq ["muss ausgefüllt werden"]
+      entry.name = "Praktisch"
+      expect(entry).to be_valid
+    end
+
+    it "ensures uniqueness of name" do
+      entry = described_class.new
+      entry.name = "Fachlich"
+      expect(entry).not_to be_valid
+      expect(entry.errors[:name]).to eq ["ist bereits vergeben"]
+    end
+  end
+
+  context "paranoia" do
+    it "hard deletes if no roles exist" do
+      expect { approval_kind.destroy }
+        .to change { described_class.unscoped.count }.by(-1)
+        .and change { Event::ApprovalKind::Translation.count }.by(-1)
+    end
+
+    it "soft deletes if roles exist" do
+      approval_kind.roles = [roles(:admin)]
+      approval_kind.save!
+
+      expect { approval_kind.destroy }.not_to change { described_class.unscoped.count }
+      expect(approval_kind.deleted_at).to be_present
+    end
+  end
+end
