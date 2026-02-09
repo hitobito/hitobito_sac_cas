8477c3995563120ffd32fb45aa51355a020a3898
Daniel Illi <daniel@illi.zone>
2026-02-09 16:17:15 +0100

    Revert "Rename module to match core changes hitobito/hitobito#3747"
    This reverts commit a6129c52f6c9c4aec66d1b3a1ec8b545e8f401b7.

--- app/domain/sac_cas/person/address.rb
+++ app/domain/sac_cas/person/address.rb
@@ -5,16 +5,16 @@
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_sac_cas.
 
-module SacCas::Contactable::Address
+module SacCas::Person::Address
   extend ActiveSupport::Concern
   def for_membership_pass
-    (contactable_and_company_name + address_with_multilanguage_country).compact.join("\n")
+    (person_and_company_name + address_with_multilanguage_country).compact.join("\n")
   end
 
   def address_with_multilanguage_country
     [
-      @contactable.address.to_s.strip,
-      [@contactable.zip_code, @contactable.town].compact.join(" ").squish,
+      @person.address.to_s.strip,
+      [@person.zip_code, @person.town].compact.join(" ").squish,
       country_string(:country_label)
     ]
   end
