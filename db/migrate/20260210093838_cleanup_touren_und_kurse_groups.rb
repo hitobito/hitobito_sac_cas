c32b77823dba164cdb77c9f81a740c18f7082031
Andreas Maierhofer <maierhofer@puzzle.ch>
2026-02-11 13:07:26 +0100

    Fix migration

--- db/migrate/20260210093838_cleanup_touren_und_kurse_groups.rb
+++ db/migrate/20260210093838_cleanup_touren_und_kurse_groups.rb
@@ -37,19 +37,24 @@ class CleanupTourenUndKurseGroups < ActiveRecord::Migration[8.0]
 
     # Set names for all Group::SektionsTourenUndKurse based on sektion language
     execute <<-SQL
+      WITH group_languages AS (
+        SELECT
+          g.id,
+          CASE
+            WHEN ma.value LIKE '%DE%' THEN 'Touren und Kurse'
+            WHEN ma.value LIKE '%EN%' THEN 'Tours and courses'
+            WHEN ma.value LIKE '%FR%' THEN 'Randonnées et cours'
+            WHEN ma.value LIKE '%IT%' THEN 'Visite e corsi'
+            ELSE 'Touren und Kurse'
+          END as translated_name
+        FROM groups g
+        LEFT JOIN mounted_attributes ma ON ma.entry_id = g.layer_group_id AND ma.entry_type = 'Group' AND ma.key = 'language'
+        WHERE g.type = 'Group::SektionsTourenUndKurse'
+      )
       UPDATE groups
-      SET name = CASE
-          WHEN mounted_attributes.value LIKE '%DE%' THEN 'Touren und Kurse'
-          WHEN mounted_attributes.value LIKE '%EN%' THEN 'Tours and courses'
-          WHEN mounted_attributes.value LIKE '%FR%' THEN 'Randonnées et cours'
-          WHEN mounted_attributes.value LIKE '%IT%' THEN 'Visite e corsi'
-          ELSE 'Touren und Kurse'
-        END
-      FROM mounted_attributes
-      WHERE groups.layer_group_id = mounted_attributes.entry_id
-      AND mounted_attributes.entry_type = 'Group'
-      AND mounted_attributes.key = 'language'
-      AND groups.type = 'Group::SektionsTourenUndKurse';
+      SET name = group_languages.translated_name
+      FROM group_languages
+      WHERE groups.id = group_languages.id
     SQL
   end
 end
