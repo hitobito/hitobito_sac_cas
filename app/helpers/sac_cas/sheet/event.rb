9af9e9c55ec3f81c7e0d7619ad30d9aa56f3892a
Nils Rauch <rauch@puzzle.ch>
2026-02-27 11:15:31 +0100

    Still render group sheet for root on participation (#2296)

--- app/helpers/sac_cas/sheet/event.rb
+++ app/helpers/sac_cas/sheet/event.rb
@@ -8,7 +8,8 @@ module SacCas::Sheet::Event
 
   class_methods do
     def parent_sheet_for(view_context)
-      if view_context.current_person&.basic_permissions_only? &&
+      if !view_context.current_person&.root? &&
+          view_context.current_person&.basic_permissions_only? &&
           view_context.controller.is_a?(::Event::ParticipationsController)
         nil
       else
