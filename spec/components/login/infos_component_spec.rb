eebda78275e3b55079f02fa8ee0a815e376efd7c
Niklas <68543592+njaeggi@users.noreply.github.com>
2026-03-31 10:46:15 +0200

    Render login info component (#2377)
    * Render specific oauth info component (#2377)
    
    * Display login info component for basic login too
    
    * Add component spec and ensure mv email is used for fr locale
    
    ---------
    
    Co-authored-by: Nils Rauch <rauch@puzzle.ch>

---
+++ spec/components/login/infos_component_spec.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2012-2023, Schweizer Alpen-Club. This file is part of
+#  hitobito_sac_cas and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sac_cas.
+
+require "spec_helper"
+
+describe Login::InfosComponent, type: :component do
+  subject(:component) { described_class.new }
+
+  subject(:html) { render_inline(component) }
+
+  subject(:parts) { html.css("aside.card .card-body") }
+
+  it "does render" do
+    expect(component).to be_render
+  end
+
+  describe "general" do
+    subject(:body) { parts[0] }
+
+    it "renders title" do
+      expect(body).to have_css("h2.card-title", text: "Fragen zum Login?")
+    end
+
+    it "info text with link to faqs" do
+      expect(body).to have_css("p.card-text", text: "Mehr Informationen findest du unter den FAQs")
+      expect(body).to have_link("FAQs", href: "https://www.sac-cas.ch/de/meta/faq/faq-sac-konto-/-sac-portal/")
+    end
+  end
+
+  describe "contact" do
+    subject(:body) { parts[1] }
+
+    it "renders title" do
+      expect(body).to have_css("h2.card-title", text: "Kontakt")
+    end
+
+    it "renders address" do
+      expect(body).to have_content <<~TEXT
+        Schweizer Alpen-Club SAC
+        Monbijoustrasse 61
+        3000 Bern 14
+        Tel: +41 31 370 18 18
+        #{SacCas::MV_EMAIL}
+      TEXT
+    end
+
+    it "renders phone number as link" do
+      expect(body).to have_link("+41 31 370 18 18", href: "tel:+41313701818")
+    end
+
+    it "does render email as link" do
+      expect(body).to have_link(SacCas::MV_EMAIL)
+    end
+
+    [:de, :fr, :it].each do |locale|
+      it "renders mv email in #{locale}" do
+        I18n.with_locale(locale) do
+          expect(body).to have_link(SacCas::MV_EMAIL)
+        end
+      end
+    end
+  end
+end
