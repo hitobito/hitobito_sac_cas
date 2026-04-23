7e30afb96b06ad9d79a8ec44dc45b64b488e4ae8
Pascal Zumkehr <zumkehr@puzzle.ch>
2026-04-23 17:53:43 +0200

    Init sac section custom content payloads (#2398)

--- app/models/sac_cas/custom_content.rb
+++ app/models/sac_cas/custom_content.rb
@@ -20,9 +20,31 @@
 module SacCas::CustomContent
   extend ActiveSupport::Concern
 
+  SECTION_SPECIFIC_CONTENTS_FILE =
+    HitobitoSacCas::Wagon.root.join("db/seeds/sac_section_custom_contents.yml")
+
   def body_with_values(placeholders = {})
     body.to_s.html_safe
       .then { RenderMarkdownTables.replace_markdown_tables(_1) }
       .then { replace_placeholders(_1, placeholders) }
+  end
+
+  module ClassMethods
+    def init_section_specific_contents(group)
+      section_specific_templates.each do |key, attrs|
+        entry = group.custom_contents.find_or_initialize_by(key: key)
+
+        entry.attributes = attrs.slice("label", "placeholders_optional", "placeholders_required")
+        if entry.new_record?
+          entry.subject = attrs["subject"]
+          entry.body = attrs["body"].split("\n").join("<br>")
+        end
+        entry.save!
+      end
+    end
+
+    def section_specific_templates
+      YAML.load_file(SECTION_SPECIFIC_CONTENTS_FILE)
+    end
   end
 end
