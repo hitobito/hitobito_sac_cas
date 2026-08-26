7e9f1ac6e091717543d15f5c24626ee81a8f5af9
Matthias Viehweger <kronn@kronn.de>
2026-08-26 21:05:13 +0200

    Add cornercard-step for Wizard

---
+++ app/models/wizards/steps/signup/sektion/cornercard_fields.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2026, Schweizer Alpen-Club. This file is part of
+#  hitobito_sac_cas and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sac_cas.
+
+module Wizards::Steps::Signup::Sektion
+  class CornercardFields < Wizards::Step
+    attribute :card_application, :boolean, default: false
+    attribute :consent_given, :boolean, default: false
+
+    validate :assert_consent_given_with_card_application
+
+    def ordered?
+      card_application && consent_given
+    end
+
+    private
+
+    def assert_consent_given_with_card_application
+      return true if card_application == consent_given
+
+      errors.add(:base, :cornercard_consent_missing)
+    end
+  end
+end
