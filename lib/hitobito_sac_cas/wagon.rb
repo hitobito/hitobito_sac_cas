bc776e0181a58e43ca8012988695a834bdb69afa
Matthias Viehweger <kronn@kronn.de>
2026-08-31 16:14:59 +0200

    Remove unneeded initializer

--- lib/hitobito_sac_cas/wagon.rb
+++ lib/hitobito_sac_cas/wagon.rb
@@ -437,10 +437,6 @@ module HitobitoSacCas
       end
     end
 
-    initializer "sac_cas.precompile_assets" do |_app|
-      Rails.application.config.assets.precompile += %w[cornercard-sac.png]
-    end
-
     initializer "sac_cas.append_doorkeeper_scope" do |_app|
       Doorkeeper.configuration.scopes.add "user_groups"
     end
