3c79e478d10798e31233ffff815e234e81b7f6e2
z <zumkehr@puzzle.ch>
2026-09-04 14:16:23 +0200

    Restrict creating/destroying certain groups for sektions admins (#2599)

--- app/abilities/sac_cas/group_ability.rb
+++ app/abilities/sac_cas/group_ability.rb
@@ -8,6 +8,18 @@
 module SacCas::GroupAbility
   extend ActiveSupport::Concern
 
+  RESTRICTED_GROUPS = [
+    Group::SektionsClubhuetten,
+    Group::SektionsClubhuette,
+    Group::Sektionshuetten,
+    Group::Sektionshuette,
+    Group::SektionsMitglieder,
+    Group::SektionsNeuanmeldungenNv,
+    Group::SektionsNeuanmeldungenSektion,
+    Group::SektionsFunktionaere,
+    Group::Ortsgruppe
+  ]
+
   prepended do
     on(Group) do
       permission(:any).may(:"index_event/tours").all
@@ -19,10 +31,46 @@ module SacCas::GroupAbility
       permission(:layer_and_below_read)
         .may(:download_statistics)
         .in_same_layer
+
+      permission(:layer_and_below_full)
+        .may(:create)
+        .with_parent_in_same_layer_or_below_except_restricted
+
+      permission(:layer_and_below_full)
+        .may(:destroy)
+        .in_same_layer_or_below_except_permission_giving_or_restricted
+
+      permission(:group_and_below_full)
+        .may(:create)
+        .with_parent_in_same_group_hierarchy_except_restricted
+
+      permission(:group_and_below_full)
+        .may(:destroy)
+        .in_below_group_except_restricted
     end
   end
 
   def if_backoffice
     role_type?(*SacCas::SAC_BACKOFFICE_ROLES)
+  end
+
+  def with_parent_in_same_layer_or_below_except_restricted
+    with_parent_in_same_layer_or_below && !restricted_sektion_group?
+  end
+
+  def with_parent_in_same_group_hierarchy_except_restricted
+    with_parent_in_same_group_hierarchy && !restricted_sektion_group?
+  end
+
+  def in_same_layer_or_below_except_permission_giving_or_restricted
+    in_same_layer_or_below_except_permission_giving && !restricted_sektion_group?
+  end
+
+  def in_below_group_except_restricted
+    in_below_group && !restricted_sektion_group?
+  end
+
+  def restricted_sektion_group?
+    !if_backoffice && RESTRICTED_GROUPS.include?(subject.class)
   end
 end
